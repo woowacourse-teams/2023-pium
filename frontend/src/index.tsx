e7e8b5b6569038c542573f8d090fc1a812925cb3
bassyu <yrkdgus@gmail.com>
2023-07-05 17:11:17 +0900

    style: import 정렬에 상대경로도 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
-import App from './App';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
