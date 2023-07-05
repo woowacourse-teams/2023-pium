dcb82834a7a4b5d5057f8ffb05e90ca25d6b3e3e
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-07-05 16:10:57 +0900

    build: 백엔드 초기 설정

---
+++ backend/pium/src/main/java/com/official/pium/PiumApplication.java
@@ -0,0 +1,13 @@
+package com.official.pium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiumApplication.class, args);
+	}
+
+}
---
+++ backend/pium/src/test/java/com/official/pium/PiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.official.pium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
