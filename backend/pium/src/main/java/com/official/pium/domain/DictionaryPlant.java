157221db761d7887dd44bcb303b7ea828890edea
최준호 <junho5336@gmail.com>
2023-07-11 16:31:59 +0900

    feat: 사전 식물 엔티티 추가

---
+++ backend/pium/src/main/java/com/official/pium/domain/DictionaryPlant.java
@@ -0,0 +1,56 @@
+package com.official.pium.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "dictionary_plant")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DictionaryPlant extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "family_name")
+    private String familyName;
+
+    @Column(name = "smell")
+    private String smell;
+
+    @Column(name = "poison")
+    private String poison;
+
+    @Column(name = "manage_level")
+    private String manageLevel;
+
+    @Column(name = "grow_speed")
+    private String growSpeed;
+
+    @Column(name = "require_temp")
+    private String requireTemp;
+
+    @Column(name = "minimum_temp")
+    private String minimumTemp;
+
+    @Column(name = "require_humidity")
+    private String requireHumidity;
+
+    @Column(name = "posting_place")
+    private String postingPlace;
+
+    @Column(name = "special_manage_info")
+    private String specialManageInfo;
+
+    @Embedded
+    private WaterCycle waterCycle;
+}
