c8b8fe60eb254c626c1e16b58ffc99ee1d9deacf
hozzijeong <ghwls0905@naver.com>
2023-07-12 13:36:45 +0900

    feat: global css storybook에 설정

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from '@storybook/react';
+import { decorateGlobalStyle } from './decolators';
 
 const preview: Preview = {
+  decorators: [decorateGlobalStyle],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
