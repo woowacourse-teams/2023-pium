12d40e039fa59897fdc00456c23465fa85404b4b
Q Kim <qoli0703@gmail.com>
2023-07-12 11:15:59 +0900

    feat: 식물사전 이름검색, 사전 식물 정보 mock data

---
+++ frontend/src/mocks/data/search.ts
@@ -0,0 +1,24 @@
+const SEARCH_DATA = [
+  {
+    id: 1,
+    name: '아',
+    image: 'https://images.unsplash.com/photo-1667342608690-828e1a839ead',
+  },
+  {
+    id: 2,
+    name: '아카',
+    image: 'https://images.unsplash.com/photo-1516205651411-aef33a44f7c2',
+  },
+  {
+    id: 3,
+    name: '아카시',
+    image: 'https://images.unsplash.com/photo-1519567770579-c2fc5436bcf9',
+  },
+  {
+    id: 4,
+    name: '아카시아',
+    image: 'https://images.unsplash.com/photo-1510325805092-2951ab330b7d',
+  },
+];
+
+export default SEARCH_DATA;
