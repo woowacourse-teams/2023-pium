f2a16edd50bbbff14b5f3ad918a5595ce84ea2f3
최준호 <junho5336@gmail.com>
2023-07-12 16:53:02 +0900

    feat: 데이터베이스 스키마 구성 (#32)
    * feat: 멤버 엔티티 추가
    
    * feat: 사전 식물 엔티티 추가
    
    * feat: 반려식물 엔티티 추가
    
    * test: 엔티티 저장, 조회 테스트 추가
    
    * build: schema.sql 추가

---
+++ backend/pium/src/main/java/com/official/pium/domain/DictionaryPlant.java
@@ -0,0 +1,74 @@
+package com.official.pium.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "dictionary_plant")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DictionaryPlant extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "family_name")
+    private String familyName;
+
+    @Column(name = "smell")
+    private String smell;
+
+    @Column(name = "poison")
+    private String poison;
+
+    @Column(name = "manage_level")
+    private String manageLevel;
+
+    @Column(name = "grow_speed")
+    private String growSpeed;
+
+    @Column(name = "require_temp")
+    private String requireTemp;
+
+    @Column(name = "minimum_temp")
+    private String minimumTemp;
+
+    @Column(name = "require_humidity")
+    private String requireHumidity;
+
+    @Column(name = "posting_place")
+    private String postingPlace;
+
+    @Column(name = "special_manage_info")
+    private String specialManageInfo;
+
+    @Embedded
+    private WaterCycle waterCycle;
+
+    @Builder
+    public DictionaryPlant(String name, String imageUrl, String familyName, String smell, String poison, String manageLevel, String growSpeed, String requireTemp, String minimumTemp, String requireHumidity, String postingPlace, String specialManageInfo, WaterCycle waterCycle) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.familyName = familyName;
+        this.smell = smell;
+        this.poison = poison;
+        this.manageLevel = manageLevel;
+        this.growSpeed = growSpeed;
+        this.requireTemp = requireTemp;
+        this.minimumTemp = minimumTemp;
+        this.requireHumidity = requireHumidity;
+        this.postingPlace = postingPlace;
+        this.specialManageInfo = specialManageInfo;
+        this.waterCycle = waterCycle;
+    }
+}
---
+++ backend/pium/src/main/resources/sql/schema.sql
@@ -0,0 +1,58 @@
+CREATE TABLE IF NOT EXISTS dictionary_plant
+(
+    id                  BIGINT AUTO_INCREMENT NOT NULL,
+    name                VARCHAR(255)          NULL,
+    image_url           TEXT                  NULL,
+    family_name         VARCHAR(255)          NULL,
+    smell               VARCHAR(255)          NULL,
+    poison              VARCHAR(255)          NULL,
+    manage_level        VARCHAR(255)          NULL,
+    grow_speed          VARCHAR(255)          NULL,
+    require_temp        VARCHAR(255)          NULL,
+    minimum_temp        VARCHAR(255)          NULL,
+    require_humidity    VARCHAR(255)          NULL,
+    posting_place       VARCHAR(255)          NULL,
+    special_manage_info TEXT                  NULL,
+    water_cycle_spring  VARCHAR(255)          NULL,
+    water_cycle_summer  VARCHAR(255)          NULL,
+    water_cycle_autumn  VARCHAR(255)          NULL,
+    water_cycle_winter  VARCHAR(255)          NULL,
+    created_at          DATETIME              NOT NULL,
+    updated_at          DATETIME              NOT NULL,
+    CONSTRAINT pk_dictionary_plant PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS member
+(
+    id         BIGINT AUTO_INCREMENT NOT NULL,
+    email      VARCHAR(255)          NULL,
+    created_at DATETIME              NOT NULL,
+    updated_at DATETIME              NOT NULL,
+    CONSTRAINT pk_member PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS pet_plant
+(
+    id                  BIGINT AUTO_INCREMENT NOT NULL,
+    member_id           BIGINT                NULL,
+    dictionary_plant_id BIGINT                NULL,
+    nickname            VARCHAR(255)          NOT NULL,
+    image_url           TEXT                  NOT NULL,
+    location            VARCHAR(255)          NOT NULL,
+    flowerpot           VARCHAR(255)          NOT NULL,
+    light               VARCHAR(255)          NOT NULL,
+    wind                VARCHAR(255)          NOT NULL,
+    birth_date          DATE                  NOT NULL,
+    next_water_date     DATE                  NOT NULL,
+    last_water_date     DATE                  NOT NULL,
+    water_cycle         INT                   NOT NULL,
+    created_at          DATETIME              NOT NULL,
+    updated_at          DATETIME              NOT NULL,
+    CONSTRAINT pk_pet_plant PRIMARY KEY (id)
+);
+
+ALTER TABLE pet_plant
+    ADD CONSTRAINT FK_PET_PLANT_ON_DICTIONARY_PLANT FOREIGN KEY (dictionary_plant_id) REFERENCES dictionary_plant (id);
+
+ALTER TABLE pet_plant
+    ADD CONSTRAINT FK_PET_PLANT_ON_MEMBER FOREIGN KEY (member_id) REFERENCES member (id);
