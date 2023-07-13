5dc6b3aae6dc33d3b5079434b17409ef74010d40
hozzijeong <ghwls0905@naver.com>
2023-07-13 09:47:44 +0900

    chore: wepback font 확장자 설정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -42,6 +42,10 @@ module.exports = {
         test: /\.(png|jpg|jpeg|svg)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
