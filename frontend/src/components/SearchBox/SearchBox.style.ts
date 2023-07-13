78794536ab56d665b6e1999c4a563df012f5d6d2
유강현 <yrkdgus@gmail.com>
2023-07-13 15:30:32 +0900

    feature: 사전 식물 검색창 컴포넌트 구현 (#34)
    * build: prettier에서 ./ 뿐만 아니라 . 도 포함
    
    * build: react-icons 설치
    
    * feat: SearchBox 컴포넌트 구현
    
    - 검색 트리거: 검색어 입력, 엔터, 화살표 클릭
    - 디바운싱: 검색어를 입력하고 0.15초동안 멈추면 API 요청
    
    * chore: Story -> Default 로 네이밍 변경
    
    * feat: API 타입 추가
    
    * chore: 결과 아이템에 alt 추가
    
    * feat: 메세지 상수 객체로 변경 및 불변성 부여
    
    * design: 검색 버튼 가운데 정렬
    
    * chore: 의미없는 async 삭제
    
    * fix: timeout이 실행되면 timeoutId를 null로 선언
    
    * feat: timeoutId는 0일 때 없다는 뜻이르모 null 타입 삭제

---
+++ frontend/src/components/SearchBox/SearchBox.style.ts
@@ -0,0 +1,82 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: min-content;
+
+  border: solid 2px #1bcc66;
+  border-radius: 29px;
+`;
+
+export const InputArea = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 32px;
+  margin: 12px 0;
+  margin-left: 8px;
+
+  font-size: 1.4rem;
+
+  border: none;
+  outline: none;
+`;
+
+export const ResultMessage = styled.p`
+  width: 100%;
+  height: 40px;
+
+  font-size: 1.4rem;
+  color: #333333;
+  text-align: center;
+`;
+
+export const ResultList = styled.div`
+  width: 100%;
+`;
+
+export const ResultItem = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 56px;
+  padding-left: 12px;
+
+  border-top: solid 2px rgba(27, 204, 102, 0.3);
+`;
+
+const skeletonBackground = keyframes`
+  0%    { background-color: rgba(27, 204, 10, 0.2) }
+  50%   { background-color: rgba(27, 204, 10, 0.5) }
+`;
+
+export const ResultThumbnail = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-right: 12px;
+
+  object-fit: cover;
+  border-radius: 50%;
+
+  animation: ${skeletonBackground} 1s infinite;
+`;
+
+export const Name = styled.p`
+  font-size: 1.4rem;
+  color: #333333;
+`;
+
+export const EnterButton = styled.button`
+  display: flex;
+  align-items: center;
+  background: transparent;
+  border: none;
+`;
