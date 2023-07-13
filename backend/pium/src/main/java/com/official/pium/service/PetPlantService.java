780e96bee57de7af4162519580f6cbab3d0a8cf8
최준호 <junho5336@gmail.com>
2023-07-13 13:32:21 +0900

    feat: 반려 식물 등록 서비스 추가

---
+++ backend/pium/src/main/java/com/official/pium/service/PetPlantService.java
@@ -0,0 +1,31 @@
+package com.official.pium.service;
+
+import com.official.pium.controller.dto.PetPlantRequest;
+import com.official.pium.domain.DictionaryPlant;
+import com.official.pium.domain.Member;
+import com.official.pium.domain.PetPlant;
+import com.official.pium.mapper.PetPlantMapper;
+import com.official.pium.repository.DictionaryPlantRepository;
+import com.official.pium.repository.PetPlantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PetPlantService {
+
+    private final PetPlantRepository petPlantRepository;
+    private final DictionaryPlantRepository dictionaryPlantRepository;
+
+    public void create(PetPlantRequest request, Member member) {
+        DictionaryPlant dictionaryPlant = dictionaryPlantRepository.findById(request.getDictionaryPlantId())
+                .orElseThrow(() -> new NoSuchElementException("사전 식물이 존재하지 않습니다. id : " + request.getDictionaryPlantId()));
+
+        PetPlant petPlant = PetPlantMapper.toEntity(request, dictionaryPlant, member);
+        petPlantRepository.save(petPlant);
+    }
+}
