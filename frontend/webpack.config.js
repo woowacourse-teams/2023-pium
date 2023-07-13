1b96e92c6c87296e193b0b41261195b0a5eab694
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-07-13 16:31:33 +0900

    chore: asset 확장자 병합
    Co-authored-by: 유강현 <yrkdgus@gmail.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -39,11 +39,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpg|jpeg|svg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(png|jpg|jpeg|svg|woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
