11c6191b4a4ccab2bb80dbabc174147461d5434c
gray <68818952+Kim0914@users.noreply.github.com>
2023-07-13 10:42:14 +0900

    fix: Repository 인터페이스 접근 제어자 추가 (#38)

--- backend/pium/src/main/java/com/official/pium/repository/MemberRepository.java
+++ backend/pium/src/main/java/com/official/pium/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package com.official.pium.repository;
 import com.official.pium.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
