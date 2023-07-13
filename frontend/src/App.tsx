3ca6ba7f19ca3f3c743bf5cfdbb6b2c3f4efe130
hozzijeong <ghwls0905@naver.com>
2023-07-13 09:48:32 +0900

    chore: font 테스트 위한 코드 작성.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { styled } from 'styled-components';
+
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Font>나눔 스퀘어 라운드입니다.</Font>
+      <Common>일반 폰트입니다.</Common>
+    </div>
+  );
 };
 
 export default App;
+
+const Font = styled.p`
+  font: ${(props) => props.theme.font.title};
+`;
+
+const Common = styled.p`
+  font-size: 3.2rem;
+`;
