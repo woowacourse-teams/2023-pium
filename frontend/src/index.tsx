4795e56a6e65e918b1f62b984077cd5a4f48c516
bassyu <yrkdgus@gmail.com>
2023-07-14 16:46:36 +0900

    chore: 병합에서 중복된 코드 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import App from './App';
 import { GlobalStyle } from './style/Global.style';
 import { GlobalFont } from './style/GlobalFont.style';
 import theme from './style/theme.style';
-
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { worker } = require('./mocks/browser');
-
-  worker.start({
-    serviceWorker: {
-      url: './mockServiceWorker.js',
-    },
-  });
-}
+import App from './App';
 
 if (process.env.NODE_ENV === 'development') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
