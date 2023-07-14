48fb02fc3ee6b2c2a7c8a944b54b7596f19d2f87
hozzijeong <ghwls0905@naver.com>
2023-07-14 16:58:41 +0900

    design: dateinput design 구현

--- frontend/src/components/DateInput/DateInput.style.ts
+++ frontend/src/components/DateInput/DateInput.style.ts
@@ -1,3 +1,76 @@
 import { styled } from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+export const DateValue = styled.label`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0%);
+
+  display: inline-block;
+
+  font: 500 2rem/3.6rem 'NanumSquareRound';
+`;
+
+export const Date = styled.input.attrs({ type: 'date' })`
+  position: relative;
+  z-index: 1;
+
+  width: 100%;
+  padding: 10px;
+
+  font-size: 100%;
+  text-align: center;
+
+  background: transparent;
+  border: none;
+
+  &::before {
+    content: attr(data-placeholder);
+    width: 100%;
+    height: 100%;
+  }
+
+  &::-webkit-datetime-edit-text {
+    display: none;
+    appearance: none;
+  }
+  &::-webkit-datetime-edit-month-field {
+    display: none;
+    appearance: none;
+  }
+  &::-webkit-datetime-edit-day-field {
+    display: none;
+    appearance: none;
+  }
+  &::-webkit-datetime-edit-year-field {
+    display: none;
+    appearance: none;
+  }
+
+  &::-webkit-calendar-picker-indicator {
+    cursor: pointer;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    color: transparent;
+
+    background: transparent;
+  }
+
+  &:hover {
+    background: #0000000c;
+  }
+
+  &:valid::before {
+    display: none;
+  }
+`;
