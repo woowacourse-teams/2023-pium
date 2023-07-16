e54e0490286704358b90c4e0651ad915f923409e
최준호 <junho5336@gmail.com>
2023-07-17 00:42:03 +0900

    refactor: RequestBody 어노테이션 추가

--- backend/pium/src/main/java/com/official/pium/controller/PetPlantController.java
+++ backend/pium/src/main/java/com/official/pium/controller/PetPlantController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -19,7 +20,7 @@ public class PetPlantController {
     private final PetPlantService petPlantService;
 
     @PostMapping("/pet-plants")
-    public ResponseEntity<Void> create(@Valid PetPlantRequest request, Member member) {
+    public ResponseEntity<Void> create(@RequestBody @Valid PetPlantRequest request, Member member) {
         PetPlantResponse petPlantResponse = petPlantService.create(request, member);
         return ResponseEntity.created(URI.create("/pet-plants/" + petPlantResponse.getId())).build();
     }
