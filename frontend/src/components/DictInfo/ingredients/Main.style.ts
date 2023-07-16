e688714cc76b980b418731cb0f29c487dfa66ab2
Q Kim <qoli0703@gmail.com>
2023-07-16 14:48:11 +0900

    design: 사전 식물 정보 디자인 개선
    - 정보 라벨 여러 줄로 표현 가능
    - 정보 제목 중앙 정렬

--- frontend/src/components/DictInfo/ingredients/Main.style.ts
+++ frontend/src/components/DictInfo/ingredients/Main.style.ts
@@ -9,9 +9,11 @@ export interface WrapperProps {
 export const Wrapper = styled.div<WrapperProps>`
   display: inline-flex;
   flex-direction: ${({ flexDirection }) => flexDirection};
+  flex-wrap: wrap;
   row-gap: 10px;
   column-gap: 10px;
+  align-self: start;
 
-  width: ${({ width }) => width ?? ''};
-  height: ${({ height }) => height ?? ''};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
 `;
--- frontend/src/components/DictInfo/ingredients/Title.style.tsx
+++ frontend/src/components/DictInfo/ingredients/Title.style.tsx
@@ -2,7 +2,8 @@ import { styled } from 'styled-components';
 import theme from '../../../style/theme.style';
 
 const Title = styled.p`
-  display: inline;
+  display: inline-flex;
+  align-items: center;
   font-size: 1.8rem;
   color: ${theme.color.sub};
 `;
