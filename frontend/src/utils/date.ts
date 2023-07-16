74b0be26b528673a1f046bc91a692a5fdc5c3234
hozzijeong <ghwls0905@naver.com>
2023-07-16 16:09:10 +0900

    refactor: date 함수 범용적인 형식으로 변경

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -1,16 +1,14 @@
 // YYYY-MM-DD 형태로 받은 날짜를 YYYY년 MM월 DD일로 변경
-export const convertDateKorYear = (date: string) => {
-  const [year, month, day] = date.split('-');
-
-  return year + '년 ' + month.replace('0', '') + '월 ' + day.replace('0', '') + '일 ';
-};
+export const convertDateKorYear = (date: string) =>
+  new Date(date).toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
 // 오늘 날짜를 YYYY-MM-DD 형태로 받기
 export const getToday = () => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = ('0' + (today.getMonth() + 1)).slice(-2);
-  const day = ('0' + today.getDate()).slice(-2);
-
-  return [year, month, day].join('-');
+  const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  const localTime = new Date(Date.now() - timezoneOffset);
+  return localTime.toISOString().slice(0, 10);
 };
