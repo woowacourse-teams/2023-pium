2fbf25a3dfa167a616aecf514aa88251fe870f81
Q Kim <qoli0703@gmail.com>
2023-07-16 18:30:11 +0900

    refactor: 컴포넌트명 파일명에 맞게 변경

--- frontend/src/components/DictInfo/index.tsx
+++ frontend/src/components/DictInfo/index.tsx
@@ -2,6 +2,6 @@ import Title from './ingredients/Title.style';
 import Content from './ingredients/Content';
 import Main from './ingredients/Main';
 
-const DictionaryInfo = Object.assign(Main, { Title, Content });
+const DictInfo = Object.assign(Main, { Title, Content });
 
-export default DictionaryInfo;
+export default DictInfo;
