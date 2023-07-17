1620cedcb1c63450c94b34fef7037ba4a634b306
Q Kim <qoli0703@gmail.com>
2023-07-17 18:24:13 +0900

    design: 라디오 선택된 항목 색 변경

--- frontend/src/components/InlineRadio/ingredients/Option.style.ts
+++ frontend/src/components/InlineRadio/ingredients/Option.style.ts
@@ -1,10 +1,12 @@
 import { styled } from 'styled-components';
+import theme from '../../../style/theme.style';
 
 export const HiddenRadio = styled.input`
   display: none;
 
   &:checked + span {
     font-weight: bold;
+    color: ${theme.color.primary};
     text-shadow: 1.2px 1.2px 1.2px lightgrey;
   }
 `;
