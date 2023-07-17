efcfa53597c57dddcf04309954db8ca25c055de1
최준호 <junho5336@gmail.com>
2023-07-17 19:50:54 +0900

    feat: 반려 식물 전체 조회기능 추가

--- backend/pium/src/main/java/com/official/pium/repository/PetPlantRepository.java
+++ backend/pium/src/main/java/com/official/pium/repository/PetPlantRepository.java
@@ -3,5 +3,8 @@ package com.official.pium.repository;
 import com.official.pium.domain.PetPlant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetPlantRepository extends JpaRepository<PetPlant, Long> {
+    List<PetPlant> findAllByMemberId(Long id);
 }
--- backend/pium/src/main/java/com/official/pium/service/PetPlantService.java
+++ backend/pium/src/main/java/com/official/pium/service/PetPlantService.java
@@ -6,15 +6,19 @@ import com.official.pium.domain.PetPlant;
 import com.official.pium.mapper.PetPlantMapper;
 import com.official.pium.repository.DictionaryPlantRepository;
 import com.official.pium.repository.PetPlantRepository;
+import com.official.pium.service.dto.DataResponse;
 import com.official.pium.service.dto.PetPlantRequest;
 import com.official.pium.service.dto.PetPlantResponse;
+import com.official.pium.service.dto.SinglePetPlantResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -36,6 +40,16 @@ public class PetPlantService {
         long nextWaterDay = getNextWaterDay(petPlant);
 
         return PetPlantMapper.toPetPlantResponse(petPlant, nextWaterDay, daySince);
+    }
+
+    public DataResponse<SinglePetPlantResponse> readAll(Member member) {
+        List<PetPlant> petPlants = petPlantRepository.findAllByMemberId(member.getId());
+
+        List<SinglePetPlantResponse> singlePetPlantResponses = petPlants.stream()
+                .map(petPlant -> PetPlantMapper.toSinglePetPlantResponse(petPlant, getDaySince(petPlant)))
+                .collect(Collectors.toList());
+
+        return new DataResponse<>(singlePetPlantResponses);
     }
 
     private long getNextWaterDay(PetPlant petPlant) {
