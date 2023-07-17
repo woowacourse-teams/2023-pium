30c9387139304bba9d947c5a02dbdaded30ab825
최준호 <junho5336@gmail.com>
2023-07-17 19:50:35 +0900

    feat: Data Response 추가

--- backend/pium/src/main/java/com/official/pium/service/dto/DataResponse.java
+++ backend/pium/src/main/java/com/official/pium/service/dto/DataResponse.java
@@ -4,9 +4,15 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DataResponse<T> {
-    
-    private T data;
+
+    private List<T> data;
+
+    public DataResponse(List<T> data) {
+        this.data = data;
+    }
 }
