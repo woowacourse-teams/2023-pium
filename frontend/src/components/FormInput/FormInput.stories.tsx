62a78b1edd9a1a0a8bf1a55a4f2ca1400e21431d
hozzijeong <ghwls0905@naver.com>
2023-07-18 13:53:57 +0900

    feat: FormInput에 필요한 파일 생성

---
+++ frontend/src/components/FormInput/FormInput.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import FormInput from '.';
+
+const meta: Meta<typeof FormInput> = {
+  component: FormInput,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FormInput>;
+
+export const Default: Story = {};
---
+++ frontend/src/components/FormInput/index.tsx
@@ -0,0 +1,5 @@
+const FormInput = () => {
+  return <div></div>;
+};
+
+export default FormInput;
