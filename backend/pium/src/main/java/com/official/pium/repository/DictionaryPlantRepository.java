dfbf2fcfadd8993742cd499d9fec07fb448025a0
rawfishthelgh <rg970604@naver.com>
2023-07-18 14:57:33 +0900

    style: 사전 식물 검색기능 구현

--- backend/pium/src/main/java/com/official/pium/repository/DictionaryPlantRepository.java
+++ backend/pium/src/main/java/com/official/pium/repository/DictionaryPlantRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DictionaryPlantRepository extends JpaRepository<DictionaryPlant, Long> {
+
     List<DictionaryPlant> findDictionaryPlantsByNameContains(String name);
 }
--- backend/pium/src/main/java/com/official/pium/service/dto/DictionaryPlantSearchResponse.java
+++ backend/pium/src/main/java/com/official/pium/service/dto/DictionaryPlantSearchResponse.java
@@ -7,6 +7,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DictionaryPlantSearchResponse {
+
     private Long id;
     private String name;
     private String image;
