156649d0743cb5c9be4df56860e1008604ab322c
hozzijeong <ghwls0905@naver.com>
2023-07-18 15:18:54 +0900

    feat: 기본적인 형태 구현

--- frontend/src/components/FormInput/index.tsx
+++ frontend/src/components/FormInput/index.tsx
@@ -1,5 +1,21 @@
-const FormInput = () => {
-  return <div></div>;
+import { InputHTMLAttributes } from 'react';
+import { GrLinkNext } from 'react-icons/gr';
+import { Input, Wrapper } from './FormInput.style';
+
+interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  onChangeCallback: React.ChangeEventHandler<HTMLInputElement>;
+  nextCallback?: () => void;
+}
+
+const FormInput = (props: FormInputProps) => {
+  const { nextCallback, onChangeCallback, ...inputProps } = props;
+
+  return (
+    <Wrapper>
+      <Input {...inputProps} onChange={onChangeCallback} />
+      {nextCallback && <GrLinkNext size={20} onClick={nextCallback} />}
+    </Wrapper>
+  );
 };
 
 export default FormInput;
