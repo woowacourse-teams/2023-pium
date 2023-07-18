38a1b2fda8487f370fbbc1ca28c1fccc894fd106
hozzijeong <ghwls0905@naver.com>
2023-07-18 15:19:04 +0900

    test: storybook 작성

--- frontend/src/components/FormInput/FormInput.stories.tsx
+++ frontend/src/components/FormInput/FormInput.stories.tsx
@@ -9,4 +9,25 @@ export default meta;
 
 type Story = StoryObj<typeof FormInput>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: () => {
+    const changeCallback: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+      console.log(e.target.value);
+
+    const nextCallback = () => console.log('clicked');
+
+    return (
+      <>
+        <FormInput
+          type="number"
+          inputMode="numeric"
+          max={365}
+          min={1}
+          nextCallback={nextCallback}
+          onChangeCallback={changeCallback}
+        />
+        <FormInput onChangeCallback={changeCallback} />
+      </>
+    );
+  },
+};
