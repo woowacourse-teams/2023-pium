6759e87c1595cfdfe55551b545b05e1d489f9316
hozzijeong <ghwls0905@naver.com>
2023-07-18 16:27:57 +0900

    refactor: 범위 유효성 검사 추가 및 주석 작성

--- frontend/src/utils/validate.ts
+++ frontend/src/utils/validate.ts
@@ -1,13 +1,28 @@
 export const inputValidate = {
+  // 입력 값이 0~9인지 확인하는 메서드
   checkNumber: (value: string) => {
     const regExp = /^[0-9]+$/;
 
     return regExp.test(value);
   },
 
-  checkRange: (value: number, start: number, end: number) => {
-    return value >= start && value <= end;
+  // 입력받은 값이 범위 내에 있는지 확인하는 메서드
+  checkRange: (value: number, start?: number, end?: number) => {
+    if (start && end) {
+      return value >= start && value <= end;
+    }
+
+    if (start && !end) {
+      return value >= start;
+    }
+
+    if (!start && end) {
+      return value <= end;
+    }
+
+    return true;
   },
 
-  checkECode: (value: string) => ['e', 'E', '+', '-'].includes(value),
+  // 숫자외에 다른 값들이 들어가있는지 확인하는 메서드
+  checkECode: (value: string) => ['e', 'E', '+', '-', '.'].includes(value),
 };
