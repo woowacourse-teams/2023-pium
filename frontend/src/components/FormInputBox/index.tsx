18e90d0033615ee2c432e83899df77be30693dda
hozzijeong <ghwls0905@naver.com>
2023-07-18 12:47:55 +0900

    refactor: nextButton 제거

--- frontend/src/components/FormInputBox/index.tsx
+++ frontend/src/components/FormInputBox/index.tsx
@@ -1,5 +1,4 @@
 import { PropsWithChildren } from 'react';
-import { GrLinkNext } from 'react-icons/gr';
 import { InputBox, RequireFlag, Title, Wrapper } from './FormInputBox.style';
 
 interface FormInputBoxProps extends PropsWithChildren {
@@ -8,22 +7,14 @@ interface FormInputBoxProps extends PropsWithChildren {
 }
 
 const FormInputBox = ({ children, title, require = false }: FormInputBoxProps) => {
-  const nextHandler = () => {
-    console.log('next clicked');
-  };
-
   return (
     <Wrapper>
       <Title>
         {title}
         <RequireFlag>{require ? '*' : ''}</RequireFlag>
       </Title>
-      <InputBox>
-        {children}
-        <button type="button" onClick={nextHandler}>
-          <GrLinkNext size={20} />
-        </button>
-      </InputBox>
+      <InputBox>{children}</InputBox>
+      <span></span>
     </Wrapper>
   );
 };
