2c72f929372da010bd97896e12474bcf71e744d6
hozzijeong <ghwls0905@naver.com>
2023-07-18 21:42:37 +0900

    feat: 상세페이지 파일 생성 및 경로 적용

---
+++ frontend/src/pages/DictionaryDetail/index.tsx
@@ -0,0 +1,6 @@
+const DictionaryDetail = () => {
+  console.log('DictionaryDetail render');
+  return <div>상세 페이지 입니다!</div>;
+};
+
+export default DictionaryDetail;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
+import DictionaryDetail from 'pages/DictionaryDetail';
 import Main from 'pages/Main';
 import Root from 'pages/Root';
+import { URL_PATH } from './constants';
 
 const router = createBrowserRouter([
   {
-    path: '',
+    path: URL_PATH.MAIN,
     element: <Root />,
     children: [
       {
-        path: '',
+        path: URL_PATH.MAIN,
         element: <Main />,
+      },
+      {
+        path: URL_PATH.DICT(),
+        element: <DictionaryDetail />,
       },
     ],
   },
