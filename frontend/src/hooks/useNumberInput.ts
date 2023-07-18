c6af5f18a5aafe281311c44023c06a86592ba603
hozzijeong <ghwls0905@naver.com>
2023-07-18 16:24:13 +0900

    feat: number형 Input에만 사용될 hooks 구현

---
+++ frontend/src/hooks/useNumberInput.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { inputValidate } from 'utils/validate';
+
+interface NumberInputProps {
+  maxRange?: number;
+  minRange?: number;
+}
+
+const useNumberInput = ({ maxRange, minRange }: NumberInputProps) => {
+  const [numberValue, setNumberValue] = useState<number>();
+  const { checkNumber, checkRange, checkECode } = inputValidate;
+
+  const changeCallback: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.target;
+
+    if (value === '') {
+      setNumberValue(undefined);
+      return;
+    }
+
+    if (!checkNumber(value)) return; // 여기서 에러 값 업데이트
+    if (!checkRange(Number(value), minRange, maxRange)) return; // 여기서 에러 값 업데이트
+
+    setNumberValue(Number(value));
+  };
+  const keyDownHandler: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    checkECode(e.key) && e.preventDefault();
+  };
+
+  return { numberValue, changeCallback, keyDownHandler };
+};
+
+export default useNumberInput;
