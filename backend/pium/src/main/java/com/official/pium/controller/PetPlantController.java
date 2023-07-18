904245a0383db0f9aaebe333e357a008c7ed3b1d
gray <68818952+Kim0914@users.noreply.github.com>
2023-07-18 14:41:14 +0900

    feat: 반려 식물 단일 조회 (#67)
    * feat: 물주기 남은 날짜, 함께한 날 도메인 로직 추가
    
    * chore: rebase conflict resolve
    
    * refactor: 테스트 대상 객체 수정
    
    * feat: 예외를 전역으로 처리하는 handler 추가
    
    * feat: 단일 반려 식물 조회 요청을 처리하는 기능 추가
    
    * feat: 반려 식물 조회 기능 추가
    
    * refactor: 테스트 fixture 및 반환 타입 수정
    
    * chore: controller dto 패키지 삭제
    
    * feat: IllegalArgumentException 핸들링 추가
    
    * refactor: 물주기 주기, 함께한 날 계산 메서드 예외 메시지 변경
    
    * refactor: 기본 생성자 접근자 변경
    
    * refactor: 반려 식물 생성 시 물주기 날짜, 함께한 날 계산 로직 변경
    
    * refactor: 테스트 fixture, 변수명 변경
    
    * refactor: 반려 식물 PK 검증 메시지 수정
    
    * test: 반려 식물 PK 검증 메시지 수정
    
    * refactor: PathVariable 명 변경

--- backend/pium/src/main/java/com/official/pium/controller/PetPlantController.java
+++ backend/pium/src/main/java/com/official/pium/controller/PetPlantController.java
@@ -5,21 +5,35 @@ import com.official.pium.service.PetPlantService;
 import com.official.pium.service.dto.PetPlantRequest;
 import com.official.pium.service.dto.PetPlantResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
+@Validated
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/pet-plants")
 public class PetPlantController {
 
     private final PetPlantService petPlantService;
 
-    @PostMapping("/pet-plants")
+    @GetMapping("/{id}")
+    public ResponseEntity<PetPlantResponse> read(
+            @PathVariable
+            @Positive(message = "반려 식물 ID는 1이상의 값이어야 합니다.") Long id) {
+        PetPlantResponse petPlantResponse = petPlantService.read(id);
+        return ResponseEntity.ok(petPlantResponse);
+    }
+
+    @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid PetPlantRequest request, Member member) {
         PetPlantResponse petPlantResponse = petPlantService.create(request, member);
         return ResponseEntity.created(URI.create("/pet-plants/" + petPlantResponse.getId())).build();
