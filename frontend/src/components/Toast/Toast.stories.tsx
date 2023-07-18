f5e718ad3d739ad69b908eb9697fb822782454c7
bassyu <yrkdgus@gmail.com>
2023-07-18 13:03:06 +0900

    feat: 토스트 스토리 작성

---
+++ frontend/src/components/Toast/Toast.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import useBoolean from 'hooks/useBoolean';
+import Toast from '.';
+
+const meta: Meta<typeof Toast> = {
+  component: Toast,
+  render: (props) => {
+    const { boolean: isOpen, on } = useBoolean();
+
+    setTimeout(on);
+
+    return <>{isOpen && <Toast {...props} />}</>;
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Toast>;
+
+export const InfoWithTitle: Story = {
+  args: {
+    type: 'info',
+    message: '피움 서비스의 정보 입니다.',
+    title: '제목이 있는 피움 서비스의 정보입니다.',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    type: 'success',
+    message: '성공했어요!',
+  },
+};
+
+export const ToastWithPopupButton: Story = {
+  args: {
+    type: 'error',
+    message: '새로고침 해주세요',
+  },
+  render: (props) => {
+    const { boolean: isOpen, onTime } = useBoolean();
+
+    const showToast = () => {
+      onTime(1000);
+    };
+
+    return (
+      <>
+        <button type="button" onClick={showToast}>
+          토스트 보기
+        </button>
+        {isOpen && <Toast {...props} />}
+      </>
+    );
+  },
+};
