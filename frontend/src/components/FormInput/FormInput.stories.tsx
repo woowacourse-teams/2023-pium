8fe16f03d5441188eb68c0fdc3612a46459cd1b0
hozzijeong <ghwls0905@naver.com>
2023-07-18 16:28:22 +0900

    chore: number FormInput 스토리 작성

--- frontend/src/components/FormInput/FormInput.stories.tsx
+++ frontend/src/components/FormInput/FormInput.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import useNumberInput from 'hooks/useNumberInput';
+import { NUMBER } from 'constants/index';
 import FormInput from '.';
 
 const meta: Meta<typeof FormInput> = {
@@ -14,20 +16,32 @@ export const Default: Story = {
     const changeCallback: React.ChangeEventHandler<HTMLInputElement> = (e) =>
       console.log(e.target.value);
 
+    return <FormInput onChangeCallback={changeCallback} />;
+  },
+};
+
+export const NumberInput: Story = {
+  render: () => {
+    const { MAX_CYCLE_DATE, MIN_CYCLE_DATE } = NUMBER;
+
+    const { numberValue, changeCallback, keyDownHandler } = useNumberInput({
+      maxRange: MAX_CYCLE_DATE,
+      minRange: MIN_CYCLE_DATE,
+    });
+
     const nextCallback = () => console.log('clicked');
 
     return (
-      <>
-        <FormInput
-          type="number"
-          inputMode="numeric"
-          max={365}
-          min={1}
-          nextCallback={nextCallback}
-          onChangeCallback={changeCallback}
-        />
-        <FormInput onChangeCallback={changeCallback} />
-      </>
+      <FormInput
+        type="number"
+        inputMode="numeric"
+        max={MAX_CYCLE_DATE}
+        min={MIN_CYCLE_DATE}
+        value={numberValue}
+        onKeyDown={keyDownHandler}
+        nextCallback={nextCallback}
+        onChangeCallback={changeCallback}
+      />
     );
   },
 };
