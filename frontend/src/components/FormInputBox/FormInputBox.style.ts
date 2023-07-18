449a9181b7705ff2ac2eb5a5356065a60fd89ca3
hozzijeong <ghwls0905@naver.com>
2023-07-18 13:16:41 +0900

    design: FormInputBox  상태에 따른 밑줄 상태 변경

--- frontend/src/components/FormInputBox/FormInputBox.style.ts
+++ frontend/src/components/FormInputBox/FormInputBox.style.ts
@@ -1,10 +1,38 @@
+import { InputStatus } from 'types/inputs';
 import { styled } from 'styled-components';
+import theme from 'style/theme.style';
+
+interface ContentBoxProps {
+  status: InputStatus;
+}
+
+const getColor = (status: InputStatus) => {
+  switch (status) {
+    case 'default':
+      return theme.color.gray;
+    case 'error':
+      return theme.color.accent;
+    case 'focus':
+      return theme.color.primary;
+
+    default:
+      return theme.color.gray;
+  }
+};
 
 export const Wrapper = styled.div`
   width: 90%;
   margin: 0 auto;
+`;
+
+export const ContentBox = styled.div<ContentBoxProps>`
+  margin-bottom: 4px;
   padding-bottom: 4px;
-  border-bottom: solid 2px ${(props) => props.theme.color.primary};
+  border-bottom: solid 2px ${(props) => getColor(props.status)};
+
+  &:focus {
+    border-bottom: solid 2px #333333;
+  }
 `;
 
 export const RequireFlag = styled.span`
@@ -19,4 +47,9 @@ export const Title = styled.p`
 export const InputBox = styled.div`
   display: flex;
   align-items: center;
+`;
+
+export const ErrorMessage = styled.span`
+  font: 400 1.2rem/1.7rem 'NanumSquareRound';
+  color: ${(props) => props.theme.color.accent};
 `;
