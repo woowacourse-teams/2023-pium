2352141db5251e74fa5518a1cc3a27e46e24b83c
hozzijeong <ghwls0905@naver.com>
2023-07-18 13:16:16 +0900

    refactor: formInputBox props 설정 변경

--- frontend/src/components/FormInputBox/index.tsx
+++ frontend/src/components/FormInputBox/index.tsx
@@ -1,20 +1,38 @@
+import { InputStatus } from 'types/inputs';
 import { PropsWithChildren } from 'react';
-import { InputBox, RequireFlag, Title, Wrapper } from './FormInputBox.style';
+import {
+  ContentBox,
+  ErrorMessage,
+  InputBox,
+  RequireFlag,
+  Title,
+  Wrapper,
+} from './FormInputBox.style';
 
 interface FormInputBoxProps extends PropsWithChildren {
   title: string;
   require: boolean;
+  status: InputStatus;
+  error?: string;
 }
 
-const FormInputBox = ({ children, title, require = false }: FormInputBoxProps) => {
+const FormInputBox = ({
+  children,
+  title,
+  require = false,
+  status = 'default',
+  error,
+}: FormInputBoxProps) => {
   return (
     <Wrapper>
-      <Title>
-        {title}
-        <RequireFlag>{require ? '*' : ''}</RequireFlag>
-      </Title>
-      <InputBox>{children}</InputBox>
-      <span></span>
+      <ContentBox status={status}>
+        <Title>
+          {title}
+          <RequireFlag>{require ? '*' : ''}</RequireFlag>
+        </Title>
+        <InputBox>{children}</InputBox>
+      </ContentBox>
+      {status === 'error' && <ErrorMessage>{error}</ErrorMessage>}
     </Wrapper>
   );
 };
