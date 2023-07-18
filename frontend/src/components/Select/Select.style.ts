507618be853d57649edd129d2c710cdfcda93ace
bassyu <yrkdgus@gmail.com>
2023-07-18 17:25:56 +0900

    style: backdrop 포지션 fixed로 수정

--- frontend/src/components/Select/Select.style.ts
+++ frontend/src/components/Select/Select.style.ts
@@ -44,10 +44,12 @@ export const IconArea = styled.div<IconAreaProps>`
 `;
 
 export const Backdrop = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
-  width: 100vw;
-  height: 100vh;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
 `;
 
 export const OptionBox = styled.ul`
