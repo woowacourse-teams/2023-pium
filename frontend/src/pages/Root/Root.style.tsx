903af5f242e12b513d878bf8d284ca66b911fca8
hozzijeong <ghwls0905@naver.com>
2023-07-19 19:24:07 +0900

    design: Wrapper height 제거

--- frontend/src/pages/Root/Root.style.tsx
+++ frontend/src/pages/Root/Root.style.tsx
@@ -3,10 +3,7 @@ import { styled } from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-
   width: 100%;
-  height: 100%;
-
   background: white;
 `;
 
