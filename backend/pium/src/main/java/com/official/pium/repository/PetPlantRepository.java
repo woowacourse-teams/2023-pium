a636d96a28724dc5447fa858627999d89b01fcf9
최준호 <junho5336@gmail.com>
2023-07-19 11:32:27 +0900

    refactor: 파라미터 이름 명시적으로 표현

--- backend/pium/src/main/java/com/official/pium/repository/PetPlantRepository.java
+++ backend/pium/src/main/java/com/official/pium/repository/PetPlantRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PetPlantRepository extends JpaRepository<PetPlant, Long> {
-    
-    List<PetPlant> findAllByMemberId(Long id);
+
+    List<PetPlant> findAllByMemberId(Long memberId);
 }
