3f09ff688403bd3ab0b0ceb7ea0c2572fed6bb92
bassyu <yrkdgus@gmail.com>
2023-07-19 22:56:42 +0900

    feat: 사전 식물 선택 페이지 구현

---
+++ frontend/src/pages/PetRegister/Search/index.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import SearchBox from 'components/SearchBox';
+import { Message, SearchBoxArea, Wrapper } from './Search.style';
+
+const PetRegisterSearch = () => {
+  const navigate = useNavigate();
+
+  const navigateRegister = (id: number) => {
+    navigate(`/pet-register/${id}`);
+  };
+
+  return (
+    <Wrapper>
+      <Message>어떤 식물을 키우시나요?</Message>
+      <SearchBoxArea>
+        <SearchBox onSelectResultItem={navigateRegister} />
+      </SearchBoxArea>
+    </Wrapper>
+  );
+};
+
+export default PetRegisterSearch;
---
+++ frontend/src/types/index.d.ts
@@ -0,0 +1,10 @@
+export interface PetPlantForm {
+  nickname: string;
+  location: string;
+  flowerpot: string;
+  waterCycle: number;
+  light: string;
+  wind: string;
+  birthDate: string;
+  lastWaterDate: string;
+}
