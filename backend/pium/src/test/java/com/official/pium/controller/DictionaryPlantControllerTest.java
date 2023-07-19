d2e64223a6ff8919bfcd5cf85f4a6d8c17824750
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2023-07-19 10:32:01 +0900

    feat: 사전 식물 상세 정보 조회 기능 구현 (#72)
    * feat: 사전 식물 상세 정보 조회 기능 구현
    
    * refactor: PathVariable `@NotNull` 검증 제거
    
    * feat: 반려 식물 단일 조회 (#67)
    
    * feat: 물주기 남은 날짜, 함께한 날 도메인 로직 추가
    
    * chore: rebase conflict resolve
    
    * refactor: 테스트 대상 객체 수정
    
    * feat: 예외를 전역으로 처리하는 handler 추가
    
    * feat: 단일 반려 식물 조회 요청을 처리하는 기능 추가
    
    * feat: 반려 식물 조회 기능 추가
    
    * refactor: 테스트 fixture 및 반환 타입 수정
    
    * chore: controller dto 패키지 삭제
    
    * feat: IllegalArgumentException 핸들링 추가
    
    * refactor: 물주기 주기, 함께한 날 계산 메서드 예외 메시지 변경
    
    * refactor: 기본 생성자 접근자 변경
    
    * refactor: 반려 식물 생성 시 물주기 날짜, 함께한 날 계산 로직 변경
    
    * refactor: 테스트 fixture, 변수명 변경
    
    * refactor: 반려 식물 PK 검증 메시지 수정
    
    * test: 반려 식물 PK 검증 메시지 수정
    
    * refactor: PathVariable 명 변경
    
    * test: DictionaryPlantControllerTest에 ID 값에 대한 테스트 추가
    
    * chore: 개행 추가, 공백문자 제거
    
    * test: 불필요한 코드 제거
    
    ---------
    
    Co-authored-by: gray <68818952+Kim0914@users.noreply.github.com>

--- backend/pium/src/test/java/com/official/pium/controller/DictionaryPlantControllerTest.java
+++ backend/pium/src/test/java/com/official/pium/controller/DictionaryPlantControllerTest.java
@@ -1,35 +1,64 @@
 package com.official.pium.controller;
 
-import com.official.pium.service.dto.DataResponse;
-import com.official.pium.service.dto.DictionaryPlantSearchResponse;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.official.pium.fixture.DictionaryPlantFixture;
+import com.official.pium.fixture.DictionaryPlantFixture.RESPONSE;
 import com.official.pium.service.DictionaryPlantService;
+import com.official.pium.service.dto.DataResponse;
+import com.official.pium.service.dto.DictionaryPlantResponse;
+import com.official.pium.service.dto.DictionaryPlantSearchResponse;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 @WebMvcTest(controllers = DictionaryPlantController.class)
-public class DictionaryPlantControllerTest {
+class DictionaryPlantControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private DictionaryPlantService dictionaryPlantService;
+
+    @Test
+    void 사전_식물_상세_정보_조회가_성공하면_OK를_반환한다() throws Exception {
+        DictionaryPlantResponse response = RESPONSE.스투키_단일_조회_응답;
+        given(dictionaryPlantService.read(response.getId()))
+                .willReturn(response);
+
+        mockMvc.perform(get("/dictionary-plants/{dictionaryPlantId}", response.getId())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("스투키"))
+                .andExpect(jsonPath("$.postingPlace").isArray())
+                .andExpect(jsonPath("$.waterCycle.spring").value("겉흙이 마르면 촉촉하게"))
+                .andDo(print());
+    }
+
+    @Test
+    void 사전_식물_상세_정보를_0이하의_ID값으로_조회하면_BAD_REQUEST를_반환한다() throws Exception {
+        mockMvc.perform(get("/dictionary-plants/{dictionaryPlantId}", 0L)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", equalTo("사전 식물 ID는 1이상의 값이어야 합니다. Value: 0")));
+    }
 
     @Test
     void 사전_식물_검색이_성공하면_OK를_반환한다() throws Exception {
--- backend/pium/src/test/java/com/official/pium/service/DictionaryPlantServiceTest.java
+++ backend/pium/src/test/java/com/official/pium/service/DictionaryPlantServiceTest.java
@@ -1,32 +1,58 @@
 package com.official.pium.service;
 
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import com.official.pium.IntegrationTest;
 import com.official.pium.domain.DictionaryPlant;
+import com.official.pium.mapper.DictionaryPlantMapper;
 import com.official.pium.service.dto.DataResponse;
+import com.official.pium.service.dto.DictionaryPlantResponse;
 import com.official.pium.service.dto.DictionaryPlantSearchResponse;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-public class DictionaryPlantServiceTest extends IntegrationTest {
+class DictionaryPlantServiceTest extends IntegrationTest {
 
     @Autowired
-    DictionaryPlantService dictionaryPlantService;
+    private DictionaryPlantService dictionaryPlantService;
+
+    @Test
+    void 사전_식물_상세_정보를_조회한다() {
+        DictionaryPlant dictionaryPlant = dictionaryPlantSupport.builder().build();
+        DictionaryPlantResponse actual = dictionaryPlantService.read(dictionaryPlant.getId());
+
+        DictionaryPlantResponse expected = DictionaryPlantMapper.toDictionaryPlantResponse(dictionaryPlant);
+
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void 사전_식물_상세_정보_조회에_실패하면_예외가_발생한다() {
+        Long id = 0L;
+
+        assertThatThrownBy(() -> dictionaryPlantService.read(id))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("사전 식물이 존재하지 않습니다. id: " + id);
+    }
 
     @Test
     void 사전식물_검색시_검색명이_포함된_사전식물을_반환한다() {
         DictionaryPlant 스투키1 = dictionaryPlantSupport.builder().build();
         DictionaryPlant 스투키2 = dictionaryPlantSupport.builder().build();
 
-        DataResponse<List<DictionaryPlantSearchResponse>> searchResultsContainsParamName = dictionaryPlantService.search("스투");
+        DataResponse<List<DictionaryPlantSearchResponse>> searchResultsContainsParamName = dictionaryPlantService.search(
+                "스투");
 
         assertAll(
                 () -> assertThat(searchResultsContainsParamName.getData()).hasSize(2),
