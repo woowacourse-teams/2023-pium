cfb3d3b4dacc5aee90f1e000894a9af32b583616
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-07-19 18:29:02 +0900

    feat: 반려 식물 등록 입력 컴포넌트 생성 (number 생성)  (#80)
    * feat: FormInput에 필요한 파일 생성
    
    * design: font전역 상태 추가
    
    * feat: 기본적인 형태 구현
    
    * test: storybook 작성
    
    * design: FormInput 디자인 구현
    
    * feat: number input type 검사 함수 작성
    
    * chore: 전역으로 사용할 NUMBER 상수 생성
    
    * feat: number형 Input에만 사용될 hooks 구현
    
    * refactor: 범위 유효성 검사 추가 및 주석 작성
    
    * chore: number FormInput 스토리 작성
    
    * refactor: input handler 관련 코드 hooks로 분리
    
    * test: 앞서 적용된 hooks 적용
    
    * refactor: onChange사용 및 type='text'로 고정
    
    * refactor: setDate 조건에 따른 값 변경
    
    * refactor: input 초기 값 변경
    
    * design: input font-family 추가
    
    * test: type이 text이기 때문에 min,max Props제거
    
    * refactor: changeCallback params value로 설정
    
    ---------
    
    Co-authored-by: Q Kim <qoli0703@gmail.com>

---
+++ frontend/src/components/FormInput/FormInput.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import useNumberInput from 'hooks/useNumberInput';
+import { NUMBER } from 'constants/index';
+import FormInput from '.';
+
+const meta: Meta<typeof FormInput> = {
+  component: FormInput,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FormInput>;
+
+export const Default: Story = {
+  render: () => {
+    const changeCallback: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+      console.log(e.target.value);
+
+    return <FormInput onChange={changeCallback} />;
+  },
+};
+
+export const NumberInput: Story = {
+  render: () => {
+    const { MAX_CYCLE_DATE, MIN_CYCLE_DATE } = NUMBER;
+
+    const { numberValue, changeCallback, keyDownHandler } = useNumberInput({
+      maxRange: MAX_CYCLE_DATE,
+      minRange: MIN_CYCLE_DATE,
+    });
+
+    const nextCallback = () => console.log('clicked');
+
+    return (
+      <FormInput
+        inputMode="numeric"
+        value={numberValue}
+        onKeyDown={keyDownHandler}
+        nextCallback={nextCallback}
+        onChange={changeCallback}
+      />
+    );
+  },
+};
