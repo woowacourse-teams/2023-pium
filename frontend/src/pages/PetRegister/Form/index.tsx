3e49c5ac9f9c342db263e0af108e2765218274a0
bassyu <yrkdgus@gmail.com>
2023-07-20 00:56:32 +0900

    feat: API 적용 및 placeholder 추가

--- frontend/src/pages/PetRegister/Form/index.tsx
+++ frontend/src/pages/PetRegister/Form/index.tsx
@@ -17,6 +17,7 @@ import {
   Wrapper,
 } from './Form.style';
 import dictionaryPlantsAPI from 'apis/dictionaryPlants';
+import petPlantsAPI from 'apis/petPlants';
 import { usePetPlantForm } from './reducer';
 
 const PetRegisterForm = () => {
@@ -26,7 +27,7 @@ const PetRegisterForm = () => {
 
   const [dictionaryPlant, setDictionaryPlant] = useState<DictionaryPlant | null>(null);
 
-  const stackElementHeight = '80px';
+  const stackElementHeight = '96px';
 
   const setNickname = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: 'SET', key: 'nickname', value });
@@ -67,7 +68,7 @@ const PetRegisterForm = () => {
   };
 
   const submit = () => {
-    console.log(form);
+    petPlantsAPI.postForm(form).catch(console.log);
   };
 
   useEffect(() => {
@@ -75,9 +76,9 @@ const PetRegisterForm = () => {
       .getDetail(Number(dictionaryPlantId))
       .then((response) => {
         if (!response.ok) return;
-        response.json().then(setDictionaryPlant).catch();
+        response.json().then(setDictionaryPlant).catch(console.log);
       })
-      .catch();
+      .catch(console.log);
   }, []);
 
   const getStatus = (index: number) => (topIndex === index ? 'focus' : 'default');
@@ -89,7 +90,7 @@ const PetRegisterForm = () => {
         <DictionaryPlantImageArea>
           <Image size="160px" src={dictionaryPlant?.image} />
         </DictionaryPlantImageArea>
-        <Stack topIndex={topIndex}>
+        <Stack topIndex={8}>
           <Stack.Element height={stackElementHeight}>
             <FormInputBox title="별명이 뭔가요?" status={getStatus(0)}>
               <FormInput
@@ -122,8 +123,15 @@ const PetRegisterForm = () => {
             <FormInputBox title="어떤 화분에서 키우고 있나요?" status={getStatus(4)}>
               <Select
                 value={form.flowerpot}
-                options={['플라스틱', '유리', '캔']}
+                options={[
+                  '플라스틱/유리/캔',
+                  '물에 젖는 토분',
+                  '수경 재배',
+                  '행잉/목부작',
+                  '유약/고화도 토분',
+                ]}
                 onChange={setFlowerpot}
+                placeholder="화분의 종류를 선택해 주세요"
               />
             </FormInputBox>
           </Stack.Element>
@@ -131,19 +139,41 @@ const PetRegisterForm = () => {
             <FormInputBox title="화분의 위치는 어디인가요?" status={getStatus(5)}>
               <Select
                 value={form.location}
-                options={['거실', '창문 앞', '어두운 방안']}
+                options={['거실', '사무실', '욕실', '베란다', '방/원룸', '주방', '기타']}
                 onChange={setLocation}
+                placeholder="화분의 위치를 선택해 주세요"
               />
             </FormInputBox>
           </Stack.Element>
           <Stack.Element height={stackElementHeight}>
             <FormInputBox title="빛을 어떻게 받고 있나요?" status={getStatus(6)}>
-              <Select value={form.light} options={['기타등등']} onChange={setLight} />
+              <Select
+                value={form.light}
+                options={[
+                  '창문 밖에서 해를 받아요',
+                  '창문 안쪽에서 해를 받아요',
+                  '일반 조명 빛을 받아요',
+                  '식물용 조명 빛을 받아요',
+                  '해를 못 받아요',
+                ]}
+                onChange={setLight}
+                placeholder="채광을 선택해 주세요"
+              />
             </FormInputBox>
           </Stack.Element>
           <Stack.Element height={stackElementHeight}>
             <FormInputBox title="바람은 얼마나 통하나요?" status={getStatus(7)}>
-              <Select value={form.wind} options={['기타등등']} onChange={setWind} />
+              <Select
+                value={form.wind}
+                options={[
+                  '5m 내 창문이 있어요',
+                  '5m 보다 멀리 창문이 있어요',
+                  '창문이 없지만 바람이 통해요',
+                  '바람이 안통해요',
+                ]}
+                onChange={setWind}
+                placeholder="통풍을 선택해 주세요"
+              />
             </FormInputBox>
           </Stack.Element>
         </Stack>
