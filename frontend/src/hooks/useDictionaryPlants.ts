0968a49183adc464dff9100dc213fa471f83b30f
hozzijeong <ghwls0905@naver.com>
2023-07-19 15:28:33 +0900

    refactor: dict option에 맞춰 데이터 변환

--- frontend/src/hooks/useDictionaryPlants.ts
+++ frontend/src/hooks/useDictionaryPlants.ts
@@ -1,9 +1,19 @@
 import { DictionaryPlant } from 'types/api/dictionary';
+import { Season, SeasonKor } from 'types/plants';
 import { useEffect, useState } from 'react';
 import dictAPI from 'apis/dict';
+import { seasonConverter } from 'utils/plants';
+
+const initialSeasonInfo: Record<SeasonKor, string> = {
+  봄: '',
+  여름: '',
+  가을: '',
+  겨울: '',
+};
 
 const useDictionaryPlants = (id: string | undefined) => {
   const [dictionary, setDictionary] = useState<DictionaryPlant | null>(null);
+  const [waterOption, setWaterOption] = useState<Record<SeasonKor, string>>(initialSeasonInfo);
 
   useEffect(() => {
     getDetail();
@@ -16,14 +26,22 @@ const useDictionaryPlants = (id: string | undefined) => {
 
       if (!response.ok) throw new Error('무엇인가 잘못됐습니다.');
 
-      const data = await response.json();
+      const data = (await response.json()) as DictionaryPlant;
+
+      const seasonOptions = [...Object.entries(data.waterCycle)].reduce((prev, cur) => {
+        const [season, data] = cur as [Season, string];
+        const key = seasonConverter(season);
+        return { ...prev, [key]: data };
+      }, waterOption);
+
+      setWaterOption(seasonOptions);
       setDictionary(data);
     } catch (e) {
       console.log(e);
     }
   };
 
-  return { dictionary };
+  return { dictionary, waterOption };
 };
 
 export default useDictionaryPlants;
