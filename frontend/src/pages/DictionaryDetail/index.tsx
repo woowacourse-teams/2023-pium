c6d1692a1c9cf60c2971355830d27f0463934304
hozzijeong <ghwls0905@naver.com>
2023-07-19 15:27:59 +0900

    feat: dictionary 페이지 구현

--- frontend/src/pages/DictionaryDetail/index.tsx
+++ frontend/src/pages/DictionaryDetail/index.tsx
@@ -1,21 +1,111 @@
-import { Wrapper } from './DictionaryDetail.style';
+import DictInfo from 'components/DictInfo';
+import DictInfoSwitch from 'components/DictInfoSwitch';
+import * as S from './DictionaryDetail.style';
 import useDictionaryPlants from 'hooks/useDictionaryPlants';
 
 const DictionaryDetail = () => {
   // const { id } = useParams();
-  const { dictionary } = useDictionaryPlants('1');
+  const { dictionary, waterOption } = useDictionaryPlants('1');
+
+  if (!dictionary) {
+    return <div>...loading</div>;
+  }
+
+  const {
+    postingPlace,
+    familyName,
+    name,
+    image,
+    manageLevel,
+    growSpeed,
+    requireHumidity,
+    requireTemp,
+    minimumTemp,
+    smell,
+    poison,
+    specialManageInfo,
+  } = dictionary;
+
+  const place = postingPlace.map((position, idx) => (
+    <DictInfo.Content key={idx}>{position}</DictInfo.Content>
+  ));
 
   return (
-    <Wrapper>
-      <section>
+    <S.Wrapper>
+      <S.HeaderBox>
         <div>
-          <p>{dictionary?.familyName}</p>
-          <p>{dictionary?.name}</p>
+          <S.FamilyName>{familyName}</S.FamilyName>
+          <S.Name>{name}</S.Name>
         </div>
-        <img src={dictionary?.image} alt={dictionary?.name} />
-      </section>
-      <section></section>
-    </Wrapper>
+        <S.PlantImage src={image} alt={name} />
+      </S.HeaderBox>
+      <S.ContentBox>
+        <S.LevelBox>
+          <DictInfo alignment="row">
+            <DictInfo.Title>난이도</DictInfo.Title>
+            <DictInfo.Content>{manageLevel}</DictInfo.Content>
+          </DictInfo>
+        </S.LevelBox>
+        <S.CycleBox>
+          <DictInfoSwitch title="물 주기" optionMap={waterOption}></DictInfoSwitch>
+        </S.CycleBox>
+        <S.LocationBox>
+          <DictInfo>
+            <DictInfo.Title>추천 장소</DictInfo.Title>
+            {place}
+          </DictInfo>
+        </S.LocationBox>
+
+        <S.PropsBox>
+          {growSpeed !== '' && (
+            <DictInfo>
+              <DictInfo.Title>생장 속도</DictInfo.Title>
+              <DictInfo.Content>{growSpeed}</DictInfo.Content>
+            </DictInfo>
+          )}
+
+          {requireHumidity !== '' && (
+            <DictInfo>
+              <DictInfo.Title>권장 습도</DictInfo.Title>
+              <DictInfo.Content>{requireHumidity}</DictInfo.Content>
+            </DictInfo>
+          )}
+
+          {requireTemp !== '' && (
+            <DictInfo>
+              <DictInfo.Title>권장 온도</DictInfo.Title>
+              <DictInfo.Content>{requireTemp}</DictInfo.Content>
+            </DictInfo>
+          )}
+
+          {minimumTemp !== '' && (
+            <DictInfo>
+              <DictInfo.Title>겨울 최저 온도</DictInfo.Title>
+              <DictInfo.Content>{minimumTemp}</DictInfo.Content>
+            </DictInfo>
+          )}
+
+          {smell !== '' && (
+            <DictInfo>
+              <DictInfo.Title>냄새</DictInfo.Title>
+              <DictInfo.Content>{smell}</DictInfo.Content>
+            </DictInfo>
+          )}
+
+          {poison !== '' && (
+            <DictInfo>
+              <DictInfo.Title>독성</DictInfo.Title>
+              <DictInfo.Content>{poison}</DictInfo.Content>
+            </DictInfo>
+          )}
+        </S.PropsBox>
+
+        <S.ManageInfoBox>
+          <p>특별 관리 정보</p>
+          <span>{specialManageInfo}</span>
+        </S.ManageInfoBox>
+      </S.ContentBox>
+    </S.Wrapper>
   );
 };
 
