88da4a1b1acbbc8ce08c5af2736a04d0f1ceece5
hozzijeong <ghwls0905@naver.com>
2023-07-19 13:51:23 +0900

    chore: storybook handler 오타 수정

--- frontend/src/mocks/storybookHandlers.ts
+++ frontend/src/mocks/storybookHandlers.ts
@@ -4,16 +4,16 @@ import DICTIONARY_PLANT_DATA from './data/dictionaryPlant';
 import SEARCH_DATA from './data/search';
 
 export const storybookHandlers = [
-  rest.get('/search', (req, res, ctx) => {
+  rest.get('/dictionary-plants?name=:query', (req, res, ctx) => {
     const target = req.url.searchParams.get('name') ?? '';
     const searchResult = SEARCH_DATA.filter(({ name }) => name.includes(target));
 
     return res(ctx.status(200), ctx.json({ data: searchResult }));
   }),
 
-  rest.get('dictionary-plnats/:id', (req, res, ctx) =>
-    res(ctx.status(200), ctx.json(DICTIONARY_PLANT_DATA))
-  ),
+  rest.get('/dictionary-plants/:id', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(DICTIONARY_PLANT_DATA));
+  }),
 
   rest.post<NewPetPlantRequest>('/pet-plants', async (req, res, ctx) => res(ctx.status(201))),
 ];
