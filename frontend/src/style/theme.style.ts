9ed0cc172fc7491808f15ac27fadd48c2cc87a1e
hozzijeong <ghwls0905@naver.com>
2023-07-19 11:23:31 +0900

    design: input font-family 추가

--- frontend/src/style/theme.style.ts
+++ frontend/src/style/theme.style.ts
@@ -11,7 +11,7 @@ const font = {
   emphasize: '',
   subTitle: '',
   title: 'normal 700 3.2rem/4rem NanumSquareRound',
-  input: 'normal 500 1.8rem/2.2rem',
+  input: 'normal 500 1.8rem/2.2rem NanumSquareRound',
 };
 
 const width = {
