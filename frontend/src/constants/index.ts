94f3b433f43027273b90044a51689c1325fb5262
hozzijeong <ghwls0905@naver.com>
2023-07-19 10:46:44 +0900

    refactor: Object.freeze -> as const 로 변환 및 함수형 제거

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,26 +1,25 @@
-export const MESSAGE = Object.freeze({
+export const MESSAGE = {
   noSearchResult: '아직 사전에 등록된 식물이 없어요 😅',
-});
+} as const;
 
-export const ERROR = Object.freeze({
+export const ERROR = {
   radioContext: '컴포넌트가 RadioProvider의 자손이 아닙니다!',
-  stackContext: '컴포넌트가 StackProvider의 자손이 아닙니다!',
-});
+} as const;
 
-export const GUIDE = Object.freeze({
+export const GUIDE = {
   search: '피움에 등록된 식물을 검색해 보세요!',
-});
+} as const;
 
-export const API_PATH = Object.freeze({
+export const API_PATH = {
   SEARCH: '/search',
-  DICT: (plantId?: string) => `/dictionary-plants/${plantId ?? ':id'}`,
-  PET_LIST: '/pet-plants',
-  PET_DETAIL: (plantId?: string) => `/pet-plants/${plantId ?? ':id'}`,
-});
+  DICT: `/dictionary-plants/:id`,
+  PET: '/pet-plants',
+  PET_DETAIL: `/pet-plants/:id`,
+} as const;
 
-export const URL_PATH = Object.freeze({
+export const URL_PATH = {
   MAIN: '/',
-  DICT: (plantId?: string) => `/dict/${plantId ?? ':id'}`,
+  DICT: `/dict/:id`,
   PET_LIST: '/pet',
-  PET_DETAIL: (plantId?: string) => `/pet/${plantId ?? ':id'}`,
-});
+  PET_DETAIL: `/pet/:id`,
+} as const;
