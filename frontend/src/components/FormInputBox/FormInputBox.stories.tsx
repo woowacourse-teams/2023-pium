4843c57e329a776968fe3b6ff000041fb08ddfa1
Q Kim <qoli0703@gmail.com>
2023-07-19 19:51:11 +0900

    fix: 코드 오류 수정

--- frontend/src/components/FormInputBox/FormInputBox.stories.tsx
+++ frontend/src/components/FormInputBox/FormInputBox.stories.tsx
@@ -42,7 +42,7 @@ export const ChildrenDateInput: Story = {
   render: ({ ...args }) => {
     return (
       <FormInputBox {...args}>
-        <DateInput initialValue={getToday()} />
+        <DateInput value={getToday()} />
       </FormInputBox>
     );
   },
