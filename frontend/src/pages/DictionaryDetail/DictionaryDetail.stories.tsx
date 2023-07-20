6c8ce8babbd90fc0a76c5dcbc4854074683eea37
hozzijeong <ghwls0905@naver.com>
2023-07-20 15:14:07 +0900

    chore: storybook router 설정

--- frontend/src/pages/DictionaryDetail/DictionaryDetail.stories.tsx
+++ frontend/src/pages/DictionaryDetail/DictionaryDetail.stories.tsx
@@ -1,28 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Link, MemoryRouter } from 'react-router-dom';
 import DictionaryDetail from '.';
 
 const meta: Meta<typeof DictionaryDetail> = {
   component: DictionaryDetail,
-  decorators: [
-    (Story) => (
-      <MemoryRouter>
-        <Story />
-      </MemoryRouter>
-    ),
-  ],
+  parameters: {
+    reactRouter: {
+      routePath: '/dict/:id',
+      routeParams: { id: '1' },
+    },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof DictionaryDetail>;
 
-export const Default: Story = {
-  render: () => {
-    return (
-      <div>
-        <Link to="/dict/1">이동</Link>
-      </div>
-    );
-  },
-};
+export const Default: Story = {};
