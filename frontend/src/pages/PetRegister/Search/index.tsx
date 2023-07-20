670f8f5b762ba33b3f91865e6105fa63a94400d5
bassyu <yrkdgus@gmail.com>
2023-07-20 18:25:53 +0900

    refactor: generatePath 적용

--- frontend/src/pages/PetRegister/Search/index.tsx
+++ frontend/src/pages/PetRegister/Search/index.tsx
@@ -1,19 +1,20 @@
-import { useNavigate } from 'react-router-dom';
+import { generatePath, useNavigate } from 'react-router-dom';
 import SearchBox from 'components/SearchBox';
 import { Message, SearchBoxArea, Wrapper } from './Search.style';
+import { URL_PATH } from 'constants/index';
 
 const PetRegisterSearch = () => {
   const navigate = useNavigate();
 
-  const navigateRegister = (id: number) => {
-    navigate(`/pet/register/${id}`);
+  const navigateForm = (id: number) => {
+    navigate(generatePath(URL_PATH.PET_REGISTER_FORM, { id: String(id) }));
   };
 
   return (
     <Wrapper>
       <Message>어떤 식물을 키우시나요?</Message>
       <SearchBoxArea>
-        <SearchBox onSelect={navigateRegister} />
+        <SearchBox onSelect={navigateForm} />
       </SearchBoxArea>
     </Wrapper>
   );
