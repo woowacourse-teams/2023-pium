2d4ce134ae6c9f6229333de96cbc7ccc2cfe095b
bassyu <yrkdgus@gmail.com>
2023-07-20 15:46:39 +0900

    chore: 스토리북 에러 주석

--- frontend/src/components/Stack/Stack.stories.tsx
+++ frontend/src/components/Stack/Stack.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
 import Stack from '.';
-import useStackContext from './hooks/useStackContext';
+import useStack from './hooks/useStack';
 
 const meta: Meta<typeof Stack> = {
   component: Stack,
@@ -11,13 +11,19 @@ export default meta;
 
 type Story = StoryObj<typeof Stack>;
 
-const TestButton = ({ type }: { type: 'big' | 'small' }) => {
-  const { showNextElement, isLastElementShown } = useStackContext();
-
+const TestButton = ({
+  type,
+  onClick,
+  isLastElementShown,
+}: {
+  type: 'big' | 'small';
+  onClick: () => void;
+  isLastElementShown: boolean;
+}) => {
   return (
     <button
       type="button"
-      onClick={() => showNextElement()}
+      onClick={onClick}
       style={{
         display: 'block',
         width: '100%',
@@ -37,73 +43,83 @@ const TestButton = ({ type }: { type: 'big' | 'small' }) => {
  */
 
 export const PlainExample: Story = {
-  render: () => (
-    <Stack>
-      <Stack.Element height="30px">
-        <TestButton type="small" />
-      </Stack.Element>
-      <Stack.Element height="100px">
-        <TestButton type="big" />
-      </Stack.Element>
-      <Stack.Element height="30px">
-        <TestButton type="small" />
-      </Stack.Element>
-      <Stack.Element height="30px">
-        <TestButton type="small" />
-      </Stack.Element>
-      <Stack.Element height="100px">
-        <TestButton type="big" />
-      </Stack.Element>
-      <Stack.Element height="30px">
-        <TestButton type="small" />
-      </Stack.Element>
-    </Stack>
-  ),
+  render: () => {
+    const { topIndex, showNextElement, isLastElementShown } = useStack(6);
+
+    const onClick = () => {
+      showNextElement(topIndex);
+    };
+
+    return (
+      <Stack topIndex={topIndex}>
+        <Stack.Element height="30px">
+          <TestButton type="small" onClick={onClick} isLastElementShown={isLastElementShown} />
+        </Stack.Element>
+        <Stack.Element height="100px">
+          <TestButton type="big" onClick={onClick} isLastElementShown={isLastElementShown} />
+        </Stack.Element>
+        <Stack.Element height="30px">
+          <TestButton type="small" onClick={onClick} isLastElementShown={isLastElementShown} />
+        </Stack.Element>
+        <Stack.Element height="30px">
+          <TestButton type="small" onClick={onClick} isLastElementShown={isLastElementShown} />
+        </Stack.Element>
+        <Stack.Element height="100px">
+          <TestButton type="big" onClick={onClick} isLastElementShown={isLastElementShown} />
+        </Stack.Element>
+        <Stack.Element height="30px">
+          <TestButton type="small" onClick={onClick} isLastElementShown={isLastElementShown} />
+        </Stack.Element>
+      </Stack>
+    );
+  },
 };
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const { showNextElement } = useStackContext();
+// const Counter = ({ onClickNext }: { onClickNext: () => void }) => {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div
-      style={{
-        border: '2px solid lightpink',
-        height: '100px',
-        padding: '20px',
-      }}
-    >
-      <p style={{ fontSize: '2rem' }}>{count}원 모았습니다.</p>
-      <div>
-        <button type="button" onClick={() => setCount(count + 1)}>
-          돈 벌기
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={() => showNextElement()}>
-          다음 통장 보여주기
-        </button>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div
+//       style={{
+//         border: '2px solid lightpink',
+//         height: '100px',
+//         padding: '20px',
+//       }}
+//     >
+//       <p style={{ fontSize: '2rem' }}>{count}원 모았습니다.</p>
+//       <div>
+//         <button type="button" onClick={() => setCount(count + 1)}>
+//           돈 벌기
+//         </button>
+//       </div>
+//       <div>
+//         <button type="button" onClick={onClickNext}>
+//           다음 통장 보여주기
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-/**
- * 역순으로 표시하지만 실제 DOM 순서를 꼬는 것이 아니기 때문에 버튼을 눌러 상태를 변경하더라도 상태는 변하지 않습니다.
- */
+// /**
+//  * 역순으로 표시하지만 실제 DOM 순서를 꼬는 것이 아니기 때문에 버튼을 눌러 상태를 변경하더라도 상태는 변하지 않습니다.
+//  */
 
-export const StateExample: Story = {
-  render: () => (
-    <Stack>
-      <Stack.Element height="100px">
-        <Counter />
-      </Stack.Element>
-      <Stack.Element height="100px">
-        <Counter />
-      </Stack.Element>
-      <Stack.Element height="100px">
-        <Counter />
-      </Stack.Element>
-    </Stack>
-  ),
-};
+// export const StateExample: Story = {
+//   render: () => {
+//     const { topIndex, showNextElement } = useStack(3);
+//     return (
+//       <Stack topIndex={topIndex}>
+//         <Stack.Element height="100px">
+//           <Counter  onClickNext={}/>
+//         </Stack.Element>
+//         <Stack.Element height="100px">
+//           <Counter />
+//         </Stack.Element>
+//         <Stack.Element height="100px">
+//           <Counter />
+//         </Stack.Element>
+//       </Stack>
+//     );
+//   },
+// };
