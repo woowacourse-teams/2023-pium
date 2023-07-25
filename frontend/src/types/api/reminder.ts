a04cdaa9ec4e0d92511896915f951f67602b44b9
hozzijeong <ghwls0905@naver.com>
2023-07-25 20:12:24 +0900

    feat: reminder데이터 UI에 활용하기 좋게 mapping 타입 선언

--- frontend/src/types/api/reminder.ts
+++ frontend/src/types/api/reminder.ts
@@ -1,3 +1,19 @@
+export type TodayStatus = 'late' | 'exist' | 'none'; // 오늘 할 일에 대한 상태
+
+export type Month =
+  | '01'
+  | '02'
+  | '03'
+  | '04'
+  | '05'
+  | '06'
+  | '07'
+  | '08'
+  | '09'
+  | '10'
+  | '11'
+  | '12';
+
 export interface Reminder {
   petPlantId: number;
   image: string;
@@ -5,4 +21,18 @@ export interface Reminder {
   dictionaryPlantName: string;
   nextWaterDay: number; // 음수면 지각 | 0이면 오늘 | 양수면 미래
   nextWaterDate: string;
+}
+
+export type MonthKeyReminderType = {
+  [key in Month]?: ReminderExtendType[];
+};
+
+export interface ReminderExtendType extends Reminder {
+  date: string;
+  status: TodayStatus;
+}
+
+export interface ConvertReminderData {
+  data: MonthKeyReminderType;
+  status: TodayStatus;
 }
