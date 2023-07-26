36d14d877ca71af51e70f039ccf29b63681660a7
hozzijeong <ghwls0905@naver.com>
2023-07-26 09:57:50 +0900

    refactor: data 값 변경

--- frontend/src/mocks/data/reminder.ts
+++ frontend/src/mocks/data/reminder.ts
@@ -5,7 +5,7 @@ const REMINDER_DATA = {
       image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
       nickName: '참새나무',
       dictionaryPlantName: '알로카시아',
-      nextWaterDay: 3,
+      nextWaterDay: -1,
       nextWaterDate: '2023-07-07',
     },
     {
@@ -13,7 +13,7 @@ const REMINDER_DATA = {
       image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
       nickName: '그레이나무',
       dictionaryPlantName: '스투키',
-      nextWaterDay: 10,
+      nextWaterDay: 8,
       nextWaterDate: '2023-07-16',
     },
   ],
