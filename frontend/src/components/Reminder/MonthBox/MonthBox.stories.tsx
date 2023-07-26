4119e8908716e0c093c33d4e8af43163e531e7ef
hozzijeong <ghwls0905@naver.com>
2023-07-26 21:29:08 +0900

    test: MonthBox 스토리 작성

---
+++ frontend/src/components/Reminder/MonthBox/MonthBox.stories.tsx
@@ -0,0 +1,80 @@
+import { ReminderExtendType } from 'types/api/reminder';
+import type { Meta, StoryObj } from '@storybook/react';
+import MonthBox from '.';
+
+const meta: Meta<typeof MonthBox> = {
+  component: MonthBox,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MonthBox>;
+
+export const NotHasDate: Story = {
+  render: () => {
+    const mockData: ReminderExtendType[] = [
+      {
+        petPlantId: 1,
+        image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
+        nickName: '참새나무',
+        dictionaryPlantName: '알로카시아',
+        dDay: 0,
+        nextWaterDate: '2023-07-07',
+        date: '07',
+        status: 'late',
+      },
+      {
+        petPlantId: 6,
+        image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
+        nickName: '쵸파 나무',
+        dictionaryPlantName: '스투키',
+        dDay: 0,
+        nextWaterDate: '2023-07-26',
+        date: '26',
+        status: 'exist',
+      },
+      {
+        petPlantId: 7,
+        image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
+        nickName: '클린 나무',
+        dictionaryPlantName: '스투키',
+        dDay: 0,
+        nextWaterDate: '2023-07-26',
+        date: '26',
+        status: 'exist',
+      },
+      {
+        petPlantId: 8,
+        image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
+        nickName: '피움 나무',
+        dictionaryPlantName: '스투키',
+        dDay: 0,
+        nextWaterDate: '2023-07-26',
+        date: '26',
+        status: 'exist',
+      },
+      {
+        petPlantId: 9,
+        image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
+        nickName: '포비 나무',
+        dictionaryPlantName: '스투키',
+        dDay: -3,
+        nextWaterDate: '2023-07-29',
+        date: '29',
+        status: 'none',
+      },
+      {
+        petPlantId: 10,
+        image: 'https://images.unsplash.com/photo-1598983062491-5934ce558814',
+        nickName: '크론 나무',
+        dictionaryPlantName: '스투키',
+        dDay: -3,
+        nextWaterDate: '2023-07-29',
+        date: '29',
+        status: 'none',
+      },
+    ];
+
+    return <MonthBox reminderDates={mockData} month="7" />;
+  },
+};
--- frontend/src/components/Reminder/MonthBox/index.tsx
+++ frontend/src/components/Reminder/MonthBox/index.tsx
@@ -11,12 +11,12 @@ const MonthBox = ({ month, reminderDates }: MonthBoxProps) => {
   const dayMap = new Map();
 
   const cardBoxes = reminderDates.map((info) => {
-    const hasDate = dayMap.has(info.date);
-    if (!hasDate) dayMap.set(info.date, true);
+    const notDate = dayMap.has(info.date);
+    if (!notDate) dayMap.set(info.date, true);
 
     const id = info.petPlantId.toString();
 
-    return <CardBox key={id} hasDate={hasDate} data={info} />;
+    return <CardBox key={id} notDate={notDate} data={info} />;
   });
 
   return (
