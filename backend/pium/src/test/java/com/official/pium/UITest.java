b7e66265e4314949cfa6316290e836473109fc7f
최준호 <junho5336@gmail.com>
2023-07-27 15:22:46 +0900

    test: 헤더 인증방식 적용에 따라 Mocking 방식 수정

--- backend/pium/src/test/java/com/official/pium/UITest.java
+++ backend/pium/src/test/java/com/official/pium/UITest.java
@@ -1,15 +1,16 @@
 package com.official.pium;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-
 import com.official.pium.config.WebMvcConfigure;
 import com.official.pium.domain.Member;
 import com.official.pium.repository.MemberRepository;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @Import(value = {WebMvcConfigure.class})
 public class UITest {
@@ -19,9 +20,11 @@ public class UITest {
 
     @BeforeEach
     void setUp() {
-        given(memberRepository.findByEmail(anyString()))
-                .willReturn(Optional.of(Member.builder()
-                        .email("pium@gmail.com")
-                        .build()));
+        given(memberRepository.findByEmail(any()))
+                .willReturn(
+                        Optional.of(Member.builder()
+                                .email("pium@gmail.com")
+                                .build())
+                );
     }
 }
