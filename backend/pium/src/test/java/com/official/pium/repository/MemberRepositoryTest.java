8c4e924bfd0bb6b702cb9c8546bf9a69e7b1dc5d
최준호 <junho5336@gmail.com>
2023-07-27 15:20:22 +0900

    fix: JPA Auditing 미적용 이슈 해결
    - JPA Auditing이 적용되지 않아 테스트 수행 시 데이터가 Null 검증에 걸렸음
    - Auditing 설정을 추가하여 해당 문제를 해결

--- backend/pium/src/test/java/com/official/pium/repository/MemberRepositoryTest.java
+++ backend/pium/src/test/java/com/official/pium/repository/MemberRepositoryTest.java
@@ -1,18 +1,24 @@
 package com.official.pium.repository;
 
+import com.official.pium.config.JpaAuditingConfig;
 import com.official.pium.domain.Member;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@DataJpaTest
+@DataJpaTest(includeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = JpaAuditingConfig.class
+))
 class MemberRepositoryTest {
 
     @Autowired
@@ -20,7 +26,9 @@ class MemberRepositoryTest {
 
     @Test
     void 사용자_저장() {
-        Member member = Member.builder().email("hello@aaa.com").build();
+        Member member = Member.builder()
+                .email("hello@aaa.com")
+                .build();
 
         Member save = memberRepository.save(member);
 
