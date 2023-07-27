1614c8f071b3b4b672e05e581b29a054d624bc7d
최준호 <junho5336@gmail.com>
2023-07-27 15:29:07 +0900

    refactor: DataJpaTest RepositoryTest로 상속

--- backend/pium/src/test/java/com/official/pium/repository/MemberRepositoryTest.java
+++ backend/pium/src/test/java/com/official/pium/repository/MemberRepositoryTest.java
@@ -1,25 +1,18 @@
 package com.official.pium.repository;
 
-import com.official.pium.config.JpaAuditingConfig;
+import com.official.pium.RepositoryTest;
 import com.official.pium.domain.Member;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@DataJpaTest(includeFilters = @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        classes = JpaAuditingConfig.class
-))
-class MemberRepositoryTest {
+class MemberRepositoryTest extends RepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
