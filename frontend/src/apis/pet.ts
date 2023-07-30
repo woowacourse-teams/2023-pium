0c19bb08bcb20e8513cf827473e4480f30816929
bassyu <yrkdgus@gmail.com>
2023-07-30 14:17:45 +0900

    feat: getList API 추가

--- frontend/src/apis/pet.ts
+++ frontend/src/apis/pet.ts
@@ -1,7 +1,14 @@
-import { NewPetPlantRequest } from 'types/api/petPlant';
+import type { NewPetPlantRequest } from 'types/api/petPlant';
 import { BASE_URL } from 'constants/index';
 
 export const PET = `${BASE_URL}/pet-plants`;
+
+const getList = () => {
+  return fetch(PET, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+};
 
 const postForm = (form: NewPetPlantRequest) => {
   return fetch(PET, {
@@ -12,6 +19,7 @@ const postForm = (form: NewPetPlantRequest) => {
 };
 
 const PetAPI = {
+  getList,
   postForm,
 };
 
