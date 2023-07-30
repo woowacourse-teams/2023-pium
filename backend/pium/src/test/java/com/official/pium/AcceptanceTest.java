759aac26734a34cbf92e19b3c1c6d07dff4a3982
최준호 <junho5336@gmail.com>
2023-07-31 00:01:45 +0900

    feat: DB 초기화 구문 모듈화

--- backend/pium/src/test/java/com/official/pium/AcceptanceTest.java
+++ backend/pium/src/test/java/com/official/pium/AcceptanceTest.java
@@ -1,10 +1,13 @@
 package com.official.pium;
 
+import com.official.pium.config.DatabaseClearExtension;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+@ExtendWith(DatabaseClearExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
 
