b9c2b62cc100c46b385918740e77086a78a3cb30
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-07-31 14:44:05 +0900

    refactor: 테스트 support에서 사용하던 fixture 제거

--- backend/pium/src/test/java/com/official/pium/support/PetPlantSupport.java
+++ backend/pium/src/test/java/com/official/pium/support/PetPlantSupport.java
@@ -3,20 +3,16 @@ package com.official.pium.support;
 import com.official.pium.domain.DictionaryPlant;
 import com.official.pium.domain.Member;
 import com.official.pium.domain.PetPlant;
+import com.official.pium.domain.WaterCycle;
 import com.official.pium.repository.DictionaryPlantRepository;
 import com.official.pium.repository.MemberRepository;
 import com.official.pium.repository.PetPlantRepository;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-
-import static com.official.pium.fixture.DictionaryPlantFixture.스투키;
-import static com.official.pium.fixture.MemberFixture.주노;
-
 @Component
 @RequiredArgsConstructor
-@SuppressWarnings("NonAsciiCharacters")
 public class PetPlantSupport {
 
     private final MemberRepository memberRepository;
@@ -45,8 +41,8 @@ public class PetPlantSupport {
         public PetPlant build() {
             return petPlantRepository.save(
                     PetPlant.builder()
-                            .dictionaryPlant(dictionaryPlant == null ? dictionaryPlantRepository.save(스투키) : dictionaryPlant)
-                            .member(member == null ? memberRepository.save(주노) : member)
+                            .dictionaryPlant(dictionaryPlant == null ? dictionaryPlantRepository.save(generateDictionaryPlant()) : dictionaryPlant)
+                            .member(member == null ? memberRepository.save(generateMember()) : member)
                             .nickname("testNickName")
                             .imageUrl("testImageUrl")
                             .location("testLocation")
@@ -59,6 +55,36 @@ public class PetPlantSupport {
                             .waterCycle(3)
                             .build()
             );
+        }
+
+        private DictionaryPlant generateDictionaryPlant() {
+            return DictionaryPlant.builder()
+                    .name("라벤다")
+                    .imageUrl("https://www.costco.co.kr/medias/sys_master/images/hd6/h37/31058517229598.jpg")
+                    .familyName("멋진 라벤더")
+                    .smell("안남")
+                    .poison("없음")
+                    .manageLevel("쉬움")
+                    .growSpeed("빨리자람")
+                    .requireTemp("14도")
+                    .minimumTemp("0도")
+                    .requireHumidity("15%")
+                    .postingPlace("베란다 앞")
+                    .specialManageInfo("물을 많이 주지않아도 됩니다.")
+                    .waterCycle(
+                            WaterCycle.builder()
+                                    .spring("겉흙이 마르면 촉촉하게")
+                                    .summer("겉흙이 마르면 촉촉하게")
+                                    .autumn("겉흙이 마르면 촉촉하게")
+                                    .winter("겉흙이 마르면 촉촉하게")
+                                    .build()
+                    ).build();
+        }
+
+        private Member generateMember() {
+            return Member.builder()
+                    .email("pium@gmail.com")
+                    .build();
         }
     }
 }
