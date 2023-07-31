9c4c2ca524ebd32a19b9d9ea1926be5e0a792d9f
bassyu <yrkdgus@gmail.com>
2023-07-31 15:50:05 +0900

    refactor: 핸들러 대신 Link 태그 사용

--- frontend/src/pages/PetList/index.tsx
+++ frontend/src/pages/PetList/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, generatePath } from 'react-router-dom';
 import Navbar from 'components/@common/Navbar';
 import PetCard from 'components/PetCard';
 import { PetCardList, RegisterButton, Title, Wrapper } from './PetList.style';
@@ -6,25 +6,20 @@ import usePetList from 'hooks/queries/pet/usePetList';
 import { URL_PATH } from 'constants/index';
 
 const PetList = () => {
-  const navigate = useNavigate();
   const { data: petList } = usePetList();
-
-  const goRegister = () => {
-    navigate(URL_PATH.petRegisterSearch);
-  };
-
-  const goPetDetail = (id: number) => {
-    navigate(`/pet/${id}`);
-  };
 
   return (
     <>
       <Wrapper>
         <Title>나의 식물 카드</Title>
         <PetCardList>
-          <RegisterButton onClick={goRegister}>+</RegisterButton>
+          <Link to={URL_PATH.petRegisterSearch}>
+            <RegisterButton type="button">+</RegisterButton>
+          </Link>
           {petList?.map((pet) => (
-            <PetCard key={pet.id} pet={pet} onClick={goPetDetail} />
+            <Link key={pet.id} to={generatePath(URL_PATH.petDetail, { id: String(pet.id) })}>
+              <PetCard pet={pet} />
+            </Link>
           ))}
         </PetCardList>
       </Wrapper>
