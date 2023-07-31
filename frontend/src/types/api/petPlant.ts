1af221d9f8ab4d918f957d8cdd13c1fcc1f7486a
bassyu <yrkdgus@gmail.com>
2023-07-31 17:09:00 +0900

    refactor: PetDetails를 가장 기본이 되는 타입으로 리팩터링

--- frontend/src/types/api/petPlant.ts
+++ frontend/src/types/api/petPlant.ts
@@ -1,40 +1,34 @@
 import type { DataResponse } from './DataResponse';
 import type { DictionaryPlant } from './dictionary';
 
-export interface Pet {
+export interface PetDetails {
   id: number;
   nickname: string;
   imageUrl: string;
-  dictionaryPlantName: string;
-  birthDate: string;
-  daySince: number;
-}
-
-export type PetListResponse = DataResponse<Pet[]>;
-
-export interface NewPetPlantRequest {
-  dictionaryPlantId: DictionaryPlant['id'];
-  nickname: string;
-  birthDate: string;
-
-  waterCycle: number;
-  lastWaterDate: string;
-
+  dictionaryPlant: Pick<DictionaryPlant, 'id' | 'name'>;
   location: string;
   flowerpot: string;
   light: string;
   wind: string;
-}
-
-export interface PetPlantDetails extends Omit<NewPetPlantRequest, 'dictionaryPlantId'> {
-  id: number;
-  imageUrl: string;
-  dictionaryPlant: {
-    id: DictionaryPlant['id'];
-    name: DictionaryPlant['name'];
-  };
-
+  birthDate: string;
+  lastWaterDate: string;
+  waterCycle: number;
   dDay: number;
   daySince: number;
   nextWaterDate: string;
 }
+
+export interface NewPetRequest
+  extends Pick<
+    PetDetails,
+    'nickname' | 'birthDate' | 'waterCycle' | 'location' | 'flowerpot' | 'light' | 'wind'
+  > {
+  dictionaryPlantId: DictionaryPlant['id'];
+}
+
+export interface PetCard
+  extends Pick<PetDetails, 'id' | 'nickname' | 'imageUrl' | 'birthDate' | 'daySince'> {
+  dictionaryPlantName: DictionaryPlant['name'];
+}
+
+export type PetCardListResponse = DataResponse<PetCard[]>;
