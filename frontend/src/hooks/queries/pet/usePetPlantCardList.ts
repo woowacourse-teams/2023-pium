cb7f9c4a0fcfd35b4ec3daae3ea1823fb992e95e
bassyu <yrkdgus@gmail.com>
2023-07-31 20:01:07 +0900

    refactor: Type 네이밍 변경 적용

--- frontend/src/hooks/queries/pet/usePetPlantCardList.ts
+++ frontend/src/hooks/queries/pet/usePetPlantCardList.ts
@@ -1,9 +1,9 @@
-import type { PetPlantCard, PetPlantCardListResponse } from 'types/api/petPlant';
+import type { PetPlant, PetPlantListResponse } from 'types/api/petPlant';
 import { useQuery } from '@tanstack/react-query';
 import PetAPI, { PET } from 'apis/pet';
 
 const usePetPlantCardList = () =>
-  useQuery<PetPlantCardListResponse, Error, PetPlantCard[]>({
+  useQuery<PetPlantListResponse, Error, PetPlant[]>({
     queryKey: [PET, 'list'],
     queryFn: async () => {
       const response = await PetAPI.getList();
