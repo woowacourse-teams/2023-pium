529a350f9d9514e9c97f5b4d74fa79eda637c603
hozzijeong <ghwls0905@naver.com>
2023-08-01 14:31:25 +0900

    refactor: 타입 재활용 및 중복 코드 삭제

--- frontend/src/mocks/storage/Reminder.ts
+++ frontend/src/mocks/storage/Reminder.ts
@@ -1,56 +1,54 @@
-import { Reminder } from "types/api/reminder";
-import { getDaysBetweenDate, getParticularDateFromSpecificDay, getToday } from "utils/date";
+import type { ReminderResult } from 'types/api/reminder';
+import { getDaysBetweenDate, getParticularDateFromSpecificDay, getToday } from 'utils/date';
 
 const KEY = 'MSW_REMINDER';
 
-const getAll = ():{data:Reminder[]} => {
+const getAll = (): ReminderResult => {
   const storageData = sessionStorage.getItem(KEY);
-  return storageData ? JSON.parse(storageData) : {data:[]};
+  return storageData ? JSON.parse(storageData) : { data: [] };
 };
 
-const water = (id:number,date:string) =>{ // 물을 준 것임. 물을 줬으니까 다음 날을 반환해 줘야 함.
-  const {data} = JSON.parse(sessionStorage.getItem(KEY) ?? '[]') as {data:Reminder[]}
+const water = (id: number, date: string) => {
+  // 물을 준 것임. 물을 줬으니까 다음 날을 반환해 줘야 함.
+  const { data } = JSON.parse(sessionStorage.getItem(KEY) ?? '[]') as ReminderResult;
 
   const updatedData = data.map((data) => {
-    const {petPlantId} = data
-    if(id !== petPlantId) return data
+    const { petPlantId } = data;
+    if (id !== petPlantId) return data;
 
     const betweenDate = getDaysBetweenDate(getToday(), date); // date에 물을 준거임... 다음 물주기는 언제가 될지 모름.
-
-    const ranNum =Math.ceil( Math.random()*10)
+    const ranNum = Math.ceil(Math.random() * 10);
 
     return {
       ...data,
-      nextWaterDate: getParticularDateFromSpecificDay(betweenDate+ranNum),
-      dDay:-(betweenDate+ranNum)
-    }
-  })
+      nextWaterDate: getParticularDateFromSpecificDay(betweenDate + ranNum),
+      dDay: -(betweenDate + ranNum),
+    };
+  });
 
+  sessionStorage.setItem(KEY, JSON.stringify({ data: updatedData }));
+};
 
-  sessionStorage.setItem(KEY, JSON.stringify({data:updatedData}))
-}
+const changeDate = (id: number, date: string) => {
+  // 여기는 미루기임. 입력된 날짜에 물을 줄 수 있도록 해야함.
+  const { data } = JSON.parse(sessionStorage.getItem(KEY) ?? '[]') as ReminderResult;
 
-const changeDate = (id:number,date:string) =>{// 여기는 미루기임. 입력된 날짜에 물을 줄 수 있도록 해야함.
-  const {data} = JSON.parse(sessionStorage.getItem(KEY) ?? '[]') as {data:Reminder[]}
-  
   const updatedData = data.map((data) => {
-    const {petPlantId} = data
-    if(id !== petPlantId) return data
-    
+    const { petPlantId } = data;
+    if (id !== petPlantId) return data;
+
     const betweenDate = getDaysBetweenDate(getToday(), date);
 
     return {
       ...data,
       nextWaterDate: date,
-      dDay:-betweenDate
-    }
-  })
+      dDay: -betweenDate,
+    };
+  });
 
-  sessionStorage.setItem(KEY, JSON.stringify({data:updatedData}))
-}
+  sessionStorage.setItem(KEY, JSON.stringify({ data: updatedData }));
+};
 
-
-
-const Reminder = { getAll,water,changeDate };
+const Reminder = { getAll, water, changeDate };
 
 export default Reminder;
