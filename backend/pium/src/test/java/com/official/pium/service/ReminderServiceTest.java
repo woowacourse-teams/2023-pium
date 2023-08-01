b6c67f7002e99eb5a1551b6fd6ca1ec11142ead0
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2023-08-01 10:35:10 +0900

    feat: 리마인더 기능 구현 (#148)
    * feat: 리마인더 물주기, 미루기 도메인 로직 구현
    
    * feat: 서비스에 리마인더 물주기, 미루기, 조회 기능 구현
    
    * feat: 컨트롤러 리마인더 물주기, 미루기, 조회 api 구현
    
    * refactor: 테스트 방법 변경
    
    * style: 개행 추가
    
    * feat: 리마인더 물주기, 미루기 사용자 검증 추가
    
    * refactor: 확정된 리마인더 기능에 맞게 검증 수정
    
    * refactor: 리마인더 응답 값에 lastWaterDate 추가
    
    * refactor: 확정된 리마인더 기능에 맞게 메서드명 수정
    
    * feat: 반려식물 물주기 및 날짜 변경 검증 로직 수정
    
    * feat: 엔티티 equals&hashCode 추가
    
    * fix: dday 계산 테스트에서 Fixture 제거 후 새로운 객체 생성
    
    * refactor: 메서드명 수정
    
    * style: 코드 라인 정리
    
    * refactor: 메서드명 수정

---
+++ backend/pium/src/test/java/com/official/pium/service/ReminderServiceTest.java
@@ -0,0 +1,167 @@
+package com.official.pium.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.tuple;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import com.official.pium.IntegrationTest;
+import com.official.pium.domain.History;
+import com.official.pium.domain.Member;
+import com.official.pium.domain.PetPlant;
+import com.official.pium.mapper.PetPlantMapper;
+import com.official.pium.repository.HistoryRepository;
+import com.official.pium.repository.PetPlantRepository;
+import com.official.pium.service.dto.DataResponse;
+import com.official.pium.service.dto.ReminderCreateRequest;
+import com.official.pium.service.dto.ReminderResponse;
+import com.official.pium.service.dto.ReminderUpdateRequest;
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class ReminderServiceTest extends IntegrationTest {
+
+    private PetPlant petPlant;
+    private Member member;
+    private Member otherMember;
+
+    @Autowired
+    private ReminderService reminderService;
+
+    @Autowired
+    private PetPlantRepository petPlantRepository;
+
+    @Autowired
+    private HistoryRepository historyRepository;
+
+    @BeforeEach
+    void setUp() {
+        petPlant = petPlantSupport.builder().build();
+        member = petPlant.getMember();
+        otherMember = memberSupport.builder().build();
+    }
+
+    @Test
+    void 정상적인_물주기_시_다음_물주기_날짜와_마지막으로_물을_준_날짜를_변경() {
+        ReminderCreateRequest request = ReminderCreateRequest.builder()
+                .waterDate(LocalDate.now())
+                .build();
+
+        reminderService.water(request, petPlant.getId(), member);
+
+        PetPlant updatedPetPlant = petPlantRepository.findById(petPlant.getId()).get();
+        LocalDate newWaterDate = request.getWaterDate();
+
+        assertSoftly(softly -> {
+                    softly.assertThat(updatedPetPlant)
+                            .extracting(PetPlant::getNextWaterDate, PetPlant::getLastWaterDate)
+                            .isEqualTo(List.of(newWaterDate.plusDays(petPlant.getWaterCycle()), newWaterDate));
+                    softly.assertThat(historyRepository.findAll())
+                            .extracting(History::getPetPlant, History::getWaterDate)
+                            .contains(tuple(updatedPetPlant, newWaterDate));
+                }
+        );
+    }
+
+    @ParameterizedTest(name = "오늘 보다 {0}일 후에 물주면 예외 발생")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void 오늘_이후_날짜에_물을_주면_예외_발생(int days) {
+        ReminderCreateRequest request = ReminderCreateRequest.builder()
+                .waterDate(LocalDate.now().plusDays(days))
+                .build();
+
+        assertThatThrownBy(
+                () -> reminderService.water(request, petPlant.getId(), member)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("오늘 이후 날짜에 물을 줄 수는 없습니다. date: " + request.getWaterDate());
+    }
+
+    @ParameterizedTest(name = "마지막으로 물준 날짜 {0}일 전에 물주면 예외 발생")
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    void 마지막으로_물준_날짜_이전에_물주면_예외_발생(int days) {
+        ReminderCreateRequest request = ReminderCreateRequest.builder()
+                .waterDate(petPlant.getLastWaterDate().minusDays(days))
+                .build();
+
+        assertThatThrownBy(
+                () -> reminderService.water(request, petPlant.getId(), member)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("마지막으로 물을 준 날짜와 그 이전 날짜에는 물을 줄 수는 없습니다. date: " + request.getWaterDate());
+    }
+
+    @Test
+    void 반려_식물의_사용자와_물주기를_요청한_사용자가_다르면_예외_발생() {
+        ReminderCreateRequest request = ReminderCreateRequest.builder()
+                .waterDate(LocalDate.now())
+                .build();
+
+        assertThatThrownBy(
+                () -> reminderService.water(request, petPlant.getId(), otherMember)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("요청 사용자와 반려 식물의 사용자가 일치하지 않습니다. memberId: " + otherMember.getId());
+    }
+
+    @ParameterizedTest(name = "오늘보다 {0}일 후 날짜로 물주기 미루기")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void 물주기_미루기(int days) {
+        LocalDate newWaterDate = LocalDate.now().plusDays(days);
+        ReminderUpdateRequest request = ReminderUpdateRequest.builder()
+                .nextWaterDate(newWaterDate)
+                .build();
+
+        reminderService.updateNextWaterDate(request, petPlant.getId(), member);
+
+        PetPlant updatedPetPlant = petPlantRepository.findById(petPlant.getId()).get();
+
+        assertThat(updatedPetPlant.getNextWaterDate()).isEqualTo(newWaterDate);
+    }
+
+    @ParameterizedTest(name = "오늘보다 {0}일 전 날짜로 물주기를 미루면 예외 발생")
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    void 오늘_또는_이전_날짜로_물주기를_미루면_예외_발생(int days) {
+        LocalDate newWaterDate = LocalDate.now().minusDays(days);
+        ReminderUpdateRequest request = ReminderUpdateRequest.builder()
+                .nextWaterDate(newWaterDate)
+                .build();
+
+        assertThatThrownBy(
+                () -> reminderService.updateNextWaterDate(request, petPlant.getId(), member)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("오늘과 그 이전 날짜로 물주기 날짜를 변경할 수는 없습니다. date: " + newWaterDate);
+    }
+
+    @Test
+    void 반려_식물의_사용자와_미루기를_요청한_사용자가_다르면_예외_발생() {
+        ReminderUpdateRequest request = ReminderUpdateRequest.builder()
+                .nextWaterDate(LocalDate.now().plusDays(1))
+                .build();
+
+        assertThatThrownBy(
+                () -> reminderService.updateNextWaterDate(request, petPlant.getId(), otherMember)
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("요청 사용자와 반려 식물의 사용자가 일치하지 않습니다. memberId: " + otherMember.getId());
+    }
+
+    @Test
+    void 리마인더_전체_조회() {
+        DataResponse<List<ReminderResponse>> actual = reminderService.readAll(petPlant.getMember());
+
+        List<ReminderResponse> expected = List.of(
+                PetPlantMapper.toReminderResponse(petPlant, petPlant.calculateDDay(LocalDate.now())));
+
+        assertThat(actual.getData())
+                .hasSize(1)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+}
