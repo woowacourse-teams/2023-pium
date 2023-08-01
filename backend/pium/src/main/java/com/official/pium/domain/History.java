66f4a6a0637106023f14d1fbdb4129316b7efd19
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2023-08-01 10:35:10 +0900

    feat: 리마인더 기능 구현 (#148)
    * feat: 리마인더 물주기, 미루기 도메인 로직 구현
    
    * feat: 서비스에 리마인더 물주기, 미루기, 조회 기능 구현
    
    * feat: 컨트롤러 리마인더 물주기, 미루기, 조회 api 구현
    
    * refactor: 테스트 방법 변경
    
    * style: 개행 추가
    
    * feat: 리마인더 물주기, 미루기 사용자 검증 추가
    
    * refactor: 확정된 리마인더 기능에 맞게 검증 수정
    
    * refactor: 리마인더 응답 값에 lastWaterDate 추가
    
    * refactor: 확정된 리마인더 기능에 맞게 메서드명 수정
    
    * feat: 반려식물 물주기 및 날짜 변경 검증 로직 수정
    
    * feat: 엔티티 equals&hashCode 추가
    
    * fix: dday 계산 테스트에서 Fixture 제거 후 새로운 객체 생성
    
    * refactor: 메서드명 수정
    
    * style: 코드 라인 정리
    
    * refactor: 메서드명 수정

--- backend/pium/src/main/java/com/official/pium/domain/History.java
+++ backend/pium/src/main/java/com/official/pium/domain/History.java
@@ -1,13 +1,22 @@
 package com.official.pium.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import org.hibernate.proxy.HibernateProxy;
 
 @Entity
 @Getter
@@ -32,5 +41,32 @@ public class History extends BaseEntity {
     private History(@NotNull PetPlant petPlant, @NotNull LocalDate waterDate) {
         this.petPlant = petPlant;
         this.waterDate = waterDate;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass =
+                o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+                        : o.getClass();
+        Class<?> thisEffectiveClass =
+                this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                        .getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        History history = (History) o;
+        return getId() != null && Objects.equals(getId(), history.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                .getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
---
+++ backend/pium/src/main/java/com/official/pium/service/ReminderService.java
@@ -0,0 +1,72 @@
+package com.official.pium.service;
+
+import com.official.pium.domain.History;
+import com.official.pium.domain.Member;
+import com.official.pium.domain.PetPlant;
+import com.official.pium.mapper.PetPlantMapper;
+import com.official.pium.repository.HistoryRepository;
+import com.official.pium.repository.PetPlantRepository;
+import com.official.pium.service.dto.DataResponse;
+import com.official.pium.service.dto.ReminderCreateRequest;
+import com.official.pium.service.dto.ReminderResponse;
+import com.official.pium.service.dto.ReminderUpdateRequest;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReminderService {
+
+    private final PetPlantRepository petPlantRepository;
+    private final HistoryRepository historyRepository;
+
+    @Transactional
+    public void water(ReminderCreateRequest reminderCreateRequest, Long petPlantId, Member member) {
+        PetPlant petPlant = petPlantRepository.findById(petPlantId)
+                .orElseThrow(() -> new NoSuchElementException("일치하는 반려 식물이 존재하지 않습니다. id: " + petPlantId));
+        checkOwner(petPlant, member);
+
+        petPlant.water(reminderCreateRequest.getWaterDate());
+
+        History history = History.builder()
+                .petPlant(petPlant)
+                .waterDate(reminderCreateRequest.getWaterDate())
+                .build();
+
+        historyRepository.save(history);
+    }
+
+    @Transactional
+    public void updateNextWaterDate(ReminderUpdateRequest reminderUpdateRequest, Long petPlantId, Member member) {
+        PetPlant petPlant = petPlantRepository.findById(petPlantId)
+                .orElseThrow(() -> new NoSuchElementException("일치하는 반려 식물이 존재하지 않습니다. id: " + petPlantId));
+        checkOwner(petPlant, member);
+
+        petPlant.changeNextWaterDate(reminderUpdateRequest.getNextWaterDate());
+    }
+
+    private void checkOwner(PetPlant petPlant, Member member) {
+        if (petPlant.isNotOwnerOf(member)) {
+            throw new IllegalArgumentException("요청 사용자와 반려 식물의 사용자가 일치하지 않습니다. memberId: " + member.getId());
+        }
+    }
+
+    public DataResponse<List<ReminderResponse>> readAll(Member member) {
+        List<PetPlant> petPlants = petPlantRepository.findAllByMemberId(member.getId());
+
+        List<ReminderResponse> reminderResponses = petPlants.stream()
+                .map(petPlant -> PetPlantMapper.toReminderResponse(
+                        petPlant,
+                        petPlant.calculateDDay(LocalDate.now())))
+                .toList();
+
+        return DataResponse.<List<ReminderResponse>>builder()
+                .data(reminderResponses)
+                .build();
+    }
+}
