4e91659479d4f563ada9141fff9d4932dcfd4998
bassyu <yrkdgus@gmail.com>
2023-08-02 15:32:37 +0900

    feat: useWaterDateList 쿼리 구현
    - infiniteQuery로 페이징 GET 처리
    - HistoryResponse를 WaterDateList로 전처리하여 반환

---
+++ frontend/src/hooks/queries/history/useWaterDateList.ts
@@ -0,0 +1,27 @@
+import type { HistoryResponse } from 'types/api/history';
+import type { PetPlantDetails } from 'types/api/petPlant';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import HistoryAPI, { HISTORY } from 'apis/history';
+
+const useWaterDateList = (petPlantId: PetPlantDetails['id']) =>
+  useInfiniteQuery<HistoryResponse, Error, string[], [string, PetPlantDetails['id']], number>({
+    queryKey: [HISTORY, petPlantId],
+    queryFn: async ({ pageParam }) => {
+      const response = await HistoryAPI.getPetPlant(petPlantId, pageParam);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    },
+    defaultPageParam: 0,
+    getNextPageParam: ({ hasNext }, _allPages, lastPageParam) => {
+      return hasNext ? lastPageParam + 1 : undefined;
+    },
+    suspense: true,
+    select: (data) =>
+      data.pages.reduce<string[]>(
+        (accWaterDateList, page) => accWaterDateList.concat(page.waterDateList),
+        []
+      ),
+  });
+
+export default useWaterDateList;
