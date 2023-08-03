9c44d3853aef336ddec720a2e0e29bbd50247360
hozzijeong <ghwls0905@naver.com>
2023-08-03 12:44:17 +0900

    chore: date를 사용하는 storybook에 ToastProvider 설정

--- frontend/src/components/@common/Calendar/Calendar.stories.tsx
+++ frontend/src/components/@common/Calendar/Calendar.stories.tsx
@@ -1,9 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import ToastProvider from 'contexts/toastContext';
 import Calendar from '.';
+import ToastList from '../Toast/ToastList';
 
 const meta: Meta<typeof Calendar> = {
   component: Calendar,
-  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <ToastProvider>
+          <Story />
+          <ToastList />
+        </ToastProvider>
+      );
+    },
+  ],
 };
 
 export default meta;
