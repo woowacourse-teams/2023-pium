ce4d3dd67500bdbdc2e8077864f71c300ba8ad44
Q Kim <qoli0703@gmail.com>
2023-08-03 14:39:38 +0900

    fix: 의문의 세미콜론 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import router from './router';
 const App = () => {
   return (
     <ErrorBoundary fallback={<Error />}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </ErrorBoundary>
   );
 };
