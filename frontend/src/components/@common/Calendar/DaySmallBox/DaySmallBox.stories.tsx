f64dcbce9b469353328b7dd57843115218155095
hozzijeong <ghwls0905@naver.com>
2023-08-03 13:38:05 +0900

    chore: storybook args 추가

--- frontend/src/components/@common/Calendar/DaySmallBox/DaySmallBox.stories.tsx
+++ frontend/src/components/@common/Calendar/DaySmallBox/DaySmallBox.stories.tsx
@@ -3,13 +3,24 @@ import DaySmallBox from '.';
 
 const meta: Meta<typeof DaySmallBox> = {
   component: DaySmallBox,
-  args: {},
+  args: {
+    date: 10,
+    isToday: false,
+    currentDate: new Date('2023-07-10'),
+    isInRange: true,
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof DaySmallBox>;
 
-export const Default: Story = {
-  args: {},
+export const Default: Story = {};
+
+export const Today: Story = {
+  args: {
+    date: new Date().getDate(),
+    isToday: true,
+    currentDate: new Date(),
+  },
 };
