7aeb7f9e312bdf08c33b9e122d4f43ac4d40d91c
bassyu <yrkdgus@gmail.com>
2023-08-03 15:26:08 +0900

    refactor: 데이터 전처리를 query 훅으로 이동

--- frontend/src/hooks/queries/history/useWaterDateList.ts
+++ frontend/src/hooks/queries/history/useWaterDateList.ts
@@ -3,8 +3,58 @@ import type { PetPlantDetails } from 'types/api/petPlant';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import HistoryAPI, { HISTORY } from 'apis/history';
 
-const useWaterDateList = (petPlantId: PetPlantDetails['id']) =>
-  useInfiniteQuery<HistoryResponse, Error, string[], [string, PetPlantDetails['id']], number>({
+type DateList = string[];
+
+type Year = string;
+type Month = string;
+type Day = number;
+
+type YearMap = Record<Year, MonthMap>;
+type MonthMap = Record<Month, Day[]>;
+
+type YearList = [Year, MonthList][];
+type MonthList = [Month, Day[]][];
+
+const convertHistoryResponseListToDateList = (historyResponseList: HistoryResponse[]) =>
+  historyResponseList.reduce<DateList>(
+    (accWaterDateList, page) => accWaterDateList.concat(page.waterDateList),
+    []
+  );
+
+const convertDateListToYearMap = (dateList: string[]) => {
+  const yearMap: YearMap = {};
+
+  dateList.forEach((date) => {
+    const [year, month, day] = date.split('-').map(Number);
+
+    if (!yearMap[year]) {
+      yearMap[year] = {};
+    }
+
+    if (!yearMap[year][month]) {
+      yearMap[year][month] = [];
+    }
+
+    yearMap[year][month].push(day);
+  });
+
+  return yearMap;
+};
+
+const convertYearMapToYearList = (yearMap: YearMap) => {
+  const map2list = <T>(map: Record<string, T>) =>
+    Object.entries(map).sort(([a], [b]) => Number(b) - Number(a));
+
+  const yearList: YearList = map2list(yearMap).map(([year, monthMap]) => [
+    year,
+    map2list(monthMap),
+  ]);
+
+  return yearList;
+};
+
+const useYearList = (petPlantId: PetPlantDetails['id']) =>
+  useInfiniteQuery<HistoryResponse, Error, YearList, [string, PetPlantDetails['id']], number>({
     queryKey: [HISTORY, petPlantId],
     queryFn: async ({ pageParam }) => {
       const response = await HistoryAPI.getPetPlant(petPlantId, pageParam);
@@ -16,11 +66,12 @@ const useWaterDateList = (petPlantId: PetPlantDetails['id']) =>
       return hasNext ? lastPageParam + 1 : undefined;
     },
     suspense: true,
-    select: (data) =>
-      data.pages.reduce<string[]>(
-        (accWaterDateList, page) => accWaterDateList.concat(page.waterDateList),
-        []
-      ),
+    select: ({ pages }) => {
+      const dateList = convertHistoryResponseListToDateList(pages);
+      const yearMap = convertDateListToYearMap(dateList);
+      const yearList = convertYearMapToYearList(yearMap);
+      return yearList;
+    },
   });
 
-export default useWaterDateList;
+export default useYearList;
--- frontend/src/pages/PetPlantTimeline/index.tsx
+++ frontend/src/pages/PetPlantTimeline/index.tsx
@@ -18,19 +18,13 @@ import {
 import useWaterDateList from 'hooks/queries/history/useWaterDateList';
 import useIntersectionRef from 'hooks/useIntersectionRef';
 import Sprout from 'assets/sprout.svg';
-import date2map from './date2map';
 
 const PetPlantTimeline = () => {
   const { id: petPlantId } = useParams();
-  const { data: waterDateList, fetchNextPage } = useWaterDateList(Number(petPlantId));
-  if (!waterDateList) return null;
+  const { data: yearList, fetchNextPage } = useWaterDateList(Number(petPlantId));
+  if (!yearList) return null;
 
   const intersectionRef = useIntersectionRef<HTMLDivElement>(fetchNextPage);
-
-  const yearMap = date2map(waterDateList);
-  const yearList = Object.entries(yearMap)
-    .sort(([aMonth], [bMonth]) => Number(aMonth) - Number(bMonth))
-    .reverse();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -42,12 +36,8 @@ const PetPlantTimeline = () => {
         <Plant>
           <PlantImage src={Sprout} alt="타임라인 꼭대기" />
         </Plant>
-        {yearList.map(([year, monthMap]) => {
-          const monthList = Object.entries(monthMap)
-            .sort(([aDay], [bDay]) => Number(aDay) - Number(bDay))
-            .reverse();
-
-          return monthList.map(([month, dayList]) => (
+        {yearList.map(([year, monthList]) =>
+          monthList.map(([month, dayList]) => (
             <MonthArea key={year + month}>
               <MonthHeader>
                 <Month>{month}월</Month>
@@ -61,8 +51,8 @@ const PetPlantTimeline = () => {
                 </TimelineItem>
               ))}
             </MonthArea>
-          ));
-        })}
+          ))
+        )}
         <Earth />
         <Sensor ref={intersectionRef} />
       </Main>
