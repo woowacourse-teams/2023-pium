f1f3b17f7f72f7136e5d6e449540fb7ee08a5cb6
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-08-08 22:10:12 +0900

    fix: 반려 식물 삭제 요청을 반환 타입 변경

--- backend/pium/src/main/java/com/official/pium/controller/PetPlantController.java
+++ backend/pium/src/main/java/com/official/pium/controller/PetPlantController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
