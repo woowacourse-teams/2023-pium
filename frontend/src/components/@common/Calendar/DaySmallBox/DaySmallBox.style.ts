76f7cca8c6d4c99a498a3387b6b1321b05a40fb0
Q Kim <qoli0703@gmail.com>
2023-08-09 10:49:10 +0900

    fix: react does not recognize prop on DOM error
    styled components transient props 적용

--- frontend/src/components/@common/Calendar/DaySmallBox/DaySmallBox.style.ts
+++ frontend/src/components/@common/Calendar/DaySmallBox/DaySmallBox.style.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 interface DayProps {
-  isToday: boolean | null;
-  isInRange: boolean;
+  $isToday: boolean | null;
+  $isInRange: boolean;
 }
 
 export const Wrapper = styled.div`
@@ -20,25 +20,25 @@ export const DaySpan = styled.span<DayProps>`
   width: 40px;
   height: 40px;
 
-  color: ${({ isToday, isInRange, theme: { color } }) => {
-    if (isToday) return color.background;
-    if (isInRange) return color.sub;
+  color: ${({ $isToday, $isInRange, theme: { color } }) => {
+    if ($isToday) return color.background;
+    if ($isInRange) return color.sub;
     return color.grayDark;
   }};
 
-  background: ${({ isToday, isInRange, theme: { color } }) => {
-    if (isToday && isInRange) return color.accent;
-    if (isToday) return color.accent + '5F';
+  background: ${({ $isToday, $isInRange, theme: { color } }) => {
+    if ($isToday && $isInRange) return color.accent;
+    if ($isToday) return color.accent + '5F';
     return color.background;
   }};
   border-radius: 50%;
 
   &:hover {
-    color: ${({ isToday, isInRange, theme: { color } }) =>
-      isToday || isInRange ? color.background : color.grayDark};
-    background: ${({ isToday, isInRange, theme: { color } }) => {
-      if (isToday && !isInRange) return color.accent + '5F';
-      if (!isToday && !isInRange) return color.background;
+    color: ${({ $isToday, $isInRange, theme: { color } }) =>
+      $isToday || $isInRange ? color.background : color.grayDark};
+    background: ${({ $isToday, $isInRange, theme: { color } }) => {
+      if ($isToday && !$isInRange) return color.accent + '5F';
+      if (!$isToday && !$isInRange) return color.background;
       return color.accent;
     }};
   }
