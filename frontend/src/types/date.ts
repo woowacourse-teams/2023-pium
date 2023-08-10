2095334aa22d81429557b3f6b02d6bdf04396c26
Q Kim <qoli0703@gmail.com>
2023-08-10 17:50:28 +0900

    fix: Day type '00' 가능한 문제 수정

--- frontend/src/types/date.ts
+++ frontend/src/types/date.ts
@@ -4,7 +4,7 @@ export type Year = `${'19' | '20'}${DecimalDigit}${DecimalDigit}`;
 
 export type Month = `0${Exclude<DecimalDigit, '0'>}` | '10' | '11' | '12';
 
-export type Day = `${'0' | '1' | '2'}${DecimalDigit}` | '30' | '31';
+export type Day = `0${Exclude<DecimalDigit, '0'>}` | `${'1' | '2'}${DecimalDigit}` | '30' | '31';
 
 export type DateFormat = `${Year}-${Month}-${Day}`;
 
