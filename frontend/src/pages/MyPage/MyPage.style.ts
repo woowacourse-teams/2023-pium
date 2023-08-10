5e82d04bec9c415181e6ac6bdb4e2dcdc4d28324
hozzijeong <ghwls0905@naver.com>
2023-08-10 15:10:41 +0900

    design: myPage 마크업

---
+++ frontend/src/pages/MyPage/MyPage.style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleBox = styled.div`
+  width: 100%;
+  height: 68px;
+`;
+
+export const Title = styled.p`
+  font: 900 4rem/6.8rem 'GmarketSans';
+`;
+
+export const UserInfoBox = styled.section`
+  display: flex;
+`;
--- frontend/src/pages/MyPage/index.tsx
+++ frontend/src/pages/MyPage/index.tsx
@@ -1,11 +1,34 @@
 import Navbar from 'components/@common/Navbar';
+import { Title, TitleBox, UserInfoBox, Wrapper } from './MyPage.style';
 
 const MyPage = () => {
   return (
-    <div>
-      마이페이지 입니다.
+    <>
+      <Wrapper>
+        <TitleBox>
+          <Title>마이페이지</Title>
+        </TitleBox>
+        <UserInfoBox>
+          <img src="" alt="프로필" />
+          <div>
+            <p>아이디</p>
+            <p>가입일</p>
+          </div>
+        </UserInfoBox>
+
+        <section>
+          <div>
+            <input id="current-plant" type="text" readOnly />
+            <label htmlFor="current-plant">현재 키우는 식물</label>
+          </div>
+          <div>
+            <input id="temp" type="text" readOnly />
+            <label htmlFor="temp">가나다라 마바사</label>
+          </div>
+        </section>
+      </Wrapper>
       <Navbar />
-    </div>
+    </>
   );
 };
 
