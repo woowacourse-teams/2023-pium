24c41acf39b13634f4f75d1dabc18d005e276d74
bassyu <yrkdgus@gmail.com>
2023-08-11 17:10:59 +0900

    feat: 시계, 물방울 아이콘 추가

---
+++ frontend/src/components/@common/Icons/Water.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from 'react';
+
+const Water = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14" {...props}>
+      <path
+        fill="#75AEDC"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.5 9C11.5 6.51 7 .5 7 .5S2.5 6.51 2.5 9a4.5 4.5 0 0 0 9 0Z"
+      ></path>
+    </svg>
+  );
+};
+
+export default Water;
