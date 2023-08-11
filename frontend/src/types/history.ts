eba7961a2db77dc6713d018af1e952b50d27e907
bassyu <yrkdgus@gmail.com>
2023-08-11 16:32:40 +0900

    feat: dateTemplate 배열에서 HistoryItem 배열로 변경

--- frontend/src/types/history.ts
+++ frontend/src/types/history.ts
@@ -1,7 +1,23 @@
+import { PetPlantDetails } from './petPlant';
+
 export interface HistoryResponse {
   page: number;
   size: number;
   elementSize: number;
   hasNext: boolean;
-  waterDateList: string[];
+  data: HistoryItem[];
 }
+
+export interface HistoryItem {
+  type: HistoryType;
+  date: string;
+  content: {
+    previous: string | null;
+    current: string;
+  };
+}
+
+export type HistoryType = keyof Pick<
+  PetPlantDetails,
+  'lastWaterDate' | 'waterCycle' | 'flowerpot' | 'light' | 'location'
+>;
