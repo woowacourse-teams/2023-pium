210f7073be88f14ae8233fb79b56e55e3b4551f5
Q Kim <qoli0703@gmail.com>
2023-08-11 21:13:31 +0900

    feat: useAddToast 훅으로 렌더링 최적화

--- frontend/src/pages/PetPlantEdit/index.tsx
+++ frontend/src/pages/PetPlantEdit/index.tsx
@@ -1,15 +1,17 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import PetPlantEditForm from 'components/petPlant/PetPlantEditForm';
 import usePetPlantDetails from 'hooks/queries/pet/usePetPlantDetails';
+import useAddToast from 'hooks/useAddToast';
 import { URL_PATH } from 'constants/index';
 
 const PetPlantEdit = () => {
   const { id } = useParams();
   const { data } = usePetPlantDetails(Number(id));
   const navigate = useNavigate();
+  const addToast = useAddToast();
 
   if (!data) {
-    alert('존재하지 않는 반려식물이에요');
+    addToast('error', '존재하지 않는 반려식물이에요');
     navigate(URL_PATH.main, { replace: true });
     return null;
   }
