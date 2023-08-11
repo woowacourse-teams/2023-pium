f5d8a168d807afbe13aa031a722cab1220fd5a5d
최준호 <junho5336@gmail.com>
2023-08-11 14:36:39 +0900

    style: import 와일드카드 제거

--- backend/pium/src/main/java/com/official/pium/domain/PetPlant.java
+++ backend/pium/src/main/java/com/official/pium/domain/PetPlant.java
@@ -1,6 +1,14 @@
 package com.official.pium.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
