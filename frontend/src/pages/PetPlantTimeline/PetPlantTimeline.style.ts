ceed0b1dc3a8edafe37a855ca3e2c4a6ec58730d
bassyu <yrkdgus@gmail.com>
2023-08-11 17:27:22 +0900

    feat: Timeline 페이지에 필터 버튼 추가

--- frontend/src/pages/PetPlantTimeline/PetPlantTimeline.style.ts
+++ frontend/src/pages/PetPlantTimeline/PetPlantTimeline.style.ts
@@ -1,115 +1,28 @@
 import { styled } from 'styled-components';
 
+export const Header = styled.header`
+  position: fixed;
+  z-index: ${(props) => props.theme.zIndex.fixed};
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  width: 100%;
+  max-width: ${(props) => props.theme.width.pad};
+  height: 48px;
+
+  background-color: ${(props) => props.theme.color.background};
+  box-shadow: 0 2px 2px -2px ${(props) => props.theme.color.gray};
+`;
+
 export const Main = styled.main`
   position: relative;
 
   width: 100%;
   min-height: 100%;
-  padding: 96px 0;
+  padding: 80px 0;
 
   background-color: white;
-`;
-
-export const Plant = styled.div`
-  position: absolute;
-  top: 56px;
-  left: 50%;
-  transform: translate(-50%);
-
-  width: 2px;
-  min-height: 100%;
-
-  background: ${(props) => props.theme.color.primary};
-`;
-
-export const PlantImage = styled.img`
-  position: absolute;
-  top: -12px;
-  left: 50%;
-  transform: translate(-52%);
-
-  width: 24px;
-`;
-
-export const Earth = styled.div`
-  position: absolute;
-  bottom: -56px;
-  left: 50%;
-  transform: translate(-50%);
-
-  width: 80px;
-  height: 80px;
-
-  background-color: #733813;
-  border-top: solid 8px #441a00;
-  border-bottom: solid 12px #bb764c;
-  border-radius: 8px 8px 50% 50%;
-`;
-
-export const Sensor = styled.div`
-  width: 100%;
-  height: 32px;
-`;
-
-export const MonthArea = styled.div`
-  position: relative;
-  width: 100%;
-`;
-
-export const MonthHeader = styled.h2`
-  position: sticky;
-  top: 0;
-  height: 0;
-  padding-left: 32px;
-`;
-
-export const Month = styled.p`
-  height: 32px;
-  padding-top: 8px;
-  font-size: 24px;
-`;
-
-export const TimelineItem = styled.div`
-  display: flex;
-  align-items: end;
-
-  width: 100%;
-  height: 32px;
-  margin-bottom: 64px;
-
-  font-weight: 700;
-`;
-
-export const Day = styled.div`
-  position: relative;
-
-  display: flex;
-  align-items: end;
-  justify-content: end;
-
-  width: 50%;
-  padding-right: 32px;
-
-  font-size: 2rem;
-`;
-
-export const Spot = styled.div`
-  position: absolute;
-  top: 50%;
-  right: 0;
-  transform: translate(50%, -50%);
-
-  width: 12px;
-  height: 12px;
-
-  background-color: white;
-  border: solid 2px ${(props) => props.theme.color.primary};
-  border-radius: 50%;
-`;
-
-export const Water = styled.div`
-  width: 50%;
-  padding-left: 32px;
-  font-size: 2rem;
-  color: #1c7fd0;
 `;
--- frontend/src/pages/PetPlantTimeline/index.tsx
+++ frontend/src/pages/PetPlantTimeline/index.tsx
@@ -1,60 +1,74 @@
-import { useEffect } from 'react';
+import type { HistoryType } from 'types/history';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CheckButton from 'components/@common/CheckButton';
 import Navbar from 'components/@common/Navbar';
-import {
-  Day,
-  Earth,
-  Main,
-  Month,
-  MonthArea,
-  MonthHeader,
-  Plant,
-  PlantImage,
-  Sensor,
-  Spot,
-  TimelineItem,
-  Water,
-} from './PetPlantTimeline.style';
-import useYearList from 'hooks/queries/history/useYearList';
-import useIntersectionRef from 'hooks/useIntersectionRef';
-import Sprout from 'assets/sprout.svg';
+import Timeline from 'components/petPlant/Timeline';
+import { Header, Main } from './PetPlantTimeline.style';
+import useSwitch from 'hooks/useSwitch';
 
 const PetPlantTimeline = () => {
   const { id: petPlantId } = useParams();
-  const { data: yearList, fetchNextPage } = useYearList(Number(petPlantId));
-  if (!yearList) return null;
 
-  const intersectionRef = useIntersectionRef<HTMLDivElement>(fetchNextPage);
+  const { isOn: isCheckedWater, toggle: toggleWater } = useSwitch(true);
+  const { isOn: isCheckedWaterCycle, toggle: toggleWaterCycle } = useSwitch(true);
+  const { isOn: isCheckedSetting, toggle: toggleSetting } = useSwitch(true);
+
+  const [filter, setFilter] = useState<HistoryType[]>([
+    'flowerpot',
+    'lastWaterDate',
+    'light',
+    'location',
+    'waterCycle',
+  ]);
+
+  const onClickWater = () => {
+    toggleWater();
+    if (isCheckedWater) {
+      setFilter(filter.filter((type) => type !== 'lastWaterDate'));
+    } else {
+      setFilter([...filter, 'lastWaterDate']);
+    }
+  };
+
+  const onClickWaterCycle = () => {
+    toggleWaterCycle();
+    if (isCheckedWaterCycle) {
+      setFilter(filter.filter((type) => type !== 'waterCycle'));
+    } else {
+      setFilter([...filter, 'waterCycle']);
+    }
+  };
+
+  const onClickSetting = () => {
+    toggleSetting();
+    if (isCheckedSetting) {
+      setFilter(filter.filter((type) => !['flowerpot', 'light', 'location'].includes(type)));
+    } else {
+      setFilter([...filter, 'flowerpot', 'light', 'location']);
+    }
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  if (!petPlantId) return null;
   return (
     <>
+      <Header>
+        <CheckButton checked={isCheckedWater} onClick={onClickWater}>
+          물 준 날
+        </CheckButton>
+        <CheckButton checked={isCheckedWaterCycle} onClick={onClickWaterCycle}>
+          물 주기 변화
+        </CheckButton>
+        <CheckButton checked={isCheckedSetting} onClick={onClickSetting}>
+          환경 변화
+        </CheckButton>
+      </Header>
       <Main>
-        <Plant>
-          <PlantImage src={Sprout} alt="타임라인 꼭대기" />
-        </Plant>
-        {yearList.map(([year, monthList]) =>
-          monthList.map(([month, dayList]) => (
-            <MonthArea key={year + month}>
-              <MonthHeader>
-                <Month>{month}월</Month>
-              </MonthHeader>
-              {dayList.map((day) => (
-                <TimelineItem key={year + month + day}>
-                  <Day>
-                    {day}일 <Spot />
-                  </Day>
-                  <Water>물 주기 완료</Water>
-                </TimelineItem>
-              ))}
-            </MonthArea>
-          ))
-        )}
-        <Earth />
-        <Sensor ref={intersectionRef} />
+        <Timeline petPlantId={Number(petPlantId)} filter={filter} />
       </Main>
       <Navbar />
     </>
