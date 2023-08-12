b3282771ab3b35360bd31e73a9ce7ddce0b4dd05
Geonhoe Lee <79038908+rawfishthelgh@users.noreply.github.com>
2023-08-12 18:37:15 +0900

    feat: 반려식물 등록 시 히스토리 추가 (#256)
    * feat: 반려식물 등록 시 히스토리 추가
    
    * feat: 반려식물 등록 시 히스토리 추가
    
    * chore: 공백 수정

--- backend/pium/src/main/java/com/official/pium/event/history/PetPlantHistory.java
+++ backend/pium/src/main/java/com/official/pium/event/history/PetPlantHistory.java
@@ -1,15 +1,17 @@
 package com.official.pium.event.history;
 
 import com.official.pium.domain.HistoryType;
-import com.official.pium.domain.PetPlant;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class PetPlantHistory {
+
+    private static final String EMPTY = "EMPTY";
 
     private final String location;
     private final String flowerpot;
@@ -28,42 +30,14 @@ public class PetPlantHistory {
         this.lastWaterDate = lastWaterDate;
     }
 
-    public PetPlantHistory(PetPlant petPlant) {
-        this.location = petPlant.getLocation();
-        this.flowerpot = petPlant.getFlowerpot();
-        this.light = petPlant.getLight();
-        this.wind = petPlant.getWind();
-        this.waterCycle = petPlant.getWaterCycle().toString();
-        this.lastWaterDate = petPlant.getLastWaterDate().toString();
-    }
-
-    public List<HistoryEvent> generateCreateHistoryEvents(Long petPlantId, PetPlantHistory other, LocalDate date) {
+    public List<HistoryEvent> generateCreateHistoryEvents(Long petPlantId, LocalDate date) {
         List<HistoryEvent> events = new ArrayList<>();
-
-        if (!location.equals(other.location)) {
-            events.add(new HistoryEvent(petPlantId, location, other.location, HistoryType.LOCATION, date));
-        }
-
-        if (!flowerpot.equals(other.flowerpot)) {
-            events.add(new HistoryEvent(petPlantId, flowerpot, other.flowerpot, HistoryType.FLOWERPOT, date));
-        }
-
-        if (!light.equals(other.light)) {
-            events.add(new HistoryEvent(petPlantId, light, other.light, HistoryType.LIGHT, date));
-        }
-
-        if (!wind.equals(other.wind)) {
-            events.add(new HistoryEvent(petPlantId, wind, other.wind, HistoryType.WIND, date));
-        }
-
-        if (!waterCycle.equals(other.waterCycle)) {
-            events.add(new HistoryEvent(petPlantId, waterCycle, other.waterCycle, HistoryType.WATER_CYCLE, date));
-        }
-
-        if (!lastWaterDate.equals(other.lastWaterDate)) {
-            events.add(new HistoryEvent(petPlantId, lastWaterDate, other.lastWaterDate, HistoryType.LAST_WATER_DATE, date));
-        }
-
+        events.add(new HistoryEvent(petPlantId, EMPTY, location, HistoryType.LOCATION, date));
+        events.add(new HistoryEvent(petPlantId, EMPTY, flowerpot, HistoryType.FLOWERPOT, date));
+        events.add(new HistoryEvent(petPlantId, EMPTY, light, HistoryType.LIGHT, date));
+        events.add(new HistoryEvent(petPlantId, EMPTY, wind, HistoryType.WIND, date));
+        events.add(new HistoryEvent(petPlantId, EMPTY, waterCycle, HistoryType.WATER_CYCLE, date));
+        events.add(new HistoryEvent(petPlantId, EMPTY, lastWaterDate, HistoryType.LAST_WATER_DATE, date));
         return events;
     }
 }
