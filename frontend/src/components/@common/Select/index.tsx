2b23fb6a3577a6ba25a05ad91c63c63a809dd8da
Q Kim <qoli0703@gmail.com>
2023-08-12 18:35:41 +0900

    feat: select 키보드 접근 가능

--- frontend/src/components/@common/Select/index.tsx
+++ frontend/src/components/@common/Select/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import ArrowDropDown from 'components/@common/Icons/ArrowDropDown';
 import { Backdrop, IconArea, OptionBox, OptionItem, SelectedValue, Wrapper } from './Select.style';
 
@@ -20,25 +20,54 @@ const Select = ({ value, options, onChange, placeholder }: SelectProps) => {
     setIsOpen(false);
   };
 
-  const select = (option: string) => () => {
+  const select = (option: string) => {
     onChange?.(option);
     close();
   };
 
+  const closeOnEscape = useCallback(
+    ({ key }: KeyboardEvent) => {
+      if (key === 'Escape') setIsOpen(false);
+    },
+    [setIsOpen]
+  );
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    window.addEventListener('keyup', closeOnEscape);
+
+    return () => {
+      window.removeEventListener('keyup', closeOnEscape);
+    };
+  }, [isOpen, closeOnEscape]);
+
   return (
-    <Wrapper role="menu">
-      <SelectedValue onClick={toggle} $placeholder={value === ''}>
+    <Wrapper>
+      <SelectedValue
+        onClick={toggle}
+        $placeholder={value === ''}
+        aria-label={`값 선택 메뉴 열기. 현재 선택된 값: ${value || '없음'}`}
+      >
         {value || placeholder}
       </SelectedValue>
-      <IconArea $rotate={isOpen}>
+      <IconArea $rotate={isOpen} aria-hidden>
         <ArrowDropDown width={24} height={24} />
       </IconArea>
       {isOpen && (
         <>
           <Backdrop onClick={close} />
-          <OptionBox>
+          <OptionBox role="menu">
             {options.map((option) => (
-              <OptionItem key={option} role="menuitem" onClick={select(option)}>
+              <OptionItem
+                key={option}
+                role="menuitem"
+                onClick={() => select(option)}
+                tabIndex={0}
+                onKeyDown={({ key }: React.KeyboardEvent<HTMLLIElement>) => {
+                  if (key === 'Enter' || key === ' ') select(option);
+                }}
+              >
                 {option}
               </OptionItem>
             ))}
