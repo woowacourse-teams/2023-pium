dcf3e5add755e21722eb2bcb5e4123dbd0e3cfee
bassyu <yrkdgus@gmail.com>
2023-08-12 15:30:43 +0900

    feat: 환경 설정 아이콘 추가

--- frontend/src/components/petPlant/TimelineDayItem/index.tsx
+++ frontend/src/components/petPlant/TimelineDayItem/index.tsx
@@ -1,9 +1,13 @@
 import ArrowRightAlt from 'components/@common/Icons/ArrowRightAlt';
+import Flowerpot from 'components/@common/Icons/Flowerpot';
 import Stopwatch from 'components/@common/Icons/Stopwatch';
+import Sun from 'components/@common/Icons/Sun';
 import Water from 'components/@common/Icons/Water';
+import Wind from 'components/@common/Icons/Wind';
 import type { TimelineItem } from 'components/petPlant/Timeline/converter';
 import { IconArea, Item, ItemContent, ItemHead, Wrapper } from './TimelineDayItem.style';
 import { getDaysBetween } from 'utils/date';
+import theme from 'style/theme.style';
 
 interface TimelineDayItemProps {
   timelineItemList: TimelineItem[];
@@ -36,7 +40,14 @@ const TimelineDayItem = ({ timelineItemList }: TimelineDayItemProps) => {
             return (
               <Item key={type + index}>
                 <IconArea>
-                  <Stopwatch />
+                  {
+                    {
+                      waterCycle: <Stopwatch />,
+                      flowerpot: <Flowerpot color={theme.color.primary} />,
+                      light: <Sun color={theme.color.primary} />,
+                      location: <Wind color={theme.color.primary} />,
+                    }[type]
+                  }
                 </IconArea>
                 <div>
                   <ItemHead>
