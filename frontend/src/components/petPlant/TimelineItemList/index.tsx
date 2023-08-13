3e0ab7d11c9fca3c57a08182d46f67f00638104f
bassyu <yrkdgus@gmail.com>
2023-08-13 15:20:11 +0900

    refactor: 아이콘 추출 및 상수를 컴포넌트 밖으로 분리

--- frontend/src/components/petPlant/TimelineItemList/index.tsx
+++ frontend/src/components/petPlant/TimelineItemList/index.tsx
@@ -13,63 +13,59 @@ interface TimelineItemListProps {
   timelineItemList: TimelineItem[];
 }
 
-const TimelineItemList = ({ timelineItemList }: TimelineItemListProps) => {
-  const TYPE_MESSAGE_MAP = {
-    waterCycle: '물 주기를 ',
-    flowerpot: '화분을 ',
-    light: '채광을 ',
-    location: '위치를 ',
-  };
-
-  return (
-    <Wrapper>
-      {timelineItemList.map(({ type, previous, current }, index) => {
-        switch (type) {
-          case 'lastWaterDate':
-            return (
-              <Item key={'water' + index}>
-                <IconArea>
-                  <Water />
-                </IconArea>
-                <ItemHead>
-                  {previous ? `${getDaysBetween(previous, current)}일 만에` : '처음'} 물을 줬어요
-                </ItemHead>
-              </Item>
-            );
-          default:
-            return (
-              <Item key={type + index}>
-                <IconArea>
-                  {
-                    {
-                      waterCycle: <Stopwatch />,
-                      flowerpot: <Flowerpot color={theme.color.primary} />,
-                      light: <Sun color={theme.color.primary} />,
-                      location: <Wind color={theme.color.primary} />,
-                    }[type]
-                  }
-                </IconArea>
-                <div>
-                  <ItemHead>
-                    {TYPE_MESSAGE_MAP[type]}
-                    {previous ? '변경' : '설정'}했어요
-                  </ItemHead>
-                  <ItemContent>
-                    {previous && (
-                      <>
-                        {previous}
-                        <ArrowRightAlt />
-                      </>
-                    )}
-                    {current}
-                  </ItemContent>
-                </div>
-              </Item>
-            );
-        }
-      })}
-    </Wrapper>
-  );
+const HISTORY_TYPE_ICON_MAP = {
+  waterCycle: <Stopwatch />,
+  flowerpot: <Flowerpot color={theme.color.primary} />,
+  light: <Sun color={theme.color.primary} />,
+  location: <Wind color={theme.color.primary} />,
 };
+
+const HISTORY_TYPE_KOREAN_MAP = {
+  waterCycle: '물 주기를 ',
+  flowerpot: '화분을 ',
+  light: '채광을 ',
+  location: '위치를 ',
+};
+
+const TimelineItemList = ({ timelineItemList }: TimelineItemListProps) => (
+  <Wrapper>
+    {timelineItemList.map(({ type, previous, current }, index) => {
+      switch (type) {
+        case 'lastWaterDate':
+          return (
+            <Item key={'water' + index}>
+              <IconArea>
+                <Water />
+              </IconArea>
+              <ItemHead>
+                {previous ? `${getDaysBetween(previous, current)}일 만에` : '처음'} 물을 줬어요
+              </ItemHead>
+            </Item>
+          );
+        default:
+          return (
+            <Item key={type + index}>
+              <IconArea>{HISTORY_TYPE_ICON_MAP[type]}</IconArea>
+              <div>
+                <ItemHead>
+                  {HISTORY_TYPE_KOREAN_MAP[type]}
+                  {previous ? '변경' : '설정'}했어요
+                </ItemHead>
+                <ItemContent>
+                  {previous && (
+                    <>
+                      {previous}
+                      <ArrowRightAlt />
+                    </>
+                  )}
+                  {current}
+                </ItemContent>
+              </div>
+            </Item>
+          );
+      }
+    })}
+  </Wrapper>
+);
 
 export default TimelineItemList;
