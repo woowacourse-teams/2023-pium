e7765ede76ac724d72b621cb64ff3a470f28e8ee
yeonkkk <tjddus0302@gmail.com>
2023-08-14 22:15:08 +0900

    test: OAuth 요청 실패 테스트 추가

--- backend/pium/src/test/java/com/official/pium/service/AuthServiceTest.java
+++ backend/pium/src/test/java/com/official/pium/service/AuthServiceTest.java
@@ -1,11 +1,14 @@
 package com.official.pium.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import com.official.pium.domain.Member;
+import com.official.pium.exception.OAuthException.KakaoTokenRequestException;
 import com.official.pium.repository.MemberRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -13,6 +16,7 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,7 +49,7 @@ class AuthServiceTest {
 
     @ParameterizedTest
     @CsvSource({"token1,12345,code1", "token2,1234534,code2", "token3,1234534,code3"})
-    void 로그인(String accessToken, Long kakaoId, String authorizationCode) {
+    void 로그인_성공(String accessToken, Long kakaoId, String authorizationCode) {
         mockServer = MockRestServiceServer.createServer(restTemplate);
 
         String tokenResponse = String.format("{\"access_token\":\"%s\"}", accessToken);
@@ -64,8 +68,22 @@ class AuthServiceTest {
         assertThat(loginMember.getKakaoId()).isEqualTo(kakaoId);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 잘못된_인증_코드로_로그인_요청_시_예외_발생(String authorizationCode) {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+
+        mockServer.expect(requestTo(TOKEN_REQUEST_URI))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withBadRequest());
+
+        assertThatThrownBy(
+                () -> authService.login(authorizationCode)
+        ).isInstanceOf(KakaoTokenRequestException.class);
+    }
+
     @Test
-    void 회원탈퇴() {
+    void 회원탈퇴_성공() {
         Member member = Member.builder().kakaoId(1234533333L).build();
         Member saveMember = memberRepository.save(member);
 
--- backend/pium/src/test/java/com/official/pium/service/dto/OAuthSupporterTest.java
+++ backend/pium/src/test/java/com/official/pium/service/dto/OAuthSupporterTest.java
@@ -3,16 +3,24 @@ package com.official.pium.service.dto;
 import static com.official.pium.service.dto.OAuthSupporter.AUTHORIZATION_HEADER;
 import static com.official.pium.service.dto.OAuthSupporter.TOKEN_TYPE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
+import com.official.pium.exception.OAuthException.KaKaoMemberInfoRequestException;
+import com.official.pium.exception.OAuthException.KakaoServerException;
+import com.official.pium.exception.OAuthException.KakaoTokenRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -64,6 +72,33 @@ class OAuthSupporterTest {
         assertThat(memberInfoResponse.getId()).isEqualTo(kakaoId);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 잘못된_토큰으로_사용자_정보_조회_요청_시_예외_발생(String accessToken) {
+        mockServer.expect(requestTo(MEMBER_INFO_REQUEST_URI))
+                .andExpect(content().contentType("application/x-www-form-urlencoded"))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(header(AUTHORIZATION_HEADER, TOKEN_TYPE + accessToken))
+                .andRespond(withBadRequest());
+
+        assertThatThrownBy(
+                () -> supporter.getMemberInfo(accessToken)
+        ).isInstanceOf(KaKaoMemberInfoRequestException.class);
+    }
+
+    @Test
+    void 사용자_정보_조회_요청_중_카카오_서버에_문제_발생_시_예외_발생() {
+        mockServer.expect(requestTo(MEMBER_INFO_REQUEST_URI))
+                .andExpect(content().contentType("application/x-www-form-urlencoded"))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(header(AUTHORIZATION_HEADER, TOKEN_TYPE + "accessToken"))
+                .andRespond(withServerError());
+
+        assertThatThrownBy(
+                () -> supporter.getMemberInfo("accessToken")
+        ).isInstanceOf(KakaoServerException.class);
+    }
+
     @Test
     void 액세스_토큰_조회() {
         String authorizationCode = "authorization code";
@@ -78,5 +113,30 @@ class OAuthSupporterTest {
         KaKaoAccessTokenResponse tokenResponse = supporter.getAccessToken(authorizationCode);
 
         assertThat(tokenResponse.getAccessToken()).isEqualTo(accessToken);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 잘못된_인증_코드로_액세스_토큰_요청_시_예외_발생(String authorizationCode) {
+        mockServer.expect(requestTo(TOKEN_REQUEST_URI))
+                .andExpect(content().contentType("application/x-www-form-urlencoded;charset=utf-8"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withBadRequest());
+
+        assertThatThrownBy(
+                () -> supporter.getAccessToken(authorizationCode)
+        ).isInstanceOf(KakaoTokenRequestException.class);
+    }
+
+    @Test
+    void 액세스_토큰_요청_중_카카오_서버에_문제_발생_시_예외_발생() {
+        mockServer.expect(requestTo(TOKEN_REQUEST_URI))
+                .andExpect(content().contentType("application/x-www-form-urlencoded;charset=utf-8"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withServerError());
+
+        assertThatThrownBy(
+                () -> supporter.getAccessToken("authorizationCode")
+        ).isInstanceOf(KakaoServerException.class);
     }
 }
