414333806d631fdac3e1291ffe83046cdd8c447c
bassyu <yrkdgus@gmail.com>
2023-08-14 13:32:45 +0900

    feat: 환경 설정 변경값 간략화

--- frontend/src/components/petPlant/TimelineItemList/index.tsx
+++ frontend/src/components/petPlant/TimelineItemList/index.tsx
@@ -15,20 +15,35 @@ interface TimelineItemListProps {
   timelineItemList: TimelineItem[];
 }
 
-const HISTORY_TYPE_ICON_MAP: Record<Exclude<HistoryType, 'lastWaterDate'>, React.ReactElement> = {
-  waterCycle: <Stopwatch />,
+type SettingType = Exclude<HistoryType, 'lastWaterDate' | 'waterCycle'>;
+
+const SETTING_ICON_MAP: Record<SettingType, React.ReactElement> = {
   flowerpot: <Flowerpot color={theme.color.primary} />,
   light: <Sun color={theme.color.primary} />,
   location: <House color={theme.color.primary} />,
   wind: <Wind color={theme.color.primary} />,
 };
 
-const HISTORY_TYPE_KOREAN_MAP: Record<Exclude<HistoryType, 'lastWaterDate'>, string> = {
-  waterCycle: '물 주기를 ',
-  flowerpot: '화분을 ',
-  light: '채광을 ',
-  location: '위치를 ',
-  wind: '바람 환경을 ',
+const SETTING_KOREAN_MAP: Record<SettingType, string> = {
+  flowerpot: '화분을',
+  light: '채광을',
+  location: '위치를',
+  wind: '바람 환경을',
+};
+
+const LIGHT_CUTBACK_MAP: Record<string, string> = {
+  '창문 밖에서 해를 받아요': '창문 밖',
+  '창문 안쪽에서 해를 받아요': '창문 안쪽',
+  '일반 조명 빛을 받아요': '일반 조명',
+  '식물용 조명 빛을 받아요': '식물용 조명',
+  '해를 못 받아요': '채광 없음',
+};
+
+const WIND_CUTBACK_MAP: Record<string, string> = {
+  '5m 내 창문이 있어요': '5m 내 창문',
+  '5m 보다 멀리 창문이 있어요': '5m 멀리 창문',
+  '창문이 없지만 바람이 통해요': '창문 없음',
+  '바람이 안 통해요': '바람 없음',
 };
 
 const TimelineItemList = ({ timelineItemList }: TimelineItemListProps) => (
@@ -46,25 +61,47 @@ const TimelineItemList = ({ timelineItemList }: TimelineItemListProps) => (
               </ItemHead>
             </Item>
           );
+        case 'waterCycle':
+          return (
+            <Item key={'waterCycle' + index}>
+              <IconArea>
+                <Stopwatch />
+              </IconArea>
+              <div>
+                <ItemHead>물 주기를 {previous ? '변경' : '설정'}했어요</ItemHead>
+                <ItemContent>
+                  {previous && (
+                    <>
+                      {previous}일 <ArrowRightAlt />
+                    </>
+                  )}
+                  {current}일
+                </ItemContent>
+              </div>
+            </Item>
+          );
         default:
+          if (type === 'light') {
+            previous = previous ? LIGHT_CUTBACK_MAP[previous] : previous;
+            current = LIGHT_CUTBACK_MAP[current];
+          } else if (type === 'wind') {
+            previous = previous ? WIND_CUTBACK_MAP[previous] : previous;
+            current = WIND_CUTBACK_MAP[current];
+          }
           return (
             <Item key={type + index}>
-              <IconArea>{HISTORY_TYPE_ICON_MAP[type]}</IconArea>
+              <IconArea>{SETTING_ICON_MAP[type]}</IconArea>
               <div>
                 <ItemHead>
-                  {HISTORY_TYPE_KOREAN_MAP[type]}
-                  {previous ? '변경' : '설정'}했어요
+                  {SETTING_KOREAN_MAP[type]} {previous ? '변경' : '설정'}했어요
                 </ItemHead>
                 <ItemContent>
                   {previous && (
                     <>
-                      {previous}
-                      {type === 'waterCycle' ? '일' : ''}
-                      <ArrowRightAlt />
+                      {previous} <ArrowRightAlt />
                     </>
                   )}
                   {current}
-                  {type === 'waterCycle' ? '일' : ''}
                 </ItemContent>
               </div>
             </Item>
