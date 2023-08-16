e94e3446ffe2181742493464a49221fdc4d6de79
Q Kim <qoli0703@gmail.com>
2023-08-16 14:02:33 +0900

    refactor: 404 페이지 에러 초기화 적용

--- frontend/src/pages/Error/index.tsx
+++ frontend/src/pages/Error/index.tsx
@@ -1,20 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import { Button, SadPiumiImage, Section, Text, Title, Wrapper } from './Error.style';
+import useResetErrorBoundary from 'hooks/useResetErrorBoundary';
 import { URL_PATH } from 'constants/index';
 import sadpiumi from 'assets/sadpiumi.svg';
 
 const Error = () => {
   const navigate = useNavigate();
+  const resetErrorBoundary = useResetErrorBoundary();
 
-  const reloadWindow = () => location.reload();
-  const goToMain = () => navigate(URL_PATH.main);
+  const reloadWindow = () => {
+    resetErrorBoundary();
+    location.reload();
+  };
+
+  const goToMain = () => {
+    resetErrorBoundary();
+    navigate(URL_PATH.main, { replace: true });
+  };
 
   return (
     <Wrapper>
       <SadPiumiImage src={sadpiumi} alt="슬픈 표정을 하는 피우미" />
       <Section>
         <Text>이런!</Text>
-        <Title>페이지를 불러올 수 없어요</Title>
+        <Title>페이지를 찾지 못했어요</Title>
       </Section>
       <Section>
         <Text>주소가 올바른지 확인해 보세요</Text>
