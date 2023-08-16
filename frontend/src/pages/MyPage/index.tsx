069923bc1899cd81901c17f72d796070de7be8d0
hozzijeong <ghwls0905@naver.com>
2023-08-16 13:06:39 +0900

    refactor: 비로그인 상태로 mypage 접근 막기

--- frontend/src/pages/MyPage/index.tsx
+++ frontend/src/pages/MyPage/index.tsx
@@ -1,11 +1,22 @@
+import { useEffect } from 'react';
 import Navbar from 'components/@common/Navbar';
 import { ButtonBox, Logout, Title, TitleBox, Withdraw, Wrapper } from './MyPage.style';
+import useCheckSessionId from 'hooks/queries/auth/useCheckSessionId';
 import useLogout from 'hooks/queries/auth/useLogout';
 import useWithdraw from 'hooks/queries/auth/useWithdraw';
+import useUnauthorize from 'hooks/useUnauthorize';
 
 const MyPage = () => {
+  const { redirectLoginPage } = useUnauthorize();
+  const { error } = useCheckSessionId();
   const { mutate: logoutMutate } = useLogout();
   const { mutate: withdrawMutate } = useWithdraw();
+
+  useEffect(() => {
+    if (error) {
+      redirectLoginPage(error);
+    }
+  }, [error]);
 
   const handleLogout: React.MouseEventHandler<HTMLButtonElement> = () => {
     logoutMutate();
@@ -13,6 +24,7 @@ const MyPage = () => {
   const handleWithdraw: React.MouseEventHandler<HTMLButtonElement> = () => {
     withdrawMutate();
   };
+
   return (
     <>
       <Wrapper>
