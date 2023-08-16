47024c596511f1860871995ee39b9aec57ad58b6
bassyu <yrkdgus@gmail.com>
2023-08-16 09:59:08 +0900

    chore: changeCallback에서 검증 역할 제외

--- frontend/src/pages/PetRegister/Form/index.tsx
+++ frontend/src/pages/PetRegister/Form/index.tsx
@@ -51,22 +51,17 @@ const PetRegisterForm = () => {
   };
 
   const setBirthDate = (value: string) => {
-    if (value > today) {
-      return false;
-    }
+    if (value > today) return;
+
     dispatch({ type: 'SET', key: 'birthDate', value });
     showNextElement(1);
-
-    return true;
   };
 
   const setLastWaterDate = (value: string) => {
-    if (value > today) {
-      return false;
-    }
+    if (value > today) return;
+
     dispatch({ type: 'SET', key: 'lastWaterDate', value });
     showNextElement(2);
-    return true;
   };
 
   const setWaterCycle = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
