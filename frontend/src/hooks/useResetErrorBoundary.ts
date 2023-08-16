86adea08e105743833f28ca6905bf5d07db60396
Q Kim <qoli0703@gmail.com>
2023-08-16 14:57:50 +0900

    style: tsdoc 추가

--- frontend/src/hooks/useResetErrorBoundary.ts
+++ frontend/src/hooks/useResetErrorBoundary.ts
@@ -2,6 +2,10 @@ import { useCallback, useContext } from 'react';
 import { ErrorBoundarySetStateContext } from 'contexts/ErrorBoundarySetStateContext';
 import { ERROR } from 'constants/index';
 
+/**
+ * 이 훅을 사용하는 컴포넌트를 fallback으로 가지는 ErrorBoundary의 에러 상태를 초기화합니다.
+ * @returns ErrorBoundary를 초기화하는 콜백 함수
+ */
 const useResetErrorBoundary = () => {
   const context = useContext(ErrorBoundarySetStateContext);
 
