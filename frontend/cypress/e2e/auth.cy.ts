b71d7034b567c74118240c881e0e8c7308c11b8b
Q Kim <qoli0703@gmail.com>
2023-08-17 13:58:37 +0900

    test: 비로그인 리다이렉션

---
+++ frontend/cypress/e2e/auth.cy.ts
@@ -0,0 +1,89 @@
+import login from '../utils/login';
+
+describe('비로그인 상태에서는 로그인 페이지로 이동한다.', () => {
+  it('리마인더', () => {
+    cy.visit('/reminder');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('내 반려 식물 목록', () => {
+    cy.visit('/pet');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('반려 식물 상세 정보', () => {
+    cy.visit('/pet/123');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('반려 식물 등록: 검색', () => {
+    cy.visit('/pet/register');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('반려 식물 등록: 양식', () => {
+    cy.visit('/pet/register/1');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('마이페이지', () => {
+    cy.visit('/myPage');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('반려 식물 정보 수정', () => {
+    cy.visit('/pet/1/edit');
+    cy.location('pathname').should('equal', '/login');
+  });
+
+  it('타임라인', () => {
+    cy.visit('/pet/1/timeline');
+    cy.location('pathname').should('equal', '/login');
+  });
+});
+
+describe('로그인 상태에서는 접근이 가능하다.', () => {
+  beforeEach(() => {
+    login();
+  });
+
+  it('리마인더', () => {
+    cy.visit('/reminder');
+    cy.location('pathname').should('equal', '/reminder');
+  });
+
+  it('내 반려 식물 목록', () => {
+    cy.visit('/pet');
+    cy.location('pathname').should('equal', '/pet');
+  });
+
+  it('반려 식물 상세 정보', () => {
+    cy.visit('/pet/123');
+    cy.location('pathname').should('equal', '/pet/123');
+  });
+
+  it('반려 식물 등록: 검색', () => {
+    cy.visit('/pet/register');
+    cy.location('pathname').should('equal', '/pet/register');
+  });
+
+  it('반려 식물 등록: 양식', () => {
+    cy.visit('/pet/register/1');
+    cy.location('pathname').should('equal', '/pet/register/1');
+  });
+
+  it('마이페이지', () => {
+    cy.visit('/myPage');
+    cy.location('pathname').should('equal', '/myPage');
+  });
+
+  it('반려 식물 정보 수정', () => {
+    cy.visit('/pet/1/edit');
+    cy.location('pathname').should('equal', '/pet/1/edit');
+  });
+
+  it('타임라인', () => {
+    cy.visit('/pet/1/timeline');
+    cy.location('pathname').should('equal', '/pet/1/timeline');
+  });
+});
