21b214908e67258f976898f98ec0da2458060db3
bassyu <yrkdgus@gmail.com>
2023-08-17 18:03:45 +0900

    chore: export 제거

--- frontend/src/components/@common/Icons/AccountCircle.tsx
+++ frontend/src/components/@common/Icons/AccountCircle.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 
-export const AccountCircle = (props: SVGProps<SVGSVGElement>) => {
+const AccountCircle = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
