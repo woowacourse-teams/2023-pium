d902afe172d5950b967e622e3d3887b81ca78485
bassyu <yrkdgus@gmail.com>
2023-08-17 18:04:43 +0900

    chore: alt 수정

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -45,11 +45,7 @@ const Login = () => {
         <Text>
           함께 해요 <PrimaryText>피움</PrimaryText>
         </Text>
-        <PlantImg
-          key={imageSrcIndex}
-          src={IMAGE_SRC_LIST[imageSrcIndex]}
-          alt="대충 식물 이미지 넣음"
-        />
+        <PlantImg key={imageSrcIndex} src={IMAGE_SRC_LIST[imageSrcIndex]} alt="식물" />
       </ContentBox>
 
       <LoginBox>
