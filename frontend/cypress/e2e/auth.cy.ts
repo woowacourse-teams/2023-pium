2a021a2ef5d8d1d51f69b87b4a4bfb31ba1077a6
Q Kim <qoli0703@gmail.com>
2023-08-17 14:31:23 +0900

    test: 리다이렉션 토스트

--- frontend/cypress/e2e/auth.cy.ts
+++ frontend/cypress/e2e/auth.cy.ts
@@ -3,42 +3,66 @@ import login from '../utils/login';
 describe('비로그인 상태에서는 로그인 페이지로 이동한다.', () => {
   it('리마인더', () => {
     cy.visit('/reminder');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('내 반려 식물 목록', () => {
     cy.visit('/pet');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('반려 식물 상세 정보', () => {
     cy.visit('/pet/123');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('반려 식물 등록: 검색', () => {
     cy.visit('/pet/register');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('반려 식물 등록: 양식', () => {
     cy.visit('/pet/register/1');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('마이페이지', () => {
     cy.visit('/myPage');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('반려 식물 정보 수정', () => {
     cy.visit('/pet/1/edit');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 
   it('타임라인', () => {
     cy.visit('/pet/1/timeline');
-    cy.location('pathname').should('equal', '/login');
+    cy.get('#toast-root')
+      .contains('로그인 후 이용 가능')
+      .location('pathname')
+      .should('equal', '/login');
   });
 });
 
