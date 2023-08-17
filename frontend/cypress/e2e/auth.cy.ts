9bd4359dd9a7b08c90ccefa25738b1901d976464
Q Kim <qoli0703@gmail.com>
2023-08-17 14:37:48 +0900

    test: url 검사 방식 변경

--- frontend/cypress/e2e/auth.cy.ts
+++ frontend/cypress/e2e/auth.cy.ts
@@ -3,66 +3,42 @@ import login from '../utils/login';
 describe('비로그인 상태에서는 로그인 페이지로 이동한다.', () => {
   it('리마인더', () => {
     cy.visit('/reminder');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('내 반려 식물 목록', () => {
     cy.visit('/pet');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('반려 식물 상세 정보', () => {
     cy.visit('/pet/123');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('반려 식물 등록: 검색', () => {
     cy.visit('/pet/register');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('반려 식물 등록: 양식', () => {
     cy.visit('/pet/register/1');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('마이페이지', () => {
     cy.visit('/myPage');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('반려 식물 정보 수정', () => {
     cy.visit('/pet/1/edit');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 
   it('타임라인', () => {
     cy.visit('/pet/1/timeline');
-    cy.get('#toast-root')
-      .contains('로그인 후 이용 가능')
-      .location('pathname')
-      .should('equal', '/login');
+    cy.get('#toast-root').contains('로그인 후 이용 가능').url().should('match', /login/);
   });
 });
 
