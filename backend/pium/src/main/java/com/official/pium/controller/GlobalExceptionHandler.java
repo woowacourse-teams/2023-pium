050bddf3aa5e22747441c08704aacadbb7309351
최준호 <junho5336@gmail.com>
2023-09-01 22:10:02 +0900

    feat: 관리자 커스텀 예외 추가

--- backend/pium/src/main/java/com/official/pium/controller/GlobalExceptionHandler.java
+++ backend/pium/src/main/java/com/official/pium/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.official.pium.controller;
 
+import com.official.pium.exception.NeedAdminException;
 import com.official.pium.exception.OAuthException;
 import com.official.pium.exception.OAuthException.KaKaoMemberInfoRequestException;
 import com.official.pium.exception.OAuthException.KakaoServerException;
@@ -112,6 +113,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
+    @ExceptionHandler(NeedAdminException.class)
+    public ResponseEntity<GlobalExceptionResponse> handleNeedAdminException(ConstraintViolationException e) {
+        String message = e.getMessage();
+        GlobalExceptionResponse exceptionResponse = createExceptionResponse(message);
+        log.warn(message);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+    }
+
     private StringBuilder getExceptionMessages(ConstraintViolationException e) {
         Iterator<ConstraintViolation<?>> iterator =
                 e.getConstraintViolations().iterator();
@@ -131,4 +140,5 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .message(message)
                 .build();
     }
+
 }
---
+++ backend/pium/src/main/java/com/official/pium/exception/NeedAdminException.java
@@ -0,0 +1,8 @@
+package com.official.pium.exception;
+
+public class NeedAdminException extends RuntimeException {
+
+    public NeedAdminException(final String message) {
+        super(message);
+    }
+}
