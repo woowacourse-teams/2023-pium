b645ef10d8bd36957b606043b1d01d790bbdd60b
hozzijeong <ghwls0905@naver.com>
2023-09-07 10:18:50 +0900

    refactor: aria-hidden 추가

--- frontend/src/components/@common/VerticalDivider/VerticalDivider.style.ts
+++ frontend/src/components/@common/VerticalDivider/VerticalDivider.style.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 type Pixel = `${number}px`;
 
-const VerticalDivider = styled.div<{ height: Pixel }>`
+const VerticalDivider = styled.div.attrs({
+  'aria-hidden': true,
+})<{ height: Pixel }>`
   width: 1px; /* 세로 선의 너비를 조절할 수 있습니다. */
   height: ${({ height }) => height};
   background: ${({ theme: { color } }) => color.gray}; /* 세로 선의 색상을 지정합니다. */
