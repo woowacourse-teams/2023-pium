7389b07d457bc9fb222d5e39c584f4e0b0f2dcd0
bassyu <yrkdgus@gmail.com>
2023-09-11 14:10:55 +0900

    design: 더보기 영역과 내용 영역 분리

--- frontend/src/components/@common/SeeMoreContentBox/SeeMoreContentBox.styles.ts
+++ frontend/src/components/@common/SeeMoreContentBox/SeeMoreContentBox.styles.ts
@@ -2,19 +2,13 @@ import { styled } from 'styled-components';
 
 export const Wrapper = styled.div<{ isSeeMore: boolean; maxHeight: string }>`
   position: relative;
-
-  overflow: hidden;
-
   width: 100%;
-  height: max-content;
-  max-height: ${({ isSeeMore, maxHeight }) => (isSeeMore ? 'max-content' : maxHeight)};
-  padding-bottom: 1rem;
+  height: ${({ isSeeMore, maxHeight }) => (isSeeMore ? 'max-content' : maxHeight)};
+`;
 
-  /* font-size: 1rem; */
-
-  background-color: transparent;
-
-  transition: height 1s;
+export const ContentArea = styled.div<{ isSeeMore: boolean }>`
+  overflow: hidden;
+  max-height: ${(props) => (props.isSeeMore ? 'max-content' : 'calc(100% - 1.6rem)')};
 `;
 
 export const SeeMoreButtonArea = styled.div`
@@ -26,13 +20,10 @@ export const SeeMoreButtonArea = styled.div`
   justify-content: start;
 
   width: 100%;
-
-  background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 30%);
 `;
 
 export const SeeMoreButton = styled.button`
   width: max-content;
   height: 1.2rem;
   font-size: 1.2rem;
-  background-color: white;
 `;
