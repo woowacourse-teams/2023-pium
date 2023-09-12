264e5b8d78dd0defd36b10602741a5a99f5aa0b2
bassyu <yrkdgus@gmail.com>
2023-09-12 12:06:11 +0900

    chore: 모두의 정원 등록 페이지 URL 추가

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -12,6 +12,8 @@ export const URL_PATH = {
   petEdit: '/pet/:id/edit',
   petRegisterSearch: '/pet/register',
   petRegisterForm: '/pet/register/:id',
+  gardenRegisterPick: '/garden/register',
+  gardenRegisterForm: '/garden/register/:id',
   reminder: '/reminder',
   login: '/login',
   authorization: '/authorization',
