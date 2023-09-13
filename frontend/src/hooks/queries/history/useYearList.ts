c0f156520c822442ef0e40868133802e86ac0b44
bassyu <yrkdgus@gmail.com>
2023-09-13 14:38:33 +0900

    style: 간략화 및 옵션 정렬 변경

--- frontend/src/hooks/queries/history/useYearList.ts
+++ frontend/src/hooks/queries/history/useYearList.ts
@@ -22,17 +22,18 @@ const useYearList = (petPlantId: PetPlantDetails['id'], filter: HistoryType[] = 
     queryKey: [HISTORY_URL, petPlantId, filter],
     queryFn: async ({ pageParam }) => {
       const response = await HistoryAPI.getPetPlant(petPlantId, pageParam, 20, filter);
-
       throwOnInvalidStatus(response);
-
-      const data = await response.json();
-      return data;
+      return response.json();
     },
 
     initialPageParam: 0,
-    getNextPageParam: ({ hasNext }, _allPages, lastPageParam) => {
-      return hasNext ? lastPageParam + 1 : undefined;
-    },
+    getNextPageParam: ({ hasNext }, _, lastPageParam) => (hasNext ? lastPageParam + 1 : null),
+
+    throwOnError: true,
+    retry: noRetryIfUnauthorized,
+    refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
+    gcTime: 0,
 
     select: (data) => {
       const historyItemList = convertHistoryResponseListToHistoryItemList(data.pages);
@@ -40,12 +41,6 @@ const useYearList = (petPlantId: PetPlantDetails['id'], filter: HistoryType[] = 
       const yearList = convertYearMapToYearList(yearMap);
       return yearList;
     },
-
-    throwOnError: true,
-    retry: noRetryIfUnauthorized,
-    refetchOnWindowFocus: false,
-    placeholderData: keepPreviousData,
-    gcTime: 0,
   });
 
 export default useYearList;
