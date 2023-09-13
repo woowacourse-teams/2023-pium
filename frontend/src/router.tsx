dcad9d02d5cda4ae2f76b977562bd8f79c530d0c
bassyu <yrkdgus@gmail.com>
2023-09-13 19:33:32 +0900

    chore: garden 라우팅 추가

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import DictionaryPlantDetail from 'pages/DictionaryPlantDetail';
 import DictionaryPlantSearch from 'pages/DictionaryPlantSearch';
 import NotFound from 'pages/Error/NotFound';
+import GardenPostList from 'pages/GardenPostList';
 import Login from 'pages/Login';
 import Authorization from 'pages/Login/Authorization';
 import Main from 'pages/Main';
@@ -62,6 +63,10 @@ const router = createBrowserRouter([
       {
         path: URL_PATH.timeline,
         element: <PetPlantTimeline />,
+      },
+      {
+        path: URL_PATH.garden,
+        element: <GardenPostList />,
       },
       {
         path: URL_PATH.login,
