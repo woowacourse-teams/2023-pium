5f203fb50a90c1feb681dcfe08ac34a6e74c8acf
hozzijeong <ghwls0905@naver.com>
2023-09-14 13:31:41 +0900

    chore: 코드 스플리팅만 적용

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -85,13 +85,7 @@ module.exports = (env) => ({
   ],
   optimization: {
     splitChunks: {
-      cacheGroups: {
-        react: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: 'react',
-          chunks: 'all',
-        },
-      },
+      chunks: 'all',
     },
   },
   devServer: {
