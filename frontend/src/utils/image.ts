5bdd39d85385f35cb5b9fe1f4509530d8a04b397
Q Kim <qoli0703@gmail.com>
2023-09-15 13:44:14 +0900

    refactor: 사전 식물 등록 신청 FormData 컨벤션을 맞춘다. (#360)
    * refactor: formdata 컨벤션 통일
    
    * design: 검색창 등록 신청 링크 위치 변경
    
    * feat: 이미지 확장자 제한
    
    * refactor: 사진 등록하기 버튼에서 가능한 확장자 명시

--- frontend/src/utils/image.ts
+++ frontend/src/utils/image.ts
@@ -1,3 +1,5 @@
+import { ALLOWED_IMAGE_EXTENSIONS } from 'constants/index';
+
 export const getFirstImage = (fileList: FileList, maxByteSize: File['size'] = 5_000_000) => {
   const firstImage = Array.from(fileList).find(
     (file) => /^image/.test(file.type) && file.size <= maxByteSize
@@ -9,3 +11,6 @@ export const getImageUrl = (file: File) => {
   if (!/^image/.test(file.type)) throw new Error('file type is not image');
   return URL.createObjectURL(file);
 };
+
+export const isAllowedImageExtension = (file: File) =>
+  ALLOWED_IMAGE_EXTENSIONS.includes(file.type.toLowerCase());
