5ba03fbe5ae93d5ae0aad7ae2bc5bdaeff9ab6d8
Q Kim <qoli0703@gmail.com>
2023-09-15 13:44:14 +0900

    refactor: 사전 식물 등록 신청 FormData 컨벤션을 맞춘다. (#360)
    * refactor: formdata 컨벤션 통일
    
    * design: 검색창 등록 신청 링크 위치 변경
    
    * feat: 이미지 확장자 제한
    
    * refactor: 사진 등록하기 버튼에서 가능한 확장자 명시

--- frontend/src/pages/NewDictionaryPlantRequest/Form/index.tsx
+++ frontend/src/pages/NewDictionaryPlantRequest/Form/index.tsx
@@ -12,7 +12,7 @@ import {
 } from './Form.style';
 import useDictionaryPlantRegister from 'hooks/queries/dictionaryPlantRegistration/useDictionaryPlantRegister';
 import useAddToast from 'hooks/useAddToast';
-import { getFirstImage, getImageUrl } from 'utils/image';
+import { getFirstImage, getImageUrl, isAllowedImageExtension } from 'utils/image';
 import { NUMBER } from 'constants/index';
 
 interface FormProps {
@@ -53,6 +53,10 @@ const Form = (props: FormProps) => {
 
     const firstImage = getFirstImage(files);
     if (!firstImage) addToast('warning', '5MB 이하의 사진을 올려주세요!');
+    if (firstImage && !isAllowedImageExtension(firstImage)) {
+      addToast('warning', '지원하지 않는 확장자입니다!');
+      return;
+    }
     setImage(firstImage ? firstImage : image);
   };
 
@@ -81,7 +85,11 @@ const Form = (props: FormProps) => {
         <HiddenInput ref={fileInputRef} type="file" accept="image/*" onChange={setImageIfValid} />
         <ImageContent>
           <UploadButton type="button" aria-label="사진 등록하기" onClick={accessFileInput}>
-            {image ? <Thumbnail src={getImageUrl(image)} alt={image.name} /> : '사진 등록하기'}
+            {image ? (
+              <Thumbnail src={getImageUrl(image)} alt={image.name} />
+            ) : (
+              '사진 등록하기 (jpg, jpeg, png, heic)'
+            )}
           </UploadButton>
           <ImageName>{image ? image.name : '아직 사진을 올리지 않았어요.'}</ImageName>
         </ImageContent>
