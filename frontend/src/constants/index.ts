37862bfd7eed4596ce4f0737d8ba91c195e1266c
bassyu <yrkdgus@gmail.com>
2023-09-15 16:48:57 +0900

    fix: 중복 필드 제거

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -19,7 +19,6 @@ export const URL_PATH = {
   login: '/login',
   authorization: '/authorization',
   myPage: '/myPage',
-  garden: '/garden',
   newDictionaryPlantRequest: '/dict/new-plant-request',
 } as const;
 
