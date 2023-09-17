9860d1603cfdc2642cb2ef2629dfbb9d3d79f7d9
Geonhoe Lee <79038908+rawfishthelgh@users.noreply.github.com>
2023-09-17 17:04:58 +0900

    feat : 사전 식물 등록 요청 기능 (#363)
    * feat : 사전 식물 등록 요청 기능
    
    * refactor : 1차 리뷰 반영
    
    * refactor : 예외메세지 수정 테스트에 반영

---
+++ backend/pium/src/main/java/com/official/pium/admin/dto/RegistrationResponse.java
@@ -0,0 +1,18 @@
+package com.official.pium.admin.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RegistrationResponse {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+}
