b8f9759806cdab40de0f8d1e2fa9544145c9b24f
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2023-09-18 19:28:36 +0900

    feat: 모두의 정원 구현 (#365)
    * refactor: 모두의 정원 식물 daySince 타입 수정
    
    * feat: 모두의 정원 식물 등록 기능 구현
    
    * feat: 모두의 정원 식물 조회 기능 구현
    
    * test: GardenController 조회 테스트 추가
    
    * test: 모두의 정원 RestDocs 코드 추가
    
    * style: 전체 import, line 정리
    
    * refactor: fetchJoin을 이용하여 garden 조회
    
    * style: 개행 제거

---
+++ backend/pium/src/main/java/com/official/pium/service/dto/SingleGardenResponse.java
@@ -0,0 +1,49 @@
+package com.official.pium.service.dto;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SingleGardenResponse {
+
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String dictionaryPlantName;
+    private String content;
+    private String manageLevel;
+    private PetPlantInfo petPlant;
+
+    @Getter
+    @NoArgsConstructor
+    public static class PetPlantInfo {
+
+        private String imageUrl;
+        private String nickname;
+        private String location;
+        private String flowerpot;
+        private String light;
+        private String wind;
+        private Long daySince;
+        private Integer waterCycle;
+
+        @Builder
+        private PetPlantInfo(String imageUrl, String nickname, String location, String flowerpot, String light, String wind, Long daySince, Integer waterCycle) {
+            this.imageUrl = imageUrl;
+            this.nickname = nickname;
+            this.location = location;
+            this.flowerpot = flowerpot;
+            this.light = light;
+            this.wind = wind;
+            this.daySince = daySince;
+            this.waterCycle = waterCycle;
+        }
+    }
+}
