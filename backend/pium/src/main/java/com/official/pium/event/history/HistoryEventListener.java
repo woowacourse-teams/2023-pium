8f4b3265f2866267ef04a58e6c14ec3aaa5f88f8
최준호 <junho5336@gmail.com>
2023-09-19 14:51:09 +0900

    refactor: 불필요한 count 쿼리 삭제

--- backend/pium/src/main/java/com/official/pium/event/history/HistoryEventListener.java
+++ backend/pium/src/main/java/com/official/pium/event/history/HistoryEventListener.java
@@ -12,8 +12,8 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -84,7 +84,7 @@ public class HistoryEventListener {
     public void updateLastWaterDateHistory(LastWaterDateEvent lastWaterDateEvent) {
         final PageRequest TOP_ORDER_BY_DATE_DESC = PageRequest.of(0, 1, Sort.Direction.DESC, "date");
 
-        Page<History> historyAboutLastWaterDate = historyRepository.findAllByPetPlantIdAndHistoryCategoryHistoryType(
+        Slice<History> historyAboutLastWaterDate = historyRepository.findAllByPetPlantIdAndHistoryCategoryHistoryType(
                 lastWaterDateEvent.getPetPlantId(), HistoryType.LAST_WATER_DATE, TOP_ORDER_BY_DATE_DESC);
 
         validateContentSize(historyAboutLastWaterDate);
@@ -100,7 +100,7 @@ public class HistoryEventListener {
         }
     }
 
-    private void validateContentSize(Page<History> historyAboutLastWaterDate) {
+    private void validateContentSize(Slice<History> historyAboutLastWaterDate) {
         if (historyAboutLastWaterDate.getSize() != 1) {
             throw new IllegalStateException("최근 물주기 정보는 하나의 값만 존재해야합니다. size: " + historyAboutLastWaterDate.getSize());
         }
