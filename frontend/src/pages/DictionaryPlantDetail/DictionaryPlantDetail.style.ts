56f68bcfd107d1523f32750e02dcd5e4a8f671a3
bassyu <yrkdgus@gmail.com>
2023-09-19 19:34:44 +0900

    feat: Navbar 대신 뒤로가기 헤더와 등록하기 버튼 추가

--- frontend/src/pages/DictionaryPlantDetail/DictionaryPlantDetail.style.ts
+++ frontend/src/pages/DictionaryPlantDetail/DictionaryPlantDetail.style.ts
@@ -6,3 +6,54 @@ export const Main = styled.main`
   gap: 24px;
   margin: 0 auto 100px auto;
 `;
+
+export const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 2rem;
+`;
+
+export const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+
+  display: flex;
+  justify-content: space-around;
+
+  width: 100%;
+  max-width: ${(props) => props.theme.width.pad};
+  padding: 16px 32px;
+
+  background-color: ${(props) => props.theme.color.background};
+  box-shadow: 0 -1px 1px -1px ${(props) => props.theme.color.subLight};
+`;
+
+const Button = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 36px;
+  padding: 0 1rem;
+
+  font-size: 1.5rem;
+  font-weight: 500;
+  letter-spacing: 1px;
+
+  border-radius: 4px;
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+export const PrimaryButton = styled(Button)`
+  color: ${({ theme }) => theme.color.background};
+  background: ${(props) => props.theme.color.primary};
+
+  &:disabled {
+    color: ${(props) => props.theme.color.sub + '40'};
+    background: ${(props) => props.theme.color.primary + '40'};
+  }
+`;
--- frontend/src/pages/DictionaryPlantDetail/index.tsx
+++ frontend/src/pages/DictionaryPlantDetail/index.tsx
@@ -1,9 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { generatePath, useNavigate, useParams } from 'react-router-dom';
+import { Header } from 'pages/PetPlantRegister/Form/Form.style';
 import Image from 'components/@common/Image';
-import Navbar from 'components/@common/Navbar';
+import SvgFill from 'components/@common/SvgIcons/SvgFill';
 import DictionaryPlantContent from 'components/dictionaryPlant/DictionaryPlantContent';
-import { Main } from './DictionaryPlantDetail.style';
+import { BackButton, Footer, Main, PrimaryButton } from './DictionaryPlantDetail.style';
 import useDictionaryPlantDetail from 'hooks/queries/dictionaryPlant/useDictionaryPlantDetail';
+import { URL_PATH } from 'constants/index';
+import theme from 'style/theme.style';
 
 const DictionaryPlantDetail = () => {
   const { id } = useParams();
@@ -13,13 +16,30 @@ const DictionaryPlantDetail = () => {
   const { data: dictionaryPlantDetail } = useDictionaryPlantDetail(dictionaryPlantId);
   const { image, name } = dictionaryPlantDetail;
 
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goPetPlantRegisterForm = () => {
+    navigate(generatePath(URL_PATH.petRegisterForm, { id: String(dictionaryPlantId) }));
+  };
+
   return (
     <>
+      <Header>
+        <BackButton onClick={goBack}>
+          <SvgFill icon="line-arrow-left" aria-label="뒤로 가기" color={theme.color.sub} />
+        </BackButton>
+      </Header>
       <Main>
         <Image type="wide" src={image} alt={name} size="300px" />
         <DictionaryPlantContent {...dictionaryPlantDetail} />
       </Main>
-      <Navbar />
+      <Footer>
+        <PrimaryButton onClick={goPetPlantRegisterForm}>반려 식물로 등록하기</PrimaryButton>
+      </Footer>
     </>
   );
 };
