1ba62abd7ea4d1c9f1cb325e288585b6c65c89b5
bassyu <yrkdgus@gmail.com>
2023-09-19 15:29:07 +0900

    chore: GardenPostList import 추가

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import DictionaryPlantDetail from 'pages/DictionaryPlantDetail';
 import DictionaryPlantSearch from 'pages/DictionaryPlantSearch';
 import NotFound from 'pages/Error/NotFound';
+import GardenPostList from 'pages/GardenPostList';
 import GardenRegisterForm from 'pages/GardenRegister/Form';
 import GardenRegisterPick from 'pages/GardenRegister/Pick';
 import Main from 'pages/Main';
