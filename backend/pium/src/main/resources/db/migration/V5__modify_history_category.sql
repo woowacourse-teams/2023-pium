09158023229083c4d8896b212214682803c54986
최준호 <junho5336@gmail.com>
2023-09-19 16:08:45 +0900

    fix: flyway 구문 수정

--- backend/pium/src/main/resources/db/migration/V5__modify_history_category.sql
+++ backend/pium/src/main/resources/db/migration/V5__modify_history_category.sql
@@ -1,4 +1,4 @@
 ALTER TABLE history_category DROP COLUMN created_at;
 ALTER TABLE history_category DROP COLUMN updated_at;
 
-ALTER TABLE history_category ADD history_type VARCHAR(255) UNIQUE;
+ALTER TABLE history_category ADD UNIQUE (history_type);
