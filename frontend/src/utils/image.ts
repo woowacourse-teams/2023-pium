fc7bee351a57a41caba7385461746de938ad8ac8
Q Kim <qoli0703@gmail.com>
2023-09-20 21:31:55 +0900

    feat: 사진 크기에 따라 url을 제시하는 유틸 함수

--- frontend/src/utils/image.ts
+++ frontend/src/utils/image.ts
@@ -14,3 +14,34 @@ export const getImageUrl = (file: File) => {
 
 export const isAllowedImageExtension = (file: File) =>
   ALLOWED_IMAGE_EXTENSIONS.includes(file.type.toLowerCase());
+
+const X_SMALL_WIDTH = 64;
+const SMALL_WIDTH = 256;
+
+/**
+ * 피움 서비스의 정적 이미지 파일 네이밍 정책을 따르는
+ * 알맞은 크기의 사진 url을 반환합니다.
+ *
+ * `size`가 64보다 작으면 x-small, 256보다 작으면 small 크기로 재조정된 사진 url을 반환합니다.
+ *
+ * @param url img src에 들어갈 수 있는 url. 확장자명으로 끝나야 제대로 작동합니다.
+ * @param size 사진 픽셀 크기
+ * @param extension 원하는 확장자
+ * @returns 새로운 url
+ */
+export const getResizedImageUrl = (url: string, size: number, extension: 'png' | 'webp') => {
+  const urlTokens = url.split('.');
+  const originalExtension = urlTokens.pop();
+
+  if (originalExtension === undefined) return '';
+
+  if (size < X_SMALL_WIDTH) {
+    urlTokens.push('x-small', extension);
+  } else if (size < SMALL_WIDTH) {
+    urlTokens.push('small', extension);
+  } else {
+    urlTokens.push(originalExtension);
+  }
+
+  return urlTokens.join('.');
+};
