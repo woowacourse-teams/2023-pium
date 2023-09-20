71c5c1056b83a5a0317c3a5196895d8a06898d32
hozzijeong <ghwls0905@naver.com>
2023-09-20 12:57:37 +0900

    refactor: generateSiteMap 위치 수정

--- frontend/generateSiteMap.js
+++ frontend/generateSiteMap.js
@@ -30,9 +30,9 @@ const generateSiteMap = `
     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">        
-            ${pageSitemap}
+    ${pageSitemap}
   </urlset>`;
 
 const formattedSitemap = formatting(generateSiteMap);
 
-fs.writeFileSync('../../public/seo/sitemap.xml', formattedSitemap, 'utf8');
+fs.writeFileSync('public/seo/sitemap.xml', formattedSitemap, 'utf8');
