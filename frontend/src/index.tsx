d2b4a55af6a06118e0c8aafeebaf906ff37fbcc3
hozzijeong <ghwls0905@naver.com>
2023-09-27 16:36:08 +0900

    feat: serviceWorker 작성

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import swDev from './swDev';
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { worker } = require('./mocks/browser');
+// if (process.env.NODE_ENV === 'development') {
+//   // eslint-disable-next-line @typescript-eslint/no-var-requires
+//   const { worker } = require('./mocks/browser');
 
-  worker.start({
-    serviceWorker: {
-      url: 'http://localhost:8282/mockServiceWorker.js',
-    },
-  });
-}
-
+//   worker.start({
+//     serviceWorker: {
+//       url: 'http://localhost:8282/mockServiceWorker.js',
+//     },
+//   });
+// } else {
+swDev();
+// }
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
