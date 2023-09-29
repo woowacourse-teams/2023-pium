d4008e6fabed8370381106b4107a4c070054273b
bassyu <yrkdgus@gmail.com>
2023-09-29 21:47:41 +0900

    feat: 토스트 고도화
    - 디자인 변경
    - 프로그래스바 추가
    - 닫기 버튼 추가
    - 인터렉션 버튼 추가

--- frontend/src/components/@common/Toast/index.tsx
+++ frontend/src/components/@common/Toast/index.tsx
@@ -1,34 +1,75 @@
 import type { ToastItem } from 'types/@common';
 import { memo, useEffect, useState, useCallback } from 'react';
-import SvgIcons from 'components/@common/SvgIcons/SvgFill';
-import { IconArea, Message, MessageArea, Title, Wrapper } from './Toast.style';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import SvgFill from 'components/@common/SvgIcons/SvgFill';
+import {
+  IconArea,
+  InteractionButton,
+  LeftArea,
+  Message,
+  ContentArea,
+  CloseButton,
+  Title,
+  Wrapper,
+  ProgressBar,
+} from './Toast.style';
+import { toastListState } from 'store/atoms/@common';
 import theme from 'style/theme.style';
 
 const icons: Record<ToastItem['type'], JSX.Element> = {
-  info: <SvgIcons color={theme.color.background} icon="info-circle" aria-label="정보 알림" />,
-  success: <SvgIcons color={theme.color.background} icon="check-circle" aria-label="성공 알림" />,
-  warning: <SvgIcons color={theme.color.background} icon="warning" aria-label="경고 알림" />,
-  error: <SvgIcons color={theme.color.background} icon="close-circle" aria-label="실패 알림" />,
+  info: <SvgFill color={theme.color.primary} icon="info-circle" aria-label="정보 알림" size={20} />,
+  success: <SvgFill color="#3fa2ed" icon="check-circle" aria-label="성공 알림" size={20} />,
+  warning: <SvgFill color="#ec9b40" icon="warning" aria-label="경고 알림" size={20} />,
+  error: (
+    <SvgFill color={theme.color.accent} icon="close-circle" aria-label="실패 알림" size={20} />
+  ),
 };
 
-const Toast = ({ type, title, message }: ToastItem) => {
+const Toast = (props: ToastItem) => {
+  const { id, type, message, time = 2100, title, buttonContent, onClickButton } = props;
+
+  const [toastList, setToastList] = useRecoilState(toastListState);
   const [visible, setVisible] = useState(true);
 
-  const handleToastClose = useCallback(() => {
+  const close = useCallback(() => {
     setVisible(false);
+    setTimeout(() => {
+      setToastList((prev) => prev.filter(({ id: toastId }) => toastId !== id));
+    }, 200);
   }, []);
 
+  const handleClickButton = () => {
+    onClickButton?.();
+    close();
+  };
+
   useEffect(() => {
-    setTimeout(handleToastClose, 2100);
-  }, [handleToastClose]);
+    setTimeout(close, time);
+  }, [close, time]);
 
   return (
-    <Wrapper $type={type} $visible={visible} role="alert">
-      <IconArea>{icons[type]}</IconArea>
-      <MessageArea>
-        {title && <Title>{title}</Title>}
-        <Message>{message}</Message>
-      </MessageArea>
+    <Wrapper
+      role="alert"
+      $type={type}
+      $visible={visible}
+      $isTop={toastList[toastList.length - 1].id === id}
+    >
+      <LeftArea>
+        <IconArea>{icons[type]}</IconArea>
+        <ContentArea>
+          {title && <Title>{title}</Title>}
+          <Message>{message}</Message>
+          {buttonContent && (
+            <InteractionButton type="button" onClick={handleClickButton}>
+              {buttonContent}
+            </InteractionButton>
+          )}
+        </ContentArea>
+      </LeftArea>
+      <CloseButton type="button" onClick={close}>
+        <SvgFill size={16} icon="close" color={theme.color.sub} />
+      </CloseButton>
+      <ProgressBar $type={type} $time={time} />
     </Wrapper>
   );
 };
