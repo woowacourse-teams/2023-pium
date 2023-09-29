d796229383b52f84471f68e133984bd89e26789f
hozzijeong <ghwls0905@naver.com>
2023-09-29 23:27:24 +0900

    chore: msw 주석 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,18 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import swDev from './swDev';
 
-// if (process.env.NODE_ENV === 'development') {
-//   // eslint-disable-next-line @typescript-eslint/no-var-requires
-//   const { worker } = require('./mocks/browser');
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require('./mocks/browser');
 
-//   worker.start({
-//     serviceWorker: {
-//       url: 'http://localhost:8282/mockServiceWorker.js',
-//     },
-//   });
-// } else {
-swDev();
-// }
+  worker.start({
+    serviceWorker: {
+      url: 'http://localhost:8282/mockServiceWorker.js',
+    },
+  });
+} else {
+  swDev();
+}
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
