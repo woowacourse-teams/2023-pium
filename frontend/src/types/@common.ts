a962cb7b8f585c75873d1375f8670285393ed769
bassyu <yrkdgus@gmail.com>
2023-09-29 21:51:06 +0900

    refactor: addToast 사용법 변경
    - 버튼 내용, onClick param 추가
    - 닫기 제어를 훅에서 컴포넌트로 이동

--- frontend/src/types/@common.ts
+++ frontend/src/types/@common.ts
@@ -2,7 +2,10 @@ export interface ToastItem {
   id: string;
   type: 'info' | 'success' | 'warning' | 'error';
   message: string;
+  time?: number;
   title?: string;
+  buttonContent?: string;
+  onClickButton?: () => void;
 }
 
 export interface ImageFormData<T> {
