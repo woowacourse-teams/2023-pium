de947727654c47eae06e2c9530ec3d684f400bb7
hozzijeong <ghwls0905@naver.com>
2023-10-01 19:40:53 +0900

    chore: submodule 업데이트

--- frontend/src/swDev.ts
+++ frontend/src/swDev.ts
@@ -1,5 +1,5 @@
 const swDev = () => {
-  const swUrl = `${process.env.HOST}/serviceWorker.js`;
+  const swUrl = `${process.env.ROOT}/serviceWorker.js`;
 
   navigator.serviceWorker.register(swUrl).then((response) => {
     console.warn('response', response);
