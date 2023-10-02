af5297e1f7274b625eeb3570b0805b0ba01d5698
hozzijeong <ghwls0905@naver.com>
2023-10-02 15:18:37 +0900

    feat: 바로가기 Prompt 생성

---
+++ frontend/src/components/@common/InstallPrompt/InstallPrompt.style.ts
@@ -0,0 +1,71 @@
+import styled, { keyframes } from 'styled-components';
+
+const slideDown = keyframes` 
+  0% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  `;
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  width: 100%;
+  min-width: ${({ theme }) => theme.width.mobile};
+  max-width: ${({ theme }) => theme.width.pad};
+  height: 120px;
+  margin: 16px 0;
+  padding: 0 32px;
+
+  background: ${({ theme }) => theme.color.background};
+  border-bottom: solid 2px ${({ theme }) => theme.color.grayLight};
+
+  animation: ${slideDown} 1s ease-in-out;
+`;
+
+export const ContentWrapper = styled.div`
+  position: relative;
+
+  svg {
+    position: absolute;
+    top: -20px;
+    right: 0;
+  }
+`;
+
+export const GuideParagraph = styled.p`
+  font: 500 1.6rem/2.2rem NanumSquareRound;
+`;
+
+export const IosGuide = styled.p`
+  justify-content: center;
+  height: 30px;
+  font: 600 1.6rem/2.2rem NanumsquareRound;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+  height: 30px;
+  font: 500 1.8rem/2.2rem NanumSquareRound;
+
+  button {
+    width: 100%;
+    background-color: ${({ theme }) => theme.color.grayLight};
+    border-radius: 8px;
+  }
+
+  :last-child {
+    color: ${({ theme }) => theme.color.background};
+    background-color: ${({ theme }) => theme.color.primary};
+  }
+`;
--- frontend/src/components/@common/SvgIcons/SvgSpriteMap.tsx
+++ frontend/src/components/@common/SvgIcons/SvgSpriteMap.tsx
@@ -195,6 +195,9 @@ const SvgIcons = () => (
           d="M5 12h7m7 0h-7m0 0V5m0 7v7"
         ></path>
       </symbol>
+      <symbol viewBox="0 0 24 24" id="ios-share">
+        <path d="M6 23q-.825 0-1.413-.588T4 21V10q0-.825.588-1.413T6 8h3v2H6v11h12V10h-3V8h3q.825 0 1.413.588T20 10v11q0 .825-.588 1.413T18 23H6Zm5-7V4.825l-1.6 1.6L8 5l4-4l4 4l-1.4 1.425l-1.6-1.6V16h-2Z"></path>
+      </symbol>
     </defs>
   </Svg>
 );
