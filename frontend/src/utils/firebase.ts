0b1f78112c33768101e043a7df86253fe1d15746
hozzijeong <ghwls0905@naver.com>
2023-10-04 22:37:03 +0900

    chore: firebase 이동

---
+++ frontend/src/utils/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNVyJ1qLPvMiuZDK97O-rmB3mz48UqC1g',
+  authDomain: 'pium-7445f.firebaseapp.com',
+  projectId: 'pium-7445f',
+  storageBucket: 'pium-7445f.appspot.com',
+  messagingSenderId: '14284052337',
+  appId: '1:14284052337:web:4ccb34224d907e73fa48d0',
+  measurementId: 'G-Z5F62MDJ8N',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
+export { analytics, messaging };
