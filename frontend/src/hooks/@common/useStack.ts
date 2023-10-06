5b540252f9636e5613ef1e8aba0af2cc5e08b373
Q Kim <qoli0703@gmail.com>
2023-10-06 10:40:01 +0900

    refactor: 배포한 `@pium/stack-component` 적용 (#419)
    * chore: update submodule
    
    * chore: pium stack component 설치
    
    * refactor: 라이브러리 적용
    
    * chore: 사용하지 않는 파일 삭제
    
    * test: cypress 코드 수정
    
    - dom 순서와 ui 순서가 같아져서 돌아가지 않던 코드 수정

--- frontend/src/hooks/@common/useStack.ts
+++ frontend/src/hooks/@common/useStack.ts
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 
-const useStack = (stackSize: number) => {
-  const [topIndex, setTopIndex] = useState(0);
-  const isLastElementShown = topIndex + 1 === stackSize;
+/**
+ * `@pium/stack-component`를 사용하기 위한 훅.
+ * @param initialShowCount 맨 처음에 보여줄 개수
+ * @param stackSize 스택 안에 들어가는 요소의 수
+ * @returns `showCount`: 현재 보여주는 개수
+ * @returns `showNextElement`: 현재 보여지고 있는 마지막 요소의 index를 받았다면 다음 요소를 보여주는 함수
+ * @returns `isLastElementShown`: 마지막 요소까지 전부 보여줬다면 `true`
+ */
+const useStack = (initialShowCount: number, stackSize: number) => {
+  const [showCount, setShowCount] = useState(initialShowCount);
+  const isLastElementShown = showCount === stackSize;
 
-  const showNextElement = (index: number) => {
-    const nextAlreadyShown = topIndex > index;
+  const showNextElement = (myIndex: number) => {
+    const nextAlreadyShown = showCount - 1 > myIndex;
     if (isLastElementShown || nextAlreadyShown) return;
 
-    setTopIndex(index + 1);
+    setShowCount(showCount + 1);
   };
 
-  return { topIndex, showNextElement, isLastElementShown };
+  return { showCount, showNextElement, isLastElementShown };
 };
 
 export default useStack;
