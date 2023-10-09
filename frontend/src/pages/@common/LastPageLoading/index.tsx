b11d4d46c12f6c9ab1a55bac077ee9fedf3211dd
bassyu <yrkdgus@gmail.com>
2023-10-09 17:21:23 +0900

    design: backdrop 제거

--- frontend/src/pages/@common/LastPageLoading/index.tsx
+++ frontend/src/pages/@common/LastPageLoading/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { styled } from 'styled-components';
 import Loading from 'pages/@common/Loading';
 import { isShowPageLoadingState, lastPageState } from 'store/atoms/@common';
 
@@ -16,23 +15,7 @@ const LastPageLoading = () => {
     };
   }, []);
 
-  return (
-    <>
-      {lastPageValue || <Loading />}
-      <Backdrop />
-    </>
-  );
+  return lastPageValue || <Loading />;
 };
 
 export default LastPageLoading;
-
-const Backdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-
-  width: 100%;
-  height: 100%;
-
-  background-color: rgba(168, 168, 168, 0.1);
-`;
