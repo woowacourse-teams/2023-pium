dd1c42b2ed112b7dcc9a28dfa90789ecb6988170
bassyu <yrkdgus@gmail.com>
2023-10-09 17:38:21 +0900

    fix: Page effect에 로딩바 숨기기 추가

--- frontend/src/components/@common/PageLogger/index.tsx
+++ frontend/src/components/@common/PageLogger/index.tsx
@@ -1,13 +1,15 @@
 import { PropsWithChildren, useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { lastPageState } from 'store/atoms/@common';
+import { isShowPageLoadingState, lastPageState } from 'store/atoms/@common';
 
 const PageLogger = (props: PropsWithChildren) => {
   const { children } = props;
   const setLastPage = useSetRecoilState(lastPageState);
+  const setIsShowPageLoadingState = useSetRecoilState(isShowPageLoadingState);
 
   useEffect(() => {
     setLastPage(children);
+    setIsShowPageLoadingState(false);
   }, []);
 
   return children;
