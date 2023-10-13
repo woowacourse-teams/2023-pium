f9f0f8a71b1d0f64e3d144194d9504942f749f54
hozzijeong <ghwls0905@naver.com>
2023-10-13 11:07:16 +0900

    refactor: 의존성 업데이트

--- frontend/src/components/@common/Toggle/index.tsx
+++ frontend/src/components/@common/Toggle/index.tsx
@@ -24,7 +24,7 @@ const Toggle = ({
     } else {
       toggleOnCallback && toggleOnCallback();
     }
-  }, [state]);
+  }, [state, toggleOffCallback, toggleOnCallback]);
 
   return (
     <Wrapper>
