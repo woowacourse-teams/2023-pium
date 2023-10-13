52bfbcde329e0db9e8e563b66b588ce64c5b1ad9
hozzijeong <ghwls0905@naver.com>
2023-10-13 20:38:40 +0900

    feat: footer에 개인정보 처리 방침 추가

--- frontend/src/pages/@common/Home/index.tsx
+++ frontend/src/pages/@common/Home/index.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import InstallPrompt from 'components/@common/InstallPrompt';
 import Navbar from 'components/@common/Navbar';
 import PageLogger from 'components/@common/PageLogger';
 import SearchBox from 'components/search/SearchBox';
-import { LogoMessage, SearchBoxArea, SearchMessage, Main, Image, ImageArea } from './Home.style';
+import {
+  LogoMessage,
+  SearchBoxArea,
+  SearchMessage,
+  Main,
+  Image,
+  ImageArea,
+  Footer,
+} from './Home.style';
 import useDictionaryNavigate from 'hooks/dictionaryPlant/useDictionaryPlantNavigate';
+import { URL_PATH } from 'constants/index';
 import LogoSvg from 'assets/logo.svg';
 import LogoWebp from 'assets/logo.webp';
 
@@ -34,6 +44,13 @@ const Home = () => {
         </SearchBoxArea>
         <SearchMessage>피움에 등록된 식물을 검색해 보세요!</SearchMessage>
       </Main>
+      <Footer>
+        <Link to={URL_PATH.privacy}>개인정보 처리 방침</Link>
+        <p>
+          contact:<a href={`mailto: easyplantscheduler@gmail.com`}> easyplantscheduler@gmail.com</a>
+        </p>
+        <p>©️ 피움 All rights reserved</p>
+      </Footer>
       <Navbar />
     </PageLogger>
   );
