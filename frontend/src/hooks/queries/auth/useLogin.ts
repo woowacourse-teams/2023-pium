f6232a4832997ebe27960114458ca9784cc32e7c
hozzijeong <ghwls0905@naver.com>
2023-10-13 14:33:13 +0900

    refactor: 로그인 했을 경우 Success, Error callback 작성

--- frontend/src/hooks/queries/auth/useLogin.ts
+++ frontend/src/hooks/queries/auth/useLogin.ts
@@ -1,15 +1,31 @@
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import useAddToast from 'hooks/@common/useAddToast';
 import AuthAPI from 'apis/auth';
 import throwOnInvalidStatus from 'utils/throwOnInvalidStatus';
+import { URL_PATH } from 'constants/index';
 
-const useLogin = () =>
-  useMutation<null, Error, string>({
+const useLogin = () => {
+  const navigation = useNavigate();
+  const addToast = useAddToast();
+
+  return useMutation<null, Error, string>({
     mutationFn: async (code: string) => {
       const response = await AuthAPI.getSessionId(code);
 
       throwOnInvalidStatus(response);
       return null;
     },
+
+    onSuccess: () => {
+      navigation(URL_PATH.reminder, { replace: true });
+    },
+
+    onError: (error) => {
+      addToast({ type: 'error', message: error.message, time: 3000 });
+      navigation(URL_PATH.login, { replace: true });
+    },
   });
+};
 
 export default useLogin;
