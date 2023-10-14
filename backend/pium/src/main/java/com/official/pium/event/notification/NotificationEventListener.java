d709877420b8ccd60d21d8ef5e4c390384dda74f
최준호 <junho5336@gmail.com>
2023-10-14 10:20:28 +0900

    refactor: 알림 전송 이벤트 비동기처리

--- backend/pium/src/main/java/com/official/pium/event/notification/NotificationEventListener.java
+++ backend/pium/src/main/java/com/official/pium/event/notification/NotificationEventListener.java
@@ -4,6 +4,7 @@ import com.official.pium.service.NotificationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +14,7 @@ public class NotificationEventListener {
     private final NotificationService notificationService;
 
     @EventListener
+    @Async
     public void handleNotificationEvents(List<NotificationEvent> notificationEvent) {
         for (NotificationEvent event : notificationEvent) {
             notificationService.sendNotification(event.getDeviceToken(), event.getTitle(), event.getBody());
