fb50fd8be2e88233c5396e92c79a2f3ffa279f48
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-10-14 22:26:43 +0900

    fix: 로그인 되지 않던 이슈 해결 (#432)
    * refactor: 렌더링 이후에 api 호출하도록 변경
    
    * refactor: 로그인 했을 경우 Success, Error callback 작성

--- frontend/src/pages/auth/Login/Authorization.tsx
+++ frontend/src/pages/auth/Login/Authorization.tsx
@@ -1,18 +1,19 @@
 import { useEffect } from 'react';
-import { Navigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import Loading from 'pages/@common/Loading';
 import useLogin from 'hooks/queries/auth/useLogin';
-import { URL_PATH } from 'constants/index';
 
 const Authorization = () => {
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
+
   const { mutate: login } = useLogin();
 
   useEffect(() => {
     login(code ?? '');
   }, [code, login]);
 
-  return <Navigate to={URL_PATH.reminder} replace />;
+  return <Loading />;
 };
 
 export default Authorization;
