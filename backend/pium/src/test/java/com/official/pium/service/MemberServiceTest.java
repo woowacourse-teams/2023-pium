1fbc83d041eefbeafe0e4b1f7c58dbe70bf71100
최준호 <junho5336@gmail.com>
2023-10-14 10:10:36 +0900

    test: 알림 구독/취소 예외 테스트 추가

--- backend/pium/src/test/java/com/official/pium/service/MemberServiceTest.java
+++ backend/pium/src/test/java/com/official/pium/service/MemberServiceTest.java
@@ -1,24 +1,20 @@
 package com.official.pium.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.SoftAssertions.*;
 
-import com.official.pium.config.DatabaseClearExtension;
-import com.official.pium.domain.Member;
-import com.official.pium.repository.MemberRepository;
-import com.official.pium.repository.PetPlantRepository;
-import com.official.pium.service.dto.NotificationCheckResponse;
-import com.official.pium.service.dto.NotificationSubscribeRequest;
-import com.official.pium.support.MemberSupport;
-import com.official.pium.support.PetPlantSupport;
-import java.util.Optional;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import com.official.pium.config.*;
+import com.official.pium.domain.*;
+import com.official.pium.repository.*;
+import com.official.pium.service.dto.*;
+import com.official.pium.support.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.context.SpringBootTest.*;
+import java.util.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -97,6 +93,34 @@ class MemberServiceTest {
         memberService.unSubscribeNotification(saveMember);
 
         assertThat(saveMember.getDeviceToken()).isNull();
+    }
+
+    @Test
+    void 알림_구독_시_구독중인_상태면_예외() {
+        Member saveMember = memberSupport.builder()
+                .kakaoId(123451L)
+                .deviceToken("deviceToken")
+                .build();
+        NotificationSubscribeRequest request = NotificationSubscribeRequest.builder()
+                .deviceToken("deviceToken")
+                .build();
+
+        Assertions.assertThatThrownBy(() -> {
+                    memberService.subscribeNotification(saveMember, request);
+                })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 알림을 구독하고 있습니다.");
+    }
+
+    @Test
+    void 알림_해지_시_구독중인_상태가_아니면_예외() {
+        Member saveMember = memberSupport.builder()
+                .kakaoId(123451L)
+                .build();
+
+        Assertions.assertThatThrownBy(() -> memberService.unSubscribeNotification(saveMember))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 알림을 구독하지 않고 있습니다.");
     }
 
     @Test
