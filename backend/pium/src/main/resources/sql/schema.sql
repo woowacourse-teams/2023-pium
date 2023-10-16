1f72b09142761924aeea7ce0e88126d01e96d048
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-10-16 17:36:28 +0900

    build: 세션 그룹 스키마 작성

--- backend/pium/src/main/resources/sql/schema.sql
+++ backend/pium/src/main/resources/sql/schema.sql
@@ -125,3 +125,18 @@ ALTER TABLE member
 
 ALTER TABLE history_category
     ADD UNIQUE (history_type);
+
+CREATE TABLE IF NOT EXISTS session_group
+(
+    id                  BIGINT AUTO_INCREMENT NOT NULL,
+    session_id          VARCHAR(255)          NOT NULL,
+    session_key         VARCHAR(255)          NOT NULL,
+    session_value       VARCHAR(255)          NOT NULL,
+    created_at          DATETIME              NOT NULL,
+    updated_at          DATETIME              NOT NULL,
+    expire_time         DATETIME              NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE session_group
+ADD CONSTRAINT uniq_sessions UNIQUE(session_id, session_key);
