7251b8b42aa1bd908f637f971633bfb60b53b78f
Q Kim <qoli0703@gmail.com>
2023-10-19 18:22:27 +0900

    fix: 내비게이션 바 버그 수정 (#450)
    * fix: 버그 수정
    
    * fix: navbar 조건부 렌더링으로 변경
    
    * refactor: 로그인, 로그아웃 시에 query chache 초기화
    
    * design: navbar 등장 애니메이션
    
    ---------
    
    Co-authored-by: hozzijeong <ghwls0905@naver.com>

--- frontend/src/components/@common/Navbar/index.tsx
+++ frontend/src/components/@common/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Link, matchRoutes, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Button, Roof, Wrapper } from './Navbar.style';
 import useAddToast from 'hooks/@common/useAddToast';
 import useNavbarRoofAnimation from 'hooks/@common/useNavbarRoofAnimation';
@@ -7,24 +7,17 @@ import useCheckSessionId from 'hooks/queries/auth/useCheckSessionId';
 import { URL_PATH } from 'constants/index';
 import NavItem from './NavItem';
 
-const NO_NAVIGATION_BAR_URLS = [
-  URL_PATH.petRegisterForm,
-  URL_PATH.dictDetail,
-  URL_PATH.petEdit,
-  URL_PATH.login,
-  URL_PATH.authorization,
-].map((path) => ({ path }));
-
 const Navbar = () => {
   const navigate = useNavigate();
   const { pathname, state } = useLocation();
 
   const addToast = useAddToast();
-  const { isSuccess: isLoggedIn } = useCheckSessionId(false);
   const { navbarRef, roofPosition, transitionOffset } = useNavbarRoofAnimation(
     state ? state.prevPathname ?? pathname : pathname,
     pathname
   );
+
+  const { isSuccess: isLoggedIn } = useCheckSessionId(false);
 
   useEffect(() => {
     const resetHistoryState = () => history.replaceState(null, '');
@@ -46,11 +39,10 @@ const Navbar = () => {
     });
   };
 
-  const hideNavbar = matchRoutes(NO_NAVIGATION_BAR_URLS, pathname) !== null;
   const newHistoryState = { prevPathname: pathname };
 
   return (
-    <Wrapper ref={navbarRef} $hide={hideNavbar}>
+    <Wrapper ref={navbarRef}>
       <Button as={Link} to={URL_PATH.main} state={newHistoryState}>
         <NavItem isActive={roofPosition === 1} iconId="home-line" label="메인" />
       </Button>
