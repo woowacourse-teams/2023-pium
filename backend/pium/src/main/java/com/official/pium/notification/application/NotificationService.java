47dc1332c72304b7fdc83a3b29e15fd28ee3c334
DongUk <68818952+Kim0914@users.noreply.github.com>
2024-01-09 21:49:09 +0900

    refactor: 불필요한 트랜잭션 설정 제거

--- backend/pium/src/main/java/com/official/pium/notification/application/NotificationService.java
+++ backend/pium/src/main/java/com/official/pium/notification/application/NotificationService.java
@@ -2,10 +2,8 @@ package com.official.pium.notification.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class NotificationService {
 
