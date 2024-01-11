659ca56710b05846f2a4e9ba8c202b4da44d68e8
DongUk <68818952+Kim0914@users.noreply.github.com>
2024-01-11 21:15:46 +0900

    refactor: Async ThreadPool 스레드 수 30으로 변경

--- backend/pium/src/main/java/com/official/pium/config/AsyncConfig.java
+++ backend/pium/src/main/java/com/official/pium/config/AsyncConfig.java
@@ -13,7 +13,7 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(40);
+        executor.setCorePoolSize(30);
 //        executor.setQueueCapacity(15);
 //        executor.setMaxPoolSize(200);
         executor.setThreadNamePrefix("2024-Pium-Thread: ");
