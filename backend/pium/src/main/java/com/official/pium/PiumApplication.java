9fd8bdf8d42600b1b574febe98da447d9f582b30
DongUk <68818952+Kim0914@users.noreply.github.com>
2024-01-22 15:38:38 +0900

    chore: 불필요한 주석 제거

--- backend/pium/src/main/java/com/official/pium/PiumApplication.java
+++ backend/pium/src/main/java/com/official/pium/PiumApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAsync
 public class PiumApplication {
 
     public static void main(String[] args) {
