1e79b974640e1a4cf656799a51eac699e82c4ef8
DongUk <68818952+Kim0914@users.noreply.github.com>
2024-01-25 18:35:19 +0900

    chore: 테스트 메서드 주석 해제

--- backend/pium/src/main/java/com/official/pium/admin/ui/TestController.java
+++ backend/pium/src/main/java/com/official/pium/admin/ui/TestController.java
@@ -2,6 +2,8 @@ package com.official.pium.admin.ui;
 
 import com.official.pium.admin.service.TestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,15 @@ public class TestController {
 
     private final TestService testService;
 
-//    @GetMapping("/notifications/ramp")
-//    public ResponseEntity<String> notificationRampTest() {
-//        testService.sendWaterNotificationAsyncRampTest();
-//        return ResponseEntity.ok("비동기 알림 기능 테스트 램프업 성공");
-//    }
-//
-//    @GetMapping("/notifications/async")
-//    public ResponseEntity<String> notificationAsyncTest() {
-//        testService.sendWaterNotificationAsyncTest();
-//        return ResponseEntity.ok("비동기 알림 기능 테스트 성공");
-//    }
+    @GetMapping("/notifications/ramp")
+    public ResponseEntity<String> notificationRampTest() {
+        testService.sendWaterNotificationAsyncRampTest();
+        return ResponseEntity.ok("비동기 알림 기능 테스트 램프업 성공");
+    }
+
+    @GetMapping("/notifications/async")
+    public ResponseEntity<String> notificationAsyncTest() {
+        testService.sendWaterNotificationAsyncTest();
+        return ResponseEntity.ok("비동기 알림 기능 테스트 성공");
+    }
 }
