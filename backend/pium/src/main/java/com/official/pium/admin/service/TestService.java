c89909690ee7b3edad02f64b52db909d3e6b02ae
DongUk <68818952+Kim0914@users.noreply.github.com>
2024-01-25 16:26:28 +0900

    chore: 현재 필요하지 않는 테스트 메서드 주석 처리

--- backend/pium/src/main/java/com/official/pium/admin/service/TestService.java
+++ backend/pium/src/main/java/com/official/pium/admin/service/TestService.java
@@ -1,9 +1,6 @@
 package com.official.pium.admin.service;
 
-import com.official.pium.petPlant.domain.PetPlant;
-import com.official.pium.petPlant.event.notification.NotificationEvent;
 import com.official.pium.petPlant.repository.PetPlantRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
@@ -33,8 +30,8 @@ public class TestService {
 //        log.info("동기 알림 테스트 종료. Thread: " + Thread.currentThread().getId() + " " + Thread.currentThread().getName());
 //    }
 
-    public void sendWaterNotificationAsyncRampTest() {
-        List<PetPlant> petPlants = petPlantRepository.findAllByMemberId(7L);
+//    public void sendWaterNotificationAsyncRampTest() {
+//        List<PetPlant> petPlants = petPlantRepository.findAllByMemberId(7L);
 //        List<NotificationEvent> events = petPlants.stream()
 //                .map(plant -> NotificationEvent.builder()
 //                        .title(plant.getNickname())
@@ -43,39 +40,39 @@ public class TestService {
 //                        .build()
 //                ).toList();
 
-        for (int i = 0; i < 100; i++) {
-            PetPlant petPlant = petPlants.get(i);
-            NotificationEvent event = NotificationEvent.builder()
-                    .title(petPlant.getNickname())
-                    .body("물줘")
-                    .deviceToken(petPlant.getMember().getDeviceToken())
-                    .build();
-            publisher.publishEvent(event);
-        }
+//        for (int i = 0; i < 100; i++) {
+//            PetPlant petPlant = petPlants.get(i);
+//            NotificationEvent event = NotificationEvent.builder()
+//                    .title(petPlant.getNickname())
+//                    .body("물줘")
+//                    .deviceToken(petPlant.getMember().getDeviceToken())
+//                    .build();
+//            publisher.publishEvent(event);
+//        }
 
 //        log.info("비동기 테스트 램프업 시작");
 //        for (int i = 0; i < 100; i++) {
 //            NotificationEvent notificationEvent = events.get(i);
 //            publisher.publishEvent(notificationEvent);
 //        }
-    }
+//    }
 
-    public void sendWaterNotificationAsyncTest() {
-        List<PetPlant> petPlants = petPlantRepository.findAllByMemberId(7L);
-        List<NotificationEvent> events = petPlants.stream()
-                .map(plant -> NotificationEvent.builder()
-                        .title(plant.getNickname())
-                        .body("(테스트 중) 물을 줄 시간이에요!")
-                        .deviceToken(plant.getMember().getDeviceToken())
-                        .build()
-                ).toList();
-
-        int i = 1;
-        log.info("비동기 알림 테스트 시작. Thread: " + Thread.currentThread().getId() + " " + Thread.currentThread().getName());
-        for (NotificationEvent event : events) {
-            log.info(i++ + "번째 알림 이벤트");
-            publisher.publishEvent(event);
-        }
-        log.info("비동기 알림 테스트 종료. Thread: " + Thread.currentThread().getId() + " " + Thread.currentThread().getName());
-    }
+//    public void sendWaterNotificationAsyncTest() {
+//        List<PetPlant> petPlants = petPlantRepository.findAllByMemberId(7L);
+//        List<NotificationEvent> events = petPlants.stream()
+//                .map(plant -> NotificationEvent.builder()
+//                        .title(plant.getNickname())
+//                        .body("(테스트 중) 물을 줄 시간이에요!")
+//                        .deviceToken(plant.getMember().getDeviceToken())
+//                        .build()
+//                ).toList();
+//
+//        int i = 1;
+//        log.info("비동기 알림 테스트 시작. Thread: " + Thread.currentThread().getId() + " " + Thread.currentThread().getName());
+//        for (NotificationEvent event : events) {
+//            log.info(i++ + "번째 알림 이벤트");
+//            publisher.publishEvent(event);
+//        }
+//        log.info("비동기 알림 테스트 종료. Thread: " + Thread.currentThread().getId() + " " + Thread.currentThread().getName());
+//    }
 }
